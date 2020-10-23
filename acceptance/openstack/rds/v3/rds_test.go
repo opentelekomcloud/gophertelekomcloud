cc9fdd3735ad34fa9dc4834962712fc9c4e42eed
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2020-10-23 18:06:35 +0300

    Fix update and delete

--- acceptance/openstack/rds/v3/rds_test.go
+++ acceptance/openstack/rds/v3/rds_test.go
@@ -1,13 +1,13 @@
 package v3
 
 import (
-	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/networking/v2/extensions/security/groups"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/rds/v3/instances"
 	"testing"
-
-	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"time"
 )
 
 func TestRdsList(t *testing.T) {
@@ -42,14 +42,14 @@ func TestRdsCRUD(t *testing.T) {
 		t.Fatalf("Unable to create create: %s", err)
 	}
 	defer deleteRDS(t, client, rds.Id)
+
 	tools.PrintResource(t, rds)
 
-	var updateOpts instances.EnlargeVolumeRdsOpts
-	updateOpts.EnlargeVolume.Size = 200
-	_, err = instances.EnlargeVolume(client, updateOpts, rds.Id).Extract()
+	err = updateRDS(t, client, rds.Id)
 	if err != nil {
-		t.Fatalf("Unable to resize volume: %s", err)
+		t.Fatalf("Unable to update RDSv3 instance: %s", err)
 	}
+	tools.PrintResource(t, rds)
 
 	listOpts := instances.ListRdsInstanceOpts{
 		Id: rds.Id,
@@ -81,7 +81,7 @@ func createRDS(t *testing.T, client *golangsdk.ServiceClient) (*instances.Instan
 		Region:           clients.OS_REGION_NAME,
 		AvailabilityZone: clients.OS_AVAILABILITY_ZONE,
 		VpcId:            clients.OS_VPC_ID,
-		SubnetId:         clients.OS_SUBNET_ID,
+		SubnetId:         clients.OS_NETWORK_ID,
 		SecurityGroupId:  sg.ID,
 
 		Volume: &instances.Volume{
@@ -102,27 +102,61 @@ func createRDS(t *testing.T, client *golangsdk.ServiceClient) (*instances.Instan
 	if err != nil {
 		return nil, err
 	}
-	if err = instances.WaitForJobCompleted(client, int(600), jobResponse.JobID); err != nil {
+	if err = instances.WaitForJobCompleted(client, 1200, jobResponse.JobID); err != nil {
 		return nil, err
 	}
-	t.Logf("Created RDSv3: %s", rdsName)
+	t.Logf("Created RDSv3: %s", rds.Instance.Id)
 
 	return &rds.Instance, nil
 }
 
 func deleteRDS(t *testing.T, client *golangsdk.ServiceClient, rdsId string) {
 	t.Logf("Attempting to delete RDSv3: %s", rdsId)
-
-	jobResponse, err := instances.Delete(client, rdsId).ExtractJobResponse()
-	if err != nil {
-		t.Fatalf("Unable to extract RDS delete response: %s", err)
+	listOpts := instances.ListRdsInstanceOpts{
+		Id: rdsId,
 	}
-	err = instances.WaitForJobCompleted(client, 600, jobResponse.JobID)
+	allPages, err := instances.List(client, listOpts).AllPages()
 	if err != nil {
-		t.Fatalf("Error deleting RDSv3: %v", err)
+		t.Fatalf("Unable to get all RDS pages: %s", err)
+	}
+	rds, err := instances.ExtractRdsInstances(allPages)
+	if err != nil {
+		t.Fatalf("Unable to extract RDS pages: %s", err)
 	}
 
-	t.Logf("Deleted RDSv3: %s", rdsId)
+	_, err = instances.Delete(client, rdsId).ExtractJobResponse()
+	if err != nil {
+		t.Fatalf("Unable to delete RDSv3: %s", err)
+	}
+	time.Sleep(4 * time.Minute)
+	t.Logf("RDSv3 instance deleted: %s", rdsId)
+
+	deleteSecGroup(t, rds.Instances[0].SecurityGroupId)
+}
+
+func updateRDS(t *testing.T, client *golangsdk.ServiceClient, rdsId string) error {
+	t.Logf("Attempting to increase volume size")
+
+	t.Logf("Update volume size could be done only in status `available`")
+	if err := instances.WaitForStateAvailable(client, 600, rdsId); err != nil {
+		t.Fatalf("Status available wasn't present")
+	}
+
+	updateOpts := instances.EnlargeVolumeRdsOpts{
+		EnlargeVolume: &instances.EnlargeVolumeSize{
+			Size: 200,
+		},
+	}
+
+	updateResult, err := instances.EnlargeVolume(client, updateOpts, rdsId).ExtractJobResponse()
+	if err != nil {
+		return err
+	}
+	if err := instances.WaitForJobCompleted(client, 1200, updateResult.JobID); err != nil {
+		return err
+	}
+	t.Logf("RDSv3 successfully updated: %s", rdsId)
+	return nil
 }
 
 func createSecGroup(sgName string) (*groups.SecGroup, error) {
@@ -139,4 +173,17 @@ func createSecGroup(sgName string) (*groups.SecGroup, error) {
 		return nil, err
 	}
 	return sg, nil
+}
+
+func deleteSecGroup(t *testing.T, sgID string) {
+	t.Logf("Attempting to delete networking_secgroup: %s", sgID)
+	nwClient, err := clients.NewNetworkV2Client()
+	if err != nil {
+		t.Fatalf("Unable to create Networkv2 client: %s", err)
+	}
+	err = groups.Delete(nwClient, sgID).ExtractErr()
+	if err != nil {
+		t.Fatalf("Unable to delete networking_secgroup: %s, err: %s", sgID, err)
+	}
+	t.Logf("Deleted networking_secgroup: %s", sgID)
 }
--- openstack/rds/v3/instances/results_job.go
+++ openstack/rds/v3/instances/results_job.go
@@ -2,8 +2,6 @@ package instances
 
 import (
 	"fmt"
-	"time"
-
 	"github.com/opentelekomcloud/gophertelekomcloud"
 )
 
@@ -38,21 +36,55 @@ func WaitForJobCompleted(client *golangsdk.ServiceClient, secs int, jobID string
 	jobClient.ResourceBase = jobClient.Endpoint
 
 	return golangsdk.WaitFor(secs, func() (bool, error) {
-		job := new(golangsdk.JobStatus)
-		_, err := jobClient.Get(jobClient.ServiceURL("jobs", jobID), &job, nil)
-		time.Sleep(5 * time.Second)
+		job := new(golangsdk.RDSJobStatus)
+
+		requestOpts := &golangsdk.RequestOpts{MoreHeaders: map[string]string{"Content-Type": "application/json"}}
+		_, err := jobClient.Get(jobURL(jobClient.ResourceBase, jobID), job, requestOpts)
 		if err != nil {
 			return false, err
 		}
 
-		if job.Status == "Completed" {
+		if job.Job.Status == "Completed" {
 			return true, nil
 		}
-		if job.Status == "Failed" {
-			err = fmt.Errorf("Job failed with code %s: %s.\n", job.ErrorCode, job.FailReason)
+		if job.Job.Status == "Failed" {
+			err = fmt.Errorf("Job failed %s.\n", job.Job.Status)
 			return false, err
 		}
 
 		return false, nil
 	})
+}
+
+func WaitForStateAvailable(client *golangsdk.ServiceClient, secs int, instanceID string) error {
+	jobClient := *client
+	jobClient.ResourceBase = jobClient.Endpoint
+
+	return golangsdk.WaitFor(secs, func() (bool, error) {
+		job := new(golangsdk.JsonRDSInstanceStatus)
+
+		requestOpts := &golangsdk.RequestOpts{MoreHeaders: map[string]string{"Content-Type": "application/json"}}
+		_, err := jobClient.Get(detailsURL(jobClient.ResourceBase, instanceID), job, requestOpts)
+		if err != nil {
+			return false, err
+		}
+
+		if job.Instances[0].Status == "ACTIVE" {
+			return true, nil
+		}
+		if job.Instances[0].Status == "FAILED" {
+			err = fmt.Errorf("Job failed %s.\n", job.Instances[0].Status)
+			return false, err
+		}
+
+		return false, nil
+	})
+}
+
+func jobURL(endpoint string, jobID string) string {
+	return fmt.Sprintf("%sjobs?id=%s", endpoint, jobID)
+}
+
+func detailsURL(endpoint string, instanceID string) string {
+	return fmt.Sprintf("%sinstances?id=%s", endpoint, instanceID)
 }
