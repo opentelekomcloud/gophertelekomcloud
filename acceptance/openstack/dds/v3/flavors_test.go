26a6ee68261aa57725193ceb2b232c070d03a394
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2020-11-23 17:31:38 +0300

    Fix issue with az in DDS flavor

---
+++ acceptance/openstack/dds/v3/flavors_test.go
@@ -0,0 +1,29 @@
+package v3
+
+import (
+	"testing"
+
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/dds/v3/flavors"
+)
+
+func TestDdsFlavorsList(t *testing.T) {
+	client, err := clients.NewDdsV3Client()
+	if err != nil {
+		t.Fatalf("Unable to create a DDSv3 client: %s", err)
+	}
+
+	listFlavorOpts := flavors.ListOpts{
+		Region: clients.OS_REGION_NAME,
+	}
+	allPages, err := flavors.List(client, listFlavorOpts).AllPages()
+	if err != nil {
+		t.Fatalf("Unable to get all pages: %s", err)
+	}
+	flavorsList, err := flavors.ExtractFlavors(allPages)
+	if err != nil {
+		t.Fatalf("Unable to extract DDS flavors: %s", err)
+	}
+	tools.PrintResource(t, flavorsList)
+}
