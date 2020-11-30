b55266877d3c7dc1bd0e9be037ebe6b0723d8373
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2020-11-30 16:40:04 +0300

    remove refs from test

--- openstack/networking/v2/extensions/vpnaas/ikepolicies/testing/requests_test.go
+++ openstack/networking/v2/extensions/vpnaas/ikepolicies/testing/requests_test.go
@@ -276,9 +276,9 @@ func TestUpdate(t *testing.T) {
 	updatedName := "updatedname"
 	updatedDescription := "updated policy"
 	options := ikepolicies.UpdateOpts{
-		Name:        &updatedName,
-		Description: &updatedDescription,
-		Lifetime: &ikepolicies.LifetimeUpdateOpts{
+		Name:        updatedName,
+		Description: updatedDescription,
+		Lifetime: ikepolicies.LifetimeUpdateOpts{
 			Value: 7000,
 		},
 	}
