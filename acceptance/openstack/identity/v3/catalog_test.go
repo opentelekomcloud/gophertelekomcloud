3fda010484fef73fc2cfeeb09d3e2897a157b475
Anton Kachurin <anton.kachurin@t-systems.com>
2020-12-04 15:44:10 +0300

    Simplified test

--- acceptance/openstack/identity/v3/catalog_test.go
+++ acceptance/openstack/identity/v3/catalog_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/identity/v3/catalog"
-	"github.com/opentelekomcloud/gophertelekomcloud/pagination"
 )
 
 func TestGetCatalog(t *testing.T) {
@@ -15,19 +14,8 @@ func TestGetCatalog(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Unable to obtain an identity client: %v", err)
 	}
-	var endpoints []string
-	err = catalog.List(client).EachPage(func(page pagination.Page) (bool, error) {
-		entries, err := catalog.ExtractServiceCatalog(page)
-		if err != nil {
-			return false, err
-		}
-		for _, ent := range entries {
-			for _, ep := range ent.Endpoints {
-				endpoints = append(endpoints, ep.URL)
-			}
-		}
-		return true, nil
-	})
+	allPages, err := catalog.List(client).AllPages()
 	require.NoError(t, err)
-	require.True(t, len(endpoints) > 0)
+	allServices, err := catalog.ExtractServiceCatalog(allPages)
+	require.True(t, len(allServices) > 0)
 }
