4e2b9de3747b5057974f60c2b98c9023c2d73fbc
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2020-12-08 17:29:53 +0300

    Fix test

--- acceptance/openstack/cbr/v3/tasks_test.go
+++ acceptance/openstack/cbr/v3/tasks_test.go
@@ -4,22 +4,27 @@ import (
 	"testing"
 
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/cbr/v3/tasks"
 )
 
 func TestTaskListAll(t *testing.T) {
 	client, err := clients.NewCbrV3Client()
 	if err != nil {
-		t.Fatalf("Unable to create a RDSv3 client: %s", err)
+		t.Fatalf("Unable to create a CBRv3 client: %s", err)
 	}
 
 	listOpts := tasks.ListOpts{}
 	pages, err := tasks.List(client, listOpts).AllPages()
 	if err != nil {
-		return
+		t.Fatalf("Unable to fetch CBR tasks pages: %s", err)
 	}
 	tasksList, err := tasks.ExtractTasks(pages)
 	if err != nil {
-		return
+		t.Fatalf("Unable to extract CBR tasks pages: %s", err)
+	}
+
+	for _, task := range tasksList {
+		tools.PrintResource(t, task)
 	}
 }
