57fcb8c86f387e317a397f4557c97308ded656ad
Anton Kachurin <anton.kachurin@t-systems.com>
2020-12-18 13:12:33 +0300

    Partly revert TestAuthenticatedClient

--- acceptance/openstack/client_test.go
+++ acceptance/openstack/client_test.go
@@ -14,9 +14,29 @@ import (
 )
 
 func TestAuthenticatedClient(t *testing.T) {
-	client, err := clients.NewObjectStorageV1Client()
+	cc, err := clients.CloudAndClient()
 	th.AssertNoErr(t, err)
-	t.Logf("Located a storage service at endpoint: [%s]", client.Endpoint)
+
+	if cc.TokenID == "" {
+		t.Errorf("No token ID assigned to the client")
+	}
+
+	if cc.ProjectID == "" {
+		t.Errorf("Project ID is not set for the client")
+	}
+	if cc.UserID == "" {
+		t.Errorf("User ID is not set for the client")
+	}
+	if cc.DomainID == "" {
+		t.Errorf("Domain ID is not set for the client")
+	}
+
+	// Find the storage service in the service catalog.
+	storage, err := openstack.NewObjectStorageV1(cc.ProviderClient, golangsdk.EndpointOpts{
+		Region: cc.RegionName,
+	})
+	th.AssertNoErr(t, err)
+	t.Logf("Located a storage service at endpoint: [%s]", storage.Endpoint)
 }
 
 func TestAuthTokenNoRegion(t *testing.T) {
