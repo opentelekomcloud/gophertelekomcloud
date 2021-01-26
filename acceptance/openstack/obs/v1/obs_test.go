841d4a7b3bd2a9c1e3229ec072a6738fcfefd851
Anton Kachurin <anton.kachurin@t-systems.com>
2021-01-26 17:20:16 +0300

    [OBS] Fix incorrect signature calculation (#79)
    [OBS] Fix incorrect signature calculation
    
    Partly revert #70: headers have to be stored lower case.
    Fix #78
    
    Reviewed-by: Anton Sidelnikov <None>
    Reviewed-by: None <None>
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>

---
+++ acceptance/openstack/obs/v1/obs_test.go
@@ -0,0 +1,65 @@
+package v1
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/obs"
+	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
+)
+
+func TestObsBucketLifecycle(t *testing.T) {
+	client, err := clients.NewOBSClient()
+	th.AssertNoErr(t, err)
+
+	bucketName := strings.ToLower(tools.RandomString("obs-sdk-test", 5))
+
+	createOpts := &obs.CreateBucketInput{
+		Bucket: bucketName,
+	}
+
+	_, err = client.CreateBucket(createOpts)
+	th.AssertNoErr(t, err)
+
+	_, err = client.DeleteBucket(bucketName)
+	th.AssertNoErr(t, err)
+}
+
+func TestObsObjectLifecycle(t *testing.T) {
+	client, err := clients.NewOBSClient()
+	th.AssertNoErr(t, err)
+
+	bucketName := strings.ToLower(tools.RandomString("obs-sdk-test", 5))
+
+	createOpts := &obs.CreateBucketInput{
+		Bucket: bucketName,
+	}
+
+	_, err = client.CreateBucket(createOpts)
+	th.AssertNoErr(t, err)
+
+	defer func() {
+		_, err = client.DeleteBucket(bucketName)
+		th.AssertNoErr(t, err)
+	}()
+
+	objectName := tools.RandomString("test-obs-", 5)
+
+	objectOpts := &obs.PutObjectInput{
+		PutObjectBasicInput: obs.PutObjectBasicInput{
+			ObjectOperationInput: obs.ObjectOperationInput{
+				Bucket: bucketName,
+				Key:    objectName,
+			},
+		},
+	}
+	_, err = client.PutObject(objectOpts)
+	th.AssertNoErr(t, err)
+
+	_, err = client.DeleteObject(&obs.DeleteObjectInput{
+		Bucket: bucketName,
+		Key:    objectName,
+	})
+}
