957f606723240977e32241947cf5e9d521709198
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-02-04 18:13:27 +0300

    Use default security group instead of creation

--- acceptance/openstack/common.go
+++ acceptance/openstack/common.go
@@ -5,7 +5,10 @@ package openstack
 import (
 	"testing"
 
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/extensions"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/compute/v2/extensions/secgroups"
+	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
 )
 
 // PrintExtension prints an extension and all of its attributes.
@@ -16,4 +19,22 @@ func PrintExtension(t *testing.T, extension *extensions.Extension) {
 	t.Logf("Description: %s", extension.Description)
 	t.Logf("Updated: %s", extension.Updated)
 	t.Logf("Links: %v", extension.Links)
+}
+
+func DefaultSecurityGroup(t *testing.T, computeClient *golangsdk.ServiceClient) string {
+	securityGroupPages, err := secgroups.List(computeClient).AllPages()
+	th.AssertNoErr(t, err)
+	securityGroups, err := secgroups.ExtractSecurityGroups(securityGroupPages)
+	th.AssertNoErr(t, err)
+	var sgId string
+	for _, val := range securityGroups {
+		if val.Name == "default" {
+			sgId = val.ID
+			break
+		}
+	}
+	if sgId == "" {
+		t.Fatalf("Unable to find default secgroup")
+	}
+	return sgId
 }
--- acceptance/openstack/dds/v3/instances_test.go
+++ acceptance/openstack/dds/v3/instances_test.go
@@ -5,8 +5,8 @@ import (
 
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/openstack"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
-	"github.com/opentelekomcloud/gophertelekomcloud/openstack/compute/v2/extensions/secgroups"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/dds/v3/instances"
 	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
 )
@@ -75,7 +75,7 @@ func createDdsInstance(t *testing.T, client *golangsdk.ServiceClient) *instances
 		AvailabilityZone: az,
 		VpcId:            vpcID,
 		SubnetId:         subnetID,
-		SecurityGroupId:  defaultSecurityGroup(t, computeClient),
+		SecurityGroupId:  openstack.DefaultSecurityGroup(t, computeClient),
 		Password:         "5ecurePa55w0rd@",
 		Mode:             "ReplicaSet",
 		Flavor: []instances.Flavor{
@@ -158,22 +158,4 @@ func waitForInstanceDelete(client *golangsdk.ServiceClient, secs int, instanceId
 		}
 		return false, nil
 	})
-}
-
-func defaultSecurityGroup(t *testing.T, computeClient *golangsdk.ServiceClient) string {
-	securityGroupPages, err := secgroups.List(computeClient).AllPages()
-	th.AssertNoErr(t, err)
-	securityGroups, err := secgroups.ExtractSecurityGroups(securityGroupPages)
-	th.AssertNoErr(t, err)
-	var sgId string
-	for _, val := range securityGroups {
-		if val.Name == "default" {
-			sgId = val.ID
-			break
-		}
-	}
-	if sgId == "" {
-		t.Fatalf("Unable to find default secgroup")
-	}
-	return sgId
 }
