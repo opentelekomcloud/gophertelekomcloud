7b26d6ba935c684fe082fb3bb1782f39c15a7ea3
Anton Kachurin <anton.kachurin@t-systems.com>
2021-03-23 16:00:26 +0300

    Fix empty CCE addon template list (#117)
    Fix empty CCE addon template list
    
    What this PR does / why we need it
    Fix incorrect handling of  ListTemplates response
    Add acceptance test for CCE addon template listing
    Which issue this PR fixes
    
    Fix #116
    
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>
    Reviewed-by: Anton Sidelnikov <None>
    Reviewed-by: None <None>

--- acceptance/openstack/cce/v3/addons_test.go
+++ acceptance/openstack/cce/v3/addons_test.go
@@ -1,50 +1,116 @@
 package v3
 
 import (
-	"fmt"
+	"encoding/json"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/suite"
 
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/cce/v3/addons"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/cce/v3/clusters"
-	"github.com/opentelekomcloud/gophertelekomcloud/openstack/networking/v1/subnets"
-	"github.com/opentelekomcloud/gophertelekomcloud/openstack/networking/v1/vpcs"
 	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
 )
 
-func TestAddonsLifecycle(t *testing.T) {
-	t.Skip("CCE addon acceptance test is skipped due to its issues and it leaves orphans")
-	vpcClient, err := clients.NewNetworkV1Client()
-	th.AssertNoErr(t, err)
-	cceClient, err := clients.NewCceV3Client()
-	th.AssertNoErr(t, err)
-	addonClient, err := clients.NewCceV3AddonClient()
+type testAddons struct {
+	suite.Suite
+
+	vpcID     string
+	subnetID  string
+	clusterID string
+}
+
+func TestAddons(t *testing.T) {
+	suite.Run(t, new(testAddons))
+}
+
+func (a *testAddons) SetupSuite() {
+	t := a.T()
+	a.vpcID = clients.EnvOS.GetEnv("VPC_ID")
+	a.subnetID = clients.EnvOS.GetEnv("NETWORK_ID")
+	if a.vpcID == "" || a.subnetID == "" {
+		t.Skip("OS_VPC_ID and OS_NETWORK_ID are required for this test")
+	}
+	a.clusterID = createCluster(t, a.vpcID, a.subnetID)
+}
+
+func (a *testAddons) TearDownSuite() {
+	t := a.T()
+	if a.clusterID != "" {
+		deleteCluster(t, a.clusterID)
+		a.clusterID = ""
+	}
+}
+
+func (a *testAddons) TestAddonsLifecycle() {
+	t := a.T()
+
+	client, err := clients.NewCceV3AddonClient()
 	th.AssertNoErr(t, err)
 
-	vpc, err := vpcs.Create(vpcClient, vpcs.CreateOpts{
-		Name: tools.RandomString("cce-vpc-", 4),
-		CIDR: "192.168.0.0/16",
-	}).Extract()
+	addon, err := addons.Create(client, addons.CreateOpts{
+		Kind:       "Addon",
+		ApiVersion: "v3",
+		Metadata: addons.CreateMetadata{
+			Annotations: addons.CreateAnnotations{
+				AddonInstallType: "install",
+			},
+		},
+		Spec: addons.RequestSpec{
+			Version:           "1.0.3",
+			ClusterID:         a.clusterID,
+			AddonTemplateName: "metrics-server",
+			Values: addons.Values{
+				Basic: map[string]interface{}{
+					"euleros_version": "2.5",
+					"rbac_enabled":    true,
+					"swr_addr":        "100.125.7.25:20202",
+					"swr_user":        "hwofficial",
+				},
+			},
+		},
+	}, a.clusterID).Extract()
 	th.AssertNoErr(t, err)
-	defer vpcs.Delete(vpcClient, vpc.ID)
 
-	subnet, err := subnets.Create(vpcClient, subnets.CreateOpts{
-		Name:             tools.RandomString("cce-subnet-", 4),
-		CIDR:             "192.168.0.0/24",
-		DnsList:          []string{"1.1.1.1", "8.8.8.8"},
-		GatewayIP:        "192.168.0.1",
-		EnableDHCP:       true,
-		AvailabilityZone: "eu-de-01",
-		VPC_ID:           vpc.ID,
-	}).Extract()
+	addonID := addon.Metadata.Id
+
+	defer func() {
+		err := addons.Delete(client, addonID, a.clusterID).ExtractErr()
+		th.AssertNoErr(t, err)
+	}()
+
+	getAddon, err := addons.Get(client, addon.Metadata.Id, a.clusterID).Extract()
 	th.AssertNoErr(t, err)
-	defer subnets.Delete(vpcClient, vpc.ID, subnet.ID)
-	th.AssertNoErr(t, waitForSubnetToActivate(vpcClient, subnet.ID, 60))
+	th.AssertEquals(t, getAddon.Spec.AddonTemplateName, "metrics-server")
+	th.AssertEquals(t, getAddon.Spec.Version, "1.0.3")
+}
 
-	cluster, err := clusters.Create(cceClient, clusters.CreateOpts{
+func (a *testAddons) TestListAddonTemplates() {
+	t := a.T()
+
+	client, err := clients.NewCceV3AddonClient()
+	th.AssertNoErr(t, err)
+
+	list, err := addons.ListTemplates(client, a.clusterID, nil).Extract()
+	th.AssertNoErr(t, err)
+
+	if len(list.Items) == 0 {
+		t.Fatal("empty addon template list")
+	}
+
+	jsonList, _ := json.MarshalIndent(list.Items, "", "  ")
+
+	t.Logf("existing addon templates:\n%s", string(jsonList))
+}
+
+func createCluster(t *testing.T, vpcID, subnetID string) string {
+	client, err := clients.NewCceV3Client()
+	th.AssertNoErr(t, err)
+
+	cluster, err := clusters.Create(client, clusters.CreateOpts{
 		Kind:       "Cluster",
 		ApiVersion: "v3",
 		Metadata: clusters.CreateMetaData{
@@ -54,8 +120,8 @@ func TestAddonsLifecycle(t *testing.T) {
 			Type:   "VirtualMachine",
 			Flavor: "cce.s1.small",
 			HostNetwork: clusters.HostNetworkSpec{
-				VpcId:    vpc.ID,
-				SubnetId: subnet.ID,
+				VpcId:    vpcID,
+				SubnetId: subnetID,
 			},
 			ContainerNetwork: clusters.ContainerNetworkSpec{
 				Mode: "overlay_l2",
@@ -68,65 +134,16 @@ func TestAddonsLifecycle(t *testing.T) {
 		},
 	}).Extract()
 	th.AssertNoErr(t, err)
-	clusterID := cluster.Metadata.Id
-	th.AssertNoErr(t, waitForClusterToActivate(cceClient, clusterID, 30*60))
-	defer clusters.Delete(cceClient, clusterID)
 
-	addon, err := addons.Create(addonClient, addons.CreateOpts{
-		Kind:       "Addon",
-		ApiVersion: "v3",
-		Metadata: addons.CreateMetadata{
-			Annotations: addons.CreateAnnotations{
-				AddonInstallType: "install",
-			},
-		},
-		Spec: addons.RequestSpec{
-			Version:           "1.0.3",
-			ClusterID:         clusterID,
-			AddonTemplateName: "metrics-server",
-			Values: addons.Values{
-				Basic: map[string]interface{}{
-					"euleros_version": "2.5",
-					"rbac_enabled":    true,
-					"swr_addr":        "100.125.7.25:20202",
-					"swr_user":        "hwofficial",
-				},
-			},
-		},
-	}, cluster.Metadata.Id).Extract()
-	th.AssertNoErr(t, err)
-
-	addonID := addon.Metadata.Id
-
-	defer func() {
-		err := addons.Delete(addonClient, addonID, clusterID).ExtractErr()
-		th.AssertNoErr(t, err)
-	}()
-
-	getAddon, err := addons.Get(addonClient, addon.Metadata.Id, cluster.Metadata.Id).Extract()
-	th.AssertNoErr(t, err)
-	th.AssertEquals(t, getAddon.Spec.AddonTemplateName, "metrics-server")
-	th.AssertEquals(t, getAddon.Spec.Version, "1.0.3")
+	th.AssertNoErr(t, waitForClusterToActivate(client, cluster.Metadata.Id, 30*60))
+	return cluster.Metadata.Id
 }
 
-func waitForSubnetToActivate(client *golangsdk.ServiceClient, subnetID string, secs int) error {
-	return golangsdk.WaitFor(secs, func() (bool, error) {
-		subnet, err := subnets.Get(client, subnetID).Extract()
-		if err != nil {
-			return false, err
-		}
-
-		if subnet.Status == "ACTIVE" {
-			return true, nil
-		}
-
-		// If subnet status is other than Active, send error
-		if subnet.Status == "DOWN" || subnet.Status == "ERROR" {
-			return false, fmt.Errorf("subnet status: '%s'", subnet.Status)
-		}
-
-		return false, nil
-	})
+func deleteCluster(t *testing.T, clusterID string) {
+	client, err := clients.NewCceV3Client()
+	th.AssertNoErr(t, err)
+	clusters.Delete(client, clusterID)
+	th.AssertNoErr(t, waitForClusterToDelete(client, clusterID, 20*60))
 }
 
 func waitForClusterToActivate(client *golangsdk.ServiceClient, id string, secs int) error {
@@ -135,8 +152,24 @@ func waitForClusterToActivate(client *golangsdk.ServiceClient, id string, secs i
 		if err != nil {
 			return false, err
 		}
+		if cluster == nil {
+			return false, nil
+		}
 		if cluster.Status.Phase == "Available" {
 			return true, nil
+		}
+		return false, nil
+	})
+}
+
+func waitForClusterToDelete(client *golangsdk.ServiceClient, id string, secs int) error {
+	return golangsdk.WaitFor(secs, func() (bool, error) {
+		_, err := clusters.Get(client, id).Extract()
+		if err != nil {
+			if _, ok := err.(golangsdk.ErrDefault404); ok {
+				return true, nil
+			}
+			return false, err
 		}
 		return false, nil
 	})
