9b01e1748058f327698f94e1d7fb7a5ac029ec54
Anton Kachurin <anton.kachurin@t-systems.com>
2021-03-29 16:07:34 +0300

    Add unit tests for `federation/providers`

--- openstack/identity/v3/federation/providers/testing/fixtures.go
+++ openstack/identity/v3/federation/providers/testing/fixtures.go
@@ -1,16 +1,21 @@
 package testing
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 const (
 	providerID          = "ACME"
 	providerDescription = "Stores ACME identities"
+
+	listURI = "/OS-FEDERATION/identity_providers"
 )
 
 var (
-	createURI = fmt.Sprintf("/OS-FEDERATION/identity_providers/%s", providerID)
+	providerURI = fmt.Sprintf("%s/%s", listURI, providerID)
 
-	jsonResponse = fmt.Sprintf(`
+	providerResponse = fmt.Sprintf(`
 {
     "identity_provider": {
         "description": "%s",
@@ -24,4 +29,38 @@ var (
     }
 }
 `, providerDescription, providerID)
+
+	providerListResponse = fmt.Sprintf(`
+{
+    "identity_providers": [
+        {
+            "description": "%s",
+            "enabled": true,
+            "id": "%s",
+            "remote_ids": [],
+            "links": {
+                "protocols": "https://example.com/v3/OS-FEDERATION/identity_providers/ACME/protocols",
+                "self": "https://example.com/v3/OS-FEDERATION/identity_providers/ACME"
+            }
+        },
+        {
+            "description": "Stores contractor identities",
+            "enabled": false,
+            "remote_ids": [],
+            "id": "ACME-contractors",
+            "links": {
+                "protocols": "https://example.com/v3/OS-FEDERATION/identity_providers/ACME-contractors/protocols",
+                "self": "https://example.com/v3/OS-FEDERATION/identity_providers/ACME-contractors"
+            }
+        }
+    ],
+    "links": {
+        "next": null,
+        "previous": null,
+        "self": "https://example.com/v3/OS-FEDERATION/identity_providers"
+    }
+}
+`, providerDescription, providerID)
+
+	updatedProviderResposnse = strings.ReplaceAll(providerResponse, "true", "false")
 )
--- openstack/identity/v3/federation/providers/testing/requests_test.go
+++ openstack/identity/v3/federation/providers/testing/requests_test.go
@@ -10,16 +10,17 @@ import (
 	fake "github.com/opentelekomcloud/gophertelekomcloud/testhelper/client"
 )
 
-func TestProviderCreateResponse(t *testing.T) {
+func TestProviderCreateRequest(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
 
-	th.Mux.HandleFunc(createURI, func(w http.ResponseWriter, r *http.Request) {
+	th.Mux.HandleFunc(providerURI, func(w http.ResponseWriter, r *http.Request) {
 		th.TestMethod(t, r, "PUT")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
 		th.TestHeader(t, r, "Content-Type", "application/json")
 		th.TestHeader(t, r, "Accept", "application/json")
 		w.WriteHeader(201)
-		_, _ = fmt.Fprintf(w, jsonResponse)
+		_, _ = fmt.Fprintf(w, providerResponse)
 	})
 
 	opts := providers.CreateOpts{
@@ -32,4 +33,77 @@ func TestProviderCreateResponse(t *testing.T) {
 	th.AssertEquals(t, opts.ID, p.ID)
 	th.AssertEquals(t, opts.Enabled, p.Enabled)
 	th.AssertEquals(t, 0, len(p.RemoteIDs))
+}
+
+func TestProviderGetRequest(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc(providerURI, func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		th.TestHeader(t, r, "Accept", "application/json")
+		_, _ = fmt.Fprintf(w, providerResponse)
+	})
+
+	p, err := providers.Get(fake.ServiceClient(), providerID).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, true, p.Enabled)
+	th.AssertEquals(t, providerDescription, p.Description)
+}
+
+func TestProviderListRequest(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc(listURI, func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		_, _ = fmt.Fprintf(w, providerListResponse)
+	})
+
+	pages, err := providers.List(fake.ServiceClient()).AllPages()
+	th.AssertNoErr(t, err)
+
+	providerList, err := providers.ExtractProviders(pages)
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, 2, len(providerList))
+}
+
+func TestProviderUpdateRequest(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc(providerURI, func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "PATCH")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		_, _ = fmt.Fprintf(w, updatedProviderResposnse)
+	})
+
+	iFalse := false
+	opts := providers.UpdateOpts{
+		Enabled: &iFalse,
+	}
+	p, err := providers.Update(fake.ServiceClient(), providerID, opts).Extract()
+
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, iFalse, p.Enabled)
+}
+
+func TestProviderDeleteRequest(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc(providerURI, func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "DELETE")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		w.WriteHeader(204)
+	})
+
+	err := providers.Delete(fake.ServiceClient(), providerID).ExtractErr()
+	th.AssertNoErr(t, err)
 }
