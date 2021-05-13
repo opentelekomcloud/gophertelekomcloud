aa4509827820b740b8c7064bd5a3b6656882e0f2
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-05-13 18:58:45 +0300

    [ECS] Add possibility to query NICs info (#148)
    [ECS] Add possibility to query NICs info
    
    What this PR does / why we need it
    Add possibility to query info about NICs
    Which issue this PR fixes
    Fixes: #149
    Refers to: opentelekomcloud/terraform-provider-opentelekomcloud#1039
    Acceptance tests
    === RUN   TestServerList
    --- PASS: TestServerList (2.67s)
    === RUN   TestServerLifecycle
        servers_test.go:37: Attempting to create ECSv2
        servers_test.go:76: Created ECSv2: 4ba882dd-21fc-4755-b79b-24f5b365485c
        servers_test.go:108: Attempting to update ECSv2: 4ba882dd-21fc-4755-b79b-24f5b365485c
        servers_test.go:118: ECSv2 successfully updated: 4ba882dd-21fc-4755-b79b-24f5b365485c
        servers_test.go:87: Attempting to delete ECSv2: 4ba882dd-21fc-4755-b79b-24f5b365485c
        servers_test.go:105: ECSv2 instance deleted: 4ba882dd-21fc-4755-b79b-24f5b365485c
    --- PASS: TestServerLifecycle (149.57s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: None <None>
    Reviewed-by: Anton Sidelnikov <None>

---
+++ acceptance/openstack/compute/v2/servers_test.go
@@ -0,0 +1,124 @@
+package v2
+
+import (
+	"testing"
+	"time"
+
+	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/openstack"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/compute/v2/flavors"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/compute/v2/images"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/compute/v2/servers"
+	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
+)
+
+func TestServerList(t *testing.T) {
+	client, err := clients.NewComputeV2Client()
+	th.AssertNoErr(t, err)
+
+	listOpts := servers.ListOpts{}
+	allServerPages, err := servers.List(client, listOpts).AllPages()
+	th.AssertNoErr(t, err)
+
+	serversList, err := servers.ExtractServers(allServerPages)
+	th.AssertNoErr(t, err)
+
+	for _, server := range serversList {
+		tools.PrintResource(t, server)
+	}
+}
+
+func TestServerLifecycle(t *testing.T) {
+	client, err := clients.NewComputeV2Client()
+	th.AssertNoErr(t, err)
+
+	t.Logf("Attempting to create ECSv2")
+	ecsName := tools.RandomString("create-ecs-", 3)
+
+	az := clients.EnvOS.GetEnv("AVAILABILITY_ZONE")
+	if az == "" {
+		az = "eu-de-01"
+	}
+
+	networkID := clients.EnvOS.GetEnv("NETWORK_ID")
+	if networkID == "" {
+		t.Skip("OS_NETWORK_ID env var is missing but ECS test requires using existing network")
+	}
+
+	imageID, err := images.IDFromName(client, "Standard_Debian_10_latest")
+	th.AssertNoErr(t, err)
+
+	flavorID, err := flavors.IDFromName(client, "s2.large.2")
+	th.AssertNoErr(t, err)
+
+	createOpts := servers.CreateOpts{
+		Name:      ecsName,
+		ImageRef:  imageID,
+		FlavorRef: flavorID,
+		SecurityGroups: []string{
+			openstack.DefaultSecurityGroup(t),
+		},
+		AvailabilityZone: az,
+		Networks: []servers.Network{
+			{
+				UUID: networkID,
+			},
+		},
+	}
+
+	ecs, err := servers.Create(client, createOpts).Extract()
+	th.AssertNoErr(t, err)
+
+	err = servers.WaitForStatus(client, ecs.ID, "ACTIVE", 1200)
+	th.AssertNoErr(t, err)
+	t.Logf("Created ECSv2: %s", ecs.ID)
+
+	ecs, err = servers.Get(client, ecs.ID).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, ecsName, ecs.Name)
+
+	nicInfo, err := servers.GetNICs(client, ecs.ID).Extract()
+	th.AssertNoErr(t, err)
+	tools.PrintResource(t, nicInfo)
+
+	defer func() {
+		t.Logf("Attempting to delete ECSv2: %s", ecs.ID)
+
+		_, err := servers.Delete(client, ecs.ID).ExtractJobResponse()
+		th.AssertNoErr(t, err)
+
+		err = golangsdk.WaitFor(1200, func() (bool, error) {
+			_, err := servers.Get(client, ecs.ID).Extract()
+			if err != nil {
+				if _, ok := err.(golangsdk.ErrDefault400); ok {
+					time.Sleep(10 * time.Second)
+					return false, nil
+				}
+				return false, err
+			}
+			return true, nil
+		})
+		th.AssertNoErr(t, err)
+
+		t.Logf("ECSv2 instance deleted: %s", ecs.ID)
+	}()
+
+	t.Logf("Attempting to update ECSv2: %s", ecs.ID)
+
+	ecsName = tools.RandomString("update-ecs-", 3)
+	updateOpts := servers.UpdateOpts{
+		Name: ecsName,
+	}
+
+	_, err = servers.Update(client, ecs.ID, updateOpts).Extract()
+	th.AssertNoErr(t, err)
+
+	t.Logf("ECSv2 successfully updated: %s", ecs.ID)
+	th.AssertNoErr(t, err)
+
+	newECS, err := servers.Get(client, ecs.ID).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, ecsName, newECS.Name)
+}
--- openstack/compute/v2/servers/urls.go
+++ openstack/compute/v2/servers/urls.go
@@ -2,8 +2,18 @@ package servers
 
 import "github.com/opentelekomcloud/gophertelekomcloud"
 
+const (
+	rootPath     = "servers"
+	nicPath      = "os-interface"
+	detailPath   = "detail"
+	actionPath   = "action"
+	metadataPath = "metadata"
+	ipsPath      = "ips"
+	passwordPath = "os-server-password"
+)
+
 func createURL(client *golangsdk.ServiceClient) string {
-	return client.ServiceURL("servers")
+	return client.ServiceURL(rootPath)
 }
 
 func listURL(client *golangsdk.ServiceClient) string {
@@ -11,11 +21,11 @@ func listURL(client *golangsdk.ServiceClient) string {
 }
 
 func listDetailURL(client *golangsdk.ServiceClient) string {
-	return client.ServiceURL("servers", "detail")
+	return client.ServiceURL(rootPath, detailPath)
 }
 
 func deleteURL(client *golangsdk.ServiceClient, id string) string {
-	return client.ServiceURL("servers", id)
+	return client.ServiceURL(rootPath, id)
 }
 
 func getURL(client *golangsdk.ServiceClient, id string) string {
@@ -27,25 +37,29 @@ func updateURL(client *golangsdk.ServiceClient, id string) string {
 }
 
 func actionURL(client *golangsdk.ServiceClient, id string) string {
-	return client.ServiceURL("servers", id, "action")
+	return client.ServiceURL(rootPath, id, actionPath)
 }
 
 func metadatumURL(client *golangsdk.ServiceClient, id, key string) string {
-	return client.ServiceURL("servers", id, "metadata", key)
+	return client.ServiceURL(rootPath, id, metadataPath, key)
 }
 
 func metadataURL(client *golangsdk.ServiceClient, id string) string {
-	return client.ServiceURL("servers", id, "metadata")
+	return client.ServiceURL(rootPath, id, metadataPath)
 }
 
 func listAddressesURL(client *golangsdk.ServiceClient, id string) string {
-	return client.ServiceURL("servers", id, "ips")
+	return client.ServiceURL(rootPath, id, ipsPath)
 }
 
 func listAddressesByNetworkURL(client *golangsdk.ServiceClient, id, network string) string {
-	return client.ServiceURL("servers", id, "ips", network)
+	return client.ServiceURL(rootPath, id, ipsPath, network)
 }
 
 func passwordURL(client *golangsdk.ServiceClient, id string) string {
-	return client.ServiceURL("servers", id, "os-server-password")
+	return client.ServiceURL(rootPath, id, passwordPath)
+}
+
+func getNICManagementURL(client *golangsdk.ServiceClient, id string) string {
+	return client.ServiceURL(rootPath, id, nicPath)
 }
