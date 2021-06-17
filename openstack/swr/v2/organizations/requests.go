0b0ef06baa366aee4ec49667965023eec4bab220
Anton Kachurin <anton.kachurin@t-systems.com>
2021-06-17 12:46:09 +0300

    Implement organization permissions management (#183)
    [SWR] Implement organization permissions
    
    What this PR does / why we need it
    Add methods for organization permissions into organizations pkg
    Which issue this PR fixes
    
    Part of #168
    Closes #183
    Special notes for your reviewer
    Acceptance (requires additional info)
    === RUN   TestOrganizationPermissionsWorkflow
    --- PASS: TestOrganizationPermissionsWorkflow (3.77s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: Anton Sidelnikov <None>
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>
    Reviewed-by: None <None>

--- openstack/swr/v2/organizations/requests.go
+++ openstack/swr/v2/organizations/requests.go
@@ -67,3 +67,59 @@ func Get(client *golangsdk.ServiceClient, id string) (r GetResult) {
 	_, r.Err = client.Get(organizationURL(client, id), &r.Body, nil)
 	return
 }
+
+type CreatePermissionsOptsBuilder interface {
+	ToPermissionCreateMap() (map[string]interface{}, error)
+}
+
+type CreatePermissionsOpts Auth
+
+func (opts CreatePermissionsOpts) ToPermissionCreateMap() (map[string]interface{}, error) {
+	return golangsdk.BuildRequestBody(opts, "")
+}
+
+func CreatePermissions(client *golangsdk.ServiceClient, organization string, opts CreatePermissionsOptsBuilder) (r CreatePermissionsResult) {
+	b, err := opts.ToPermissionCreateMap()
+	if err != nil {
+		r.Err = err
+		return
+	}
+	realBody := []interface{}{b}
+	_, r.Err = client.Post(permissionsURL(client, organization), realBody, &r.Body, nil)
+	return
+}
+
+func DeletePermissions(client *golangsdk.ServiceClient, organization string, userID string) (r DeletePermissionsResult) {
+	_, r.Err = client.Request("DELETE", permissionsURL(client, organization), &golangsdk.RequestOpts{
+		JSONBody: []interface{}{userID},
+	})
+	return
+}
+
+type UpdatePermissionsOptsBuilder interface {
+	ToPermissionUpdateMap() (map[string]interface{}, error)
+}
+
+type UpdatePermissionsOpts Auth
+
+func (opts UpdatePermissionsOpts) ToPermissionUpdateMap() (map[string]interface{}, error) {
+	return golangsdk.BuildRequestBody(opts, "")
+}
+
+func UpdatePermissions(client *golangsdk.ServiceClient, organization string, opts UpdatePermissionsOptsBuilder) (r UpdatePermissionsResult) {
+	b, err := opts.ToPermissionUpdateMap()
+	if err != nil {
+		r.Err = err
+		return
+	}
+	realBody := []interface{}{b}
+	_, r.Err = client.Patch(permissionsURL(client, organization), realBody, &r.Body, &golangsdk.RequestOpts{
+		OkCodes: []int{201},
+	})
+	return
+}
+
+func GetPermissions(client *golangsdk.ServiceClient, organization string) (r GetPermissionsResult) {
+	_, r.Err = client.Get(permissionsURL(client, organization), &r.Body, nil)
+	return
+}
--- openstack/swr/v2/organizations/results.go
+++ openstack/swr/v2/organizations/results.go
@@ -44,3 +44,45 @@ func (r GetResult) Extract() (*Organization, error) {
 	err := r.ExtractIntoStructPtr(org, "")
 	return org, err
 }
+
+type CreatePermissionsResult struct {
+	golangsdk.ErrResult
+}
+
+type DeletePermissionsResult struct {
+	golangsdk.ErrResult
+}
+
+type UpdatePermissionsResult struct {
+	golangsdk.ErrResult
+}
+
+type Auth struct {
+	// User ID, which needs to be obtained from the IAM service.
+	UserID string `json:"user_id"`
+	// Username, which needs to be obtained from the IAM service.
+	Username string `json:"user_name"`
+	// User permission that is configured. The value can be 1, 3, or 7. 7: manage 3: write 1: read
+	Auth int `json:"auth"`
+}
+
+type OrganizationPermissions struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	CreatorName string `json:"creator_name"`
+	SelfAuth    Auth   `json:"self_auth"`
+	OthersAuth  []Auth `json:"others_auths"`
+}
+
+type GetPermissionsResult struct {
+	golangsdk.Result
+}
+
+func (r GetPermissionsResult) Extract() (*OrganizationPermissions, error) {
+	perm := new(OrganizationPermissions)
+	err := r.ExtractIntoStructPtr(perm, "")
+	if err != nil {
+		return nil, err
+	}
+	return perm, nil
+}
