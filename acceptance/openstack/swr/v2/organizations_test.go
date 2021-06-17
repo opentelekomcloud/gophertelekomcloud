ee2576c98a666936d19aada556fc44707e33ac05
Anton Kachurin <anton.kachurin@t-systems.com>
2021-06-17 12:28:21 +0300

    Implement organization permissions management
    Add methods for organization permissions into `organizations` pkg

--- acceptance/openstack/swr/v2/organizations_test.go
+++ acceptance/openstack/swr/v2/organizations_test.go
@@ -1,9 +1,12 @@
 package v2
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/swr/v2/organizations"
 	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
 )
@@ -37,5 +40,91 @@ func TestOrganizationWorkflow(t *testing.T) {
 	}
 	if !found {
 		t.Fatalf("can't find organization '%s' in the list", name)
+	}
+}
+
+func TestOrganizationPermissionsWorkflow(t *testing.T) {
+	userID := os.Getenv("OS_USER_ID_2")
+	username := os.Getenv("OS_USERNAME_2")
+
+	if username == "" || userID == "" {
+		t.Skip("OS_USER_ID_2 and OS_USERNAME_2 should be set to test permission granting")
+	}
+
+	client, err := clients.NewSwrV2Client()
+	th.AssertNoErr(t, err)
+
+	// setup org
+	orgName := fmt.Sprintf("repo-test-%d", tools.RandomInt(0, 0xf))
+	dep := dependencies{t: t, client: client}
+	dep.createOrganization(orgName)
+	defer dep.deleteOrganization(orgName)
+	//
+
+	auth := organizations.Auth{
+		UserID:   userID,
+		Username: username,
+		Auth:     3,
+	}
+
+	err = organizations.CreatePermissions(client, orgName, organizations.CreatePermissionsOpts(auth)).ExtractErr()
+	th.AssertNoErr(t, err)
+
+	defer func() {
+		err := organizations.DeletePermissions(client, orgName, auth.UserID).ExtractErr()
+		th.AssertNoErr(t, err)
+		perms, err := organizations.GetPermissions(client, orgName).Extract()
+		th.AssertNoErr(t, err)
+		assertAuthNotInPermissions(t, auth, perms)
+	}()
+
+	perms, err := organizations.GetPermissions(client, orgName).Extract()
+	th.AssertNoErr(t, err)
+	assertAuthInPermissions(t, auth, perms)
+
+	newAuth := organizations.Auth{
+		UserID:   auth.UserID,
+		Username: auth.Username,
+		Auth:     1,
+	}
+	err = organizations.UpdatePermissions(client, orgName, organizations.UpdatePermissionsOpts(newAuth)).ExtractErr()
+	th.AssertNoErr(t, err)
+
+	updatedPerms, err := organizations.GetPermissions(client, orgName).Extract()
+	th.AssertNoErr(t, err)
+	assertAuthInPermissions(t, newAuth, updatedPerms)
+}
+
+func assertAuthInPermissions(t *testing.T, expected organizations.Auth, actual *organizations.OrganizationPermissions) {
+	if actual == nil {
+		t.Fatal("actual organization permissions are nil")
+	}
+
+	for _, a := range actual.OthersAuth {
+		if a.UserID == expected.UserID {
+			if a.Username != expected.Username {
+				t.Fatalf(
+					"user ID is the same, but username differ - this is unexpected (%s/%s instead of %[1]s/%s)",
+					a.UserID, a.Username, expected.Username,
+				)
+			}
+			if a.Auth != expected.Auth {
+				t.Fatalf("auth was %d, but %d expected", a.Auth, expected.Auth)
+			}
+			return
+		}
+	}
+	t.Fatal("expected permission is not found in the `others_auth` list")
+}
+
+func assertAuthNotInPermissions(t *testing.T, expected organizations.Auth, actual *organizations.OrganizationPermissions) {
+	if actual == nil {
+		t.Fatal("actual organization permissions are nil")
+	}
+
+	for _, a := range actual.OthersAuth {
+		if a.UserID == expected.UserID {
+			t.Fatalf("expected permission to be deleted, but it exist")
+		}
 	}
 }
