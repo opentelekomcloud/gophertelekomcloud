6b300ac789d803239bd398612971fcf886e13fc9
Anton Kachurin <anton.kachurin@t-systems.com>
2021-07-01 16:13:04 +0300

    Fix css snapshot `Disable` function

--- openstack/css/v1/snapshots/requests.go
+++ openstack/css/v1/snapshots/requests.go
@@ -57,7 +57,12 @@ func Enable(client *golangsdk.ServiceClient, clusterId string) (r ErrorResult) {
 
 // Disable will disable the Snapshot function with the provided ID.
 func Disable(client *golangsdk.ServiceClient, clusterId string) (r ErrorResult) {
-	_, r.Err = client.Delete(disableURL(client, clusterId), nil)
+	_, r.Err = client.Delete(disableURL(client, clusterId), &golangsdk.RequestOpts{
+		OkCodes: []int{200},
+		MoreHeaders: map[string]string{
+			"content-type": "application/json",
+		},
+	})
 	return
 }
 
