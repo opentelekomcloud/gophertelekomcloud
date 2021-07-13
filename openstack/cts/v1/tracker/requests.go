2602008425a4131b25084e5e26606cf7a7d84c04
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-07-13 14:12:36 +0300

    Revert required because it produces issues in unit tests

--- openstack/cts/v1/tracker/requests.go
+++ openstack/cts/v1/tracker/requests.go
@@ -96,10 +96,10 @@ type CreateOpts struct {
 }
 
 type SimpleMessageNotification struct {
-	IsSupportSMN          bool     `json:"is_support_smn" required:"true"`
+	IsSupportSMN          bool     `json:"is_support_smn"`
 	TopicID               string   `json:"topic_id,omitempty"`
 	Operations            []string `json:"operations,omitempty"`
-	IsSendAllKeyOperation bool     `json:"is_send_all_key_operation" required:"true"`
+	IsSendAllKeyOperation bool     `json:"is_send_all_key_operation"`
 	NeedNotifyUserList    []string `json:"need_notify_user_list,omitempty"`
 }
 
