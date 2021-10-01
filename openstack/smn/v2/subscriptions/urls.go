a91484a28b6468e85a128f6eafa4bde1ebeea532
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-10-01 16:36:16 +0300

    Refactoring subscriptions pkg

--- openstack/smn/v2/subscriptions/urls.go
+++ openstack/smn/v2/subscriptions/urls.go
@@ -2,18 +2,23 @@ package subscriptions
 
 import "github.com/opentelekomcloud/gophertelekomcloud"
 
+const (
+	rootPath   = "subscriptions"
+	topicsPath = "topics"
+)
+
 func createURL(c *golangsdk.ServiceClient, topicUrn string) string {
-	return c.ServiceURL("topics", topicUrn, "subscriptions")
+	return c.ServiceURL(topicsPath, topicUrn, rootPath)
 }
 
 func deleteURL(c *golangsdk.ServiceClient, subscriptionUrn string) string {
-	return c.ServiceURL("subscriptions", subscriptionUrn)
+	return c.ServiceURL(rootPath, subscriptionUrn)
 }
 
 func listURL(c *golangsdk.ServiceClient) string {
-	return c.ServiceURL("subscriptions?offset=0&limit=100")
+	return c.ServiceURL(rootPath)
 }
 
 func listFromTopicURL(c *golangsdk.ServiceClient, topicUrn string) string {
-	return c.ServiceURL("topics", topicUrn, "subscriptions?offset=0&limit=100")
+	return c.ServiceURL(topicsPath, topicUrn, rootPath)
 }
