629ca183a9b2e909f81c9d50a05cf99948e78912
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-10-07 10:26:16 +0300

    Add acceptance test

---
+++ acceptance/openstack/elb/v3/flavors_test.go
@@ -0,0 +1,26 @@
+package v3
+
+import (
+	"testing"
+
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/elb/v3/flavors"
+	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
+)
+
+func TestFlavorsList(t *testing.T) {
+	client, err := clients.NewElbV3Client(t)
+	th.AssertNoErr(t, err)
+
+	listOpts := flavors.ListOpts{}
+	flavorsPages, err := flavors.List(client, listOpts).AllPages()
+	th.AssertNoErr(t, err)
+
+	flavorsList, err := flavors.ExtractFlavors(flavorsPages)
+	th.AssertNoErr(t, err)
+
+	for _, flavor := range flavorsList {
+		tools.PrintResource(t, flavor)
+	}
+}
