ee34d73b26dc6f12061a84cbe8cbf058848f1e5e
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-10-08 16:49:28 +0300

    format

--- openstack/elb/v3/loadbalancers/requests.go
+++ openstack/elb/v3/loadbalancers/requests.go
@@ -47,7 +47,7 @@ func List(client *golangsdk.ServiceClient, opts ListOptsBuilder) pagination.Page
 		url += query
 	}
 	return pagination.NewPager(client, url, func(r pagination.PageResult) pagination.Page {
-		p:=LoadbalancerPage{pagination.MarkerPageBase{PageResult: r}}
+		p := LoadbalancerPage{pagination.MarkerPageBase{PageResult: r}}
 		p.MarkerPageBase.Owner = p
 		return p
 	})
