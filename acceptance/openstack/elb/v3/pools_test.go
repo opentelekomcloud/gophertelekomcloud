0375fe61dd26abfe6bcba56dfff14de1bcf29e88
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-10-15 18:29:21 +0300

    [ELB] Implement `pools` v3 pkg (#259)
    [ELB] Implement `pools` v3 pkg
    
    What this PR does / why we need it
    Implement Dedicated Load Balancer pools pkg
    Which issue this PR fixes
    Refers to: #248
    Special notes for your reviewer
    === RUN   TestPoolList
    --- PASS: TestPoolList (2.15s)
    === RUN   TestPoolLifecycle
        helpers.go:16: Attempting to create ELBv3 LoadBalancer
        helpers.go:61: Created ELBv3 LoadBalancer: 18ee6b88-6768-4786-b40d-b4a203cbe6e4
        pools_test.go:35: Attempting to create ELBv3 Pool
        pools_test.go:47: Created ELBv3 Pool: 25f0f796-2168-4f60-b239-85ab466d0f39
        pools_test.go:60: Attempting to update ELBv3 Pool: 25f0f796-2168-4f60-b239-85ab466d0f39
        pools_test.go:70: Updated ELBv3 Pool: 25f0f796-2168-4f60-b239-85ab466d0f39
        pools_test.go:50: Attempting to delete ELBv3 Pool: 25f0f796-2168-4f60-b239-85ab466d0f39
        pools_test.go:53: Deleted ELBv3 Pool: 25f0f796-2168-4f60-b239-85ab466d0f39
        helpers.go:67: Attempting to delete ELBv3 LoadBalancer: 18ee6b88-6768-4786-b40d-b4a203cbe6e4
        helpers.go:70: Deleted ELBv3 LoadBalancer: 18ee6b88-6768-4786-b40d-b4a203cbe6e4
    --- PASS: TestPoolLifecycle (11.23s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: None <None>
    Reviewed-by: Anton Kachurin <katchuring@gmail.com>
    Reviewed-by: Anton Sidelnikov <None>

---
+++ acceptance/openstack/elb/v3/pools_test.go
@@ -0,0 +1,77 @@
+package v3
+
+import (
+	"testing"
+
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/elb/v3/pools"
+	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
+)
+
+func TestPoolList(t *testing.T) {
+	client, err := clients.NewElbV3Client(t)
+	th.AssertNoErr(t, err)
+
+	listOpts := pools.ListOpts{}
+	poolPages, err := pools.List(client, listOpts).AllPages()
+	th.AssertNoErr(t, err)
+
+	poolList, err := pools.ExtractPools(poolPages)
+	th.AssertNoErr(t, err)
+
+	for _, pool := range poolList {
+		tools.PrintResource(t, pool)
+	}
+}
+
+func TestPoolLifecycle(t *testing.T) {
+	client, err := clients.NewElbV3Client(t)
+	th.AssertNoErr(t, err)
+
+	loadbalancerID := createLoadBalancer(t, client)
+	defer deleteLoadbalancer(t, client, loadbalancerID)
+
+	t.Logf("Attempting to create ELBv3 Pool")
+	poolName := tools.RandomString("create-pool-", 3)
+	createOpts := pools.CreateOpts{
+		LBMethod:       "LEAST_CONNECTIONS",
+		Protocol:       "HTTPS",
+		LoadbalancerID: loadbalancerID,
+		Name:           poolName,
+		Description:    "some interesting description",
+	}
+
+	pool, err := pools.Create(client, createOpts).Extract()
+	th.AssertNoErr(t, err)
+	t.Logf("Created ELBv3 Pool: %s", pool.ID)
+
+	defer func() {
+		t.Logf("Attempting to delete ELBv3 Pool: %s", pool.ID)
+		err := pools.Delete(client, pool.ID).ExtractErr()
+		th.AssertNoErr(t, err)
+		t.Logf("Deleted ELBv3 Pool: %s", pool.ID)
+	}()
+
+	th.AssertEquals(t, createOpts.Name, pool.Name)
+	th.AssertEquals(t, createOpts.Description, pool.Description)
+	th.AssertEquals(t, createOpts.LBMethod, pool.LBMethod)
+
+	t.Logf("Attempting to update ELBv3 Pool: %s", pool.ID)
+	poolName = tools.RandomString("update-pool-", 3)
+	emptyDescription := ""
+	updateOpts := pools.UpdateOpts{
+		Name:        poolName,
+		Description: &emptyDescription,
+		LBMethod:    "ROUND_ROBIN",
+	}
+	_, err = pools.Update(client, pool.ID, updateOpts).Extract()
+	th.AssertNoErr(t, err)
+	t.Logf("Updated ELBv3 Pool: %s", pool.ID)
+
+	newPool, err := pools.Get(client, pool.ID).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, updateOpts.Name, newPool.Name)
+	th.AssertEquals(t, emptyDescription, newPool.Description)
+	th.AssertEquals(t, updateOpts.LBMethod, newPool.LBMethod)
+}
---
+++ openstack/elb/v3/pools/results.go
@@ -0,0 +1,142 @@
+package pools
+
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/pagination"
+)
+
+// LoadBalancerID represents a load balancer.
+type LoadBalancerID struct {
+	ID string `json:"id"`
+}
+
+// ListenerID represents a listener.
+type ListenerID struct {
+	ID string `json:"id"`
+}
+
+// MemberID represents a member.
+type MemberID struct {
+	ID string `json:"id"`
+}
+
+// Pool represents a logical set of devices, such as web servers, that you
+// group together to receive and process traffic. The load balancing function
+// chooses a Member of the Pool according to the configured load balancing
+// method to handle the new requests or connections received on the VIP address.
+type Pool struct {
+	// The load-balancer algorithm, which is round-robin, least-connections, and
+	// so on. This value, which must be supported, is dependent on the provider.
+	// Round-robin must be supported.
+	LBMethod string `json:"lb_algorithm"`
+
+	// The protocol of the Pool, which is TCP, HTTP, or HTTPS.
+	Protocol string `json:"protocol"`
+
+	// Description for the Pool.
+	Description string `json:"description"`
+
+	// A list of listeners objects IDs.
+	Listeners []ListenerID `json:"listeners"`
+
+	// A list of member objects IDs.
+	Members []MemberID `json:"members"`
+
+	// The ID of associated health monitor.
+	MonitorID string `json:"healthmonitor_id"`
+
+	// The administrative state of the Pool, which is up (true) or down (false).
+	AdminStateUp bool `json:"admin_state_up"`
+
+	// Pool name. Does not have to be unique.
+	Name string `json:"name"`
+
+	ProjectID string `json:"project_id"`
+
+	// The unique ID for the Pool.
+	ID string `json:"id"`
+
+	// A list of load balancer objects IDs.
+	Loadbalancers []LoadBalancerID `json:"loadbalancers"`
+
+	// Indicates whether connections in the same session will be processed by the
+	// same Pool member or not.
+	Persistence SessionPersistence `json:"session_persistence"`
+
+	IpVersion string `json:"ip_version"`
+
+	SlowStart SlowStart `json:"slow_start"`
+}
+
+// PoolPage is the page returned by a pager when traversing over a
+// collection of pools.
+type PoolPage struct {
+	pagination.MarkerPageBase
+}
+
+func (r PoolPage) LastMarker() (string, error) {
+	results, err := ExtractPools(r)
+	if err != nil {
+		return "", err
+	}
+	if len(results) == 0 {
+		return "", nil
+	}
+	return results[len(results)-1].ID, nil
+}
+
+// IsEmpty checks whether a PoolPage struct is empty.
+func (r PoolPage) IsEmpty() (bool, error) {
+	is, err := ExtractPools(r)
+	return len(is) == 0, err
+}
+
+// ExtractPools accepts a Page struct, specifically a PoolPage struct,
+// and extracts the elements into a slice of Pool structs. In other words,
+// a generic collection is mapped into a relevant slice.
+func ExtractPools(r pagination.Page) ([]Pool, error) {
+	var s []Pool
+	err := (r.(PoolPage)).ExtractIntoSlicePtr(&s, "pools")
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+type commonResult struct {
+	golangsdk.Result
+}
+
+// Extract is a function that accepts a result and extracts a pool.
+func (r commonResult) Extract() (*Pool, error) {
+	s := new(Pool)
+	err := r.ExtractIntoStructPtr(s, "pool")
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+// CreateResult represents the result of a Create operation. Call its Extract
+// method to interpret the result as a Pool.
+type CreateResult struct {
+	commonResult
+}
+
+// GetResult represents the result of a Get operation. Call its Extract
+// method to interpret the result as a Pool.
+type GetResult struct {
+	commonResult
+}
+
+// UpdateResult represents the result of an Update operation. Call its Extract
+// method to interpret the result as a Pool.
+type UpdateResult struct {
+	commonResult
+}
+
+// DeleteResult represents the result of a Delete operation. Call its
+// ExtractErr method to determine if the request succeeded or failed.
+type DeleteResult struct {
+	golangsdk.ErrResult
+}
