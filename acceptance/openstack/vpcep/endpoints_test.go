b2ccdc6a2fcfaf935ac7c690afd67aa5b9a500b5
Anton Kachurin <anton.kachurin@t-systems.com>
2021-10-18 13:35:56 +0300

    [VPCEP] Add `endpoints` support (#261)
    [VPCEP] Add `endpoints` support
    
    What this PR does / why we need it
    Implement methods for handling VPC EP endpoints
    Which issue this PR fixes
    
    Refers to #257
    Special notes for your reviewer
    === RUN   TestEndpointLifecycle
    --- PASS: TestEndpointLifecycle (14.06s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: None <None>
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>
    Reviewed-by: Anton Sidelnikov <None>

---
+++ acceptance/openstack/vpcep/endpoints_test.go
@@ -0,0 +1,84 @@
+package vpcep
+
+import (
+	"testing"
+
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/tags"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/vpcep/v1/endpoints"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/vpcep/v1/services"
+	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
+)
+
+func createService(t *testing.T, client *golangsdk.ServiceClient, elbPortID string) string {
+	iFalse := false
+	createOpts := &services.CreateOpts{
+		PortID:      elbPortID,
+		ServiceName: tools.RandomString("svc-", 5),
+		RouterID:    routerID,
+		ServerType:  services.ServerTypeLB,
+		ServiceType: services.ServiceTypeInterface,
+		Ports: []services.PortMapping{
+			{
+				ClientPort: 80,
+				ServerPort: 8080,
+			},
+		},
+		ApprovalEnabled: &iFalse,
+	}
+	svc, err := services.Create(client, createOpts).Extract()
+	th.AssertNoErr(t, err)
+
+	err = services.WaitForServiceStatus(client, svc.ID, services.StatusAvailable, 30)
+	th.AssertNoErr(t, err)
+	return svc.ID
+}
+
+func deleteService(t *testing.T, client *golangsdk.ServiceClient, id string) {
+	th.AssertNoErr(t, services.Delete(client, id).ExtractErr())
+}
+
+func TestEndpointLifecycle(t *testing.T) {
+	if routerID == "" || networkID == "" || subnetID == "" {
+		t.Skip("OS_ROUTER_ID/VPC_ID, OS_SUBNET_ID and OS_NETWORK_ID variables need to be set")
+	}
+
+	client, err := clients.NewVPCEndpointV1Client()
+	th.AssertNoErr(t, err)
+
+	elb := createELB(t)
+	defer deleteELB(t, elb.ID)
+
+	srvID := createService(t, client, elb.VipPortID)
+	defer deleteService(t, client, srvID)
+
+	opts := endpoints.CreateOpts{
+		NetworkID: networkID,
+		ServiceID: srvID,
+		RouterID:  routerID,
+		EnableDNS: true,
+		Tags:      []tags.ResourceTag{{Key: "fizz", Value: "buzz"}},
+	}
+	created, err := endpoints.Create(client, opts).Extract()
+	th.AssertNoErr(t, err)
+
+	defer func() {
+		th.AssertNoErr(t, endpoints.Delete(client, created.ID).ExtractErr())
+	}()
+
+	got, err := endpoints.Get(client, created.ID).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, endpoints.StatusCreating, got.Status)
+
+	th.AssertNoErr(t, endpoints.WaitForEndpointStatus(client, created.ID, endpoints.StatusAccepted, 30))
+
+	pages, err := endpoints.List(client, nil).AllPages()
+	th.AssertNoErr(t, err)
+
+	eps, err := endpoints.ExtractEndpoints(pages)
+	th.AssertNoErr(t, err)
+
+	th.AssertEquals(t, created.ID, eps[0].ID)
+}
---
+++ openstack/vpcep/v1/endpoints/testing/fixtures.go
@@ -0,0 +1,122 @@
+package testing
+
+import (
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/tags"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/vpcep/v1/endpoints"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/vpcep/v1/services"
+)
+
+const (
+	createRequest = `
+{
+  "subnet_id": "68bfbcc1-dff2-47e4-a9d4-332b9bc1b8de",
+  "vpc_id": "84758cf5-9c62-43ae-a778-3dbd8370c0a4",
+  "tags": [
+    {
+      "key": "test1",
+      "value": "test1"
+    }
+  ],
+  "endpoint_service_id": "e0c748b7-d982-47df-ba06-b9c8c7650c1a",
+  "enable_dns": true
+}
+`
+	createResponse = `
+{
+  "id": "4189d3c2-8882-4871-a3c2-d380272eed83",
+  "service_type": "interface",
+  "marker_id": 322312312312,
+  "status": "creating",
+  "vpc_id": "84758cf5-9c62-43ae-a778-3dbd8370c0a4",
+  "enable_dns": true,
+  "endpoint_service_name": "test123",
+  "endpoint_service_id": "e0c748b7-d982-47df-ba06-b9c8c7650c1a",
+  "project_id": "6e9dfd51d1124e8d8498dce894923a0d",
+  "created_at": "2018-01-30T07:42:01.174",
+  "updated_at": "2018-01-30T07:42:01.174",
+  "tags": [
+    {
+      "key": "test1",
+      "value": "test1"
+    }
+  ]
+}
+`
+
+	listResponse = `
+{
+  "endpoints": [
+    {
+      "id": "03184a04-95d5-4555-86c4-e767a371ff99",
+      "status": "accepted",
+      "ip": "192.168.0.232",
+      "marker_id": 16777337,
+      "active_status": "active",
+      "vpc_id": "84758cf5-9c62-43ae-a778-3dbd8370c0a4",
+      "service_type": "interface",
+      "project_id": "295dacf46a4842fcbf7844dc2dc2489d",
+      "subnet_id": "68bfbcc1-dff2-47e4-a9d4-332b9bc1b8de",
+      "enable_dns": true,
+      "dns_name": "test123",
+      "created_at": "2018-10-18T06:49:46Z",
+      "updated_at": "2018-10-18T06:49:50Z",
+      "endpoint_service_id": "5133655d-0e28-4090-b669-13f87b355c78",
+      "endpoint_service_name": "test123",
+      "whitelist": [
+        "127.0.0.1"
+      ],
+      "enable_whitelist": true,
+      "tags": [
+        {
+          "key": "test1",
+          "value": "test1"
+        }
+      ]
+    },
+    {
+      "id": "43b0e3b0-eec9-49da-866b-6687b75f9fe5",
+      "status": "accepted",
+      "ip": "192.168.0.115",
+      "marker_id": 16777322,
+      "active_status": "active",
+      "vpc_id": "84758cf5-9c62-43ae-a778-3dbd8370c0a4",
+      "service_type": "interface",
+      "project_id": "295dacf46a4842fcbf7844dc2dc2489d",
+      "subnet_id": "65528a22-59a1-4972-ba64-88984b3207cd",
+      "enable_dns": true,
+      "dns_name": "test123",
+      "created_at": "2018-10-18T06:36:20Z",
+      "updated_at": "2018-10-18T06:36:24Z",
+      "endpoint_service_id": "5133655d-0e28-4090-b669-13f87b355c78",
+      "endpoint_service_name": "test123",
+      "whitelist": [
+        "127.0.0.1"
+      ],
+      "enable_whitelist": true,
+      "tags": [
+        {
+          "key": "test1",
+          "value": "test1"
+        }
+      ]
+    }
+  ],
+  "total_count": 17
+}
+`
+)
+
+var expected = &endpoints.Endpoint{
+	ID:          "4189d3c2-8882-4871-a3c2-d380272eed83",
+	ServiceType: services.ServiceTypeInterface,
+	MarkerID:    322312312312,
+	Status:      endpoints.StatusCreating,
+	RouterID:    "84758cf5-9c62-43ae-a778-3dbd8370c0a4",
+	EnableDNS:   true,
+	ServiceName: "test123",
+	ServiceID:   "e0c748b7-d982-47df-ba06-b9c8c7650c1a",
+	ProjectID:   "6e9dfd51d1124e8d8498dce894923a0d",
+	CreatedAt:   "2018-01-30T07:42:01.174",
+	UpdatedAt:   "2018-01-30T07:42:01.174",
+	Tags:        []tags.ResourceTag{{Key: "test1", Value: "test1"}},
+}
