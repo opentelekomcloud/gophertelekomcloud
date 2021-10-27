4ab23bdae44833161699046bdf06ca798419dc05
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-10-27 18:19:50 +0300

    [ELB] Implement `monitors` v3 pkg (#276)
    [ELB] Implement `monitors` v3 pkg
    
    What this PR does / why we need it
    Implement Dedicated Load Balancer monitors v3 pkg
    Which issue this PR fixes
    Refers to: #248
    Special notes for your reviewer
    === RUN   TestMonitorList
    --- PASS: TestMonitorList (1.98s)
    === RUN   TestMonitorLifecycle
        helpers.go:18: Attempting to create ELBv3 LoadBalancer
        helpers.go:56: Created ELBv3 LoadBalancer: b3b3002e-506d-4360-88bc-8ff4d1479b9f
        helpers.go:149: Attempting to create ELBv3 Pool
        helpers.go:164: Created ELBv3 Pool: 6f1b842f-4290-4bf7-950c-7aa443e5331e
        monitors_test.go:38: Attempting to Create ELBv3 Monitor
        monitors_test.go:61: Created ELBv3 Monitor: 3b7b2aa5-5a98-4a14-b6e1-c8d857fc4280
        monitors_test.go:63: Attempting to Update ELBv3 Monitor
        monitors_test.go:72: Updated ELBv3 Monitor: 3b7b2aa5-5a98-4a14-b6e1-c8d857fc4280
        monitors_test.go:52: Attempting to Delete ELBv3 Monitor: 3b7b2aa5-5a98-4a14-b6e1-c8d857fc4280
        monitors_test.go:55: Deleted ELBv3 Monitor: 3b7b2aa5-5a98-4a14-b6e1-c8d857fc4280
        helpers.go:170: Attempting to delete ELBv3 Pool: 6f1b842f-4290-4bf7-950c-7aa443e5331e
        helpers.go:173: Deleted ELBv3 Pool: 6f1b842f-4290-4bf7-950c-7aa443e5331e
        helpers.go:62: Attempting to delete ELBv3 LoadBalancer: b3b3002e-506d-4360-88bc-8ff4d1479b9f
        helpers.go:65: Deleted ELBv3 LoadBalancer: b3b3002e-506d-4360-88bc-8ff4d1479b9f
    --- PASS: TestMonitorLifecycle (10.17s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: Anton Kachurin <katchuring@gmail.com>
    Reviewed-by: None <None>
    Reviewed-by: Anton Sidelnikov <None>

---
+++ acceptance/openstack/elb/v3/monitors_test.go
@@ -0,0 +1,81 @@
+package v3
+
+import (
+	"testing"
+
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/elb/v3/monitors"
+	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
+)
+
+func TestMonitorList(t *testing.T) {
+	client, err := clients.NewElbV3Client(t)
+	th.AssertNoErr(t, err)
+
+	listOpts := monitors.ListOpts{}
+	monitorPages, err := monitors.List(client, listOpts).AllPages()
+	th.AssertNoErr(t, err)
+
+	monitorList, err := monitors.ExtractMonitors(monitorPages)
+	th.AssertNoErr(t, err)
+
+	for _, monitor := range monitorList {
+		tools.PrintResource(t, monitor)
+	}
+}
+
+func TestMonitorLifecycle(t *testing.T) {
+	client, err := clients.NewElbV3Client(t)
+	th.AssertNoErr(t, err)
+
+	loadbalancerID := createLoadBalancer(t, client)
+	defer deleteLoadbalancer(t, client, loadbalancerID)
+
+	poolID := createPool(t, client, loadbalancerID)
+	defer deletePool(t, client, poolID)
+
+	t.Logf("Attempting to Create ELBv3 Monitor")
+	monitorName := tools.RandomString("create-monitor-", 3)
+	createOpts := monitors.CreateOpts{
+		PoolID:     poolID,
+		Type:       monitors.TypeHTTP,
+		Delay:      1,
+		Timeout:    30,
+		MaxRetries: 3,
+		URLPath:    "/",
+		Name:       monitorName,
+	}
+	monitor, err := monitors.Create(client, createOpts).Extract()
+	th.AssertNoErr(t, err)
+	defer func() {
+		t.Logf("Attempting to Delete ELBv3 Monitor: %s", monitor.ID)
+		err := monitors.Delete(client, monitor.ID).ExtractErr()
+		th.AssertNoErr(t, err)
+		t.Logf("Deleted ELBv3 Monitor: %s", monitor.ID)
+	}()
+
+	th.AssertEquals(t, createOpts.Name, monitor.Name)
+	th.AssertEquals(t, createOpts.Type, monitor.Type)
+	th.AssertEquals(t, createOpts.MaxRetries, monitor.MaxRetries)
+	t.Logf("Created ELBv3 Monitor: %s", monitor.ID)
+
+	t.Logf("Attempting to Update ELBv3 Monitor")
+	monitorName = tools.RandomString("update-monitor-", 3)
+	updateOpts := monitors.UpdateOpts{
+		Delay:      3,
+		Timeout:    35,
+		MaxRetries: 5,
+		Name:       monitorName,
+	}
+	_, err = monitors.Update(client, monitor.ID, updateOpts).Extract()
+	th.AssertNoErr(t, err)
+	t.Logf("Updated ELBv3 Monitor: %s", monitor.ID)
+
+	newMonitor, err := monitors.Get(client, monitor.ID).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, updateOpts.Name, newMonitor.Name)
+	th.AssertEquals(t, updateOpts.Timeout, newMonitor.Timeout)
+	th.AssertEquals(t, updateOpts.Delay, newMonitor.Delay)
+	th.AssertEquals(t, updateOpts.MaxRetries, newMonitor.MaxRetries)
+}
