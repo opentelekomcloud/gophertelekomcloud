a3da9ef7a7bc9aec05e00eb497f7fff7fa623a25
Anton Kachurin <anton.kachurin@t-systems.com>
2021-11-10 10:17:57 +0300

    [ELB] Add `List` function to listeners v3

--- openstack/elb/v3/listeners/requests.go
+++ openstack/elb/v3/listeners/requests.go
@@ -3,6 +3,7 @@ package listeners
 import (
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/tags"
+	"github.com/opentelekomcloud/gophertelekomcloud/pagination"
 )
 
 // Protocol represents a listener protocol.
@@ -223,4 +224,52 @@ func Update(client *golangsdk.ServiceClient, id string, opts UpdateOptsBuilder) 
 func Delete(client *golangsdk.ServiceClient, id string) (r DeleteResult) {
 	_, r.Err = client.Delete(resourceURL(client, id), nil)
 	return
+}
+
+type ListOptsBuilder interface {
+	ToListenerListQuery() (string, error)
+}
+
+type ListOpts struct {
+	Limit       int    `q:"limit"`
+	Marker      string `q:"marker"`
+	PageReverse bool   `q:"page_reverse"`
+
+	ProtocolPort            []int      `q:"protocol_port"`
+	Protocol                []Protocol `q:"protocol"`
+	Description             []string   `q:"description"`
+	DefaultTLSContainerRef  []string   `q:"default_tls_container_ref"`
+	ClientCATLSContainerRef []string   `q:"client_ca_tls_container_ref"`
+	DefaultPoolID           []string   `q:"default_pool_id"`
+	ID                      []string   `q:"id"`
+	Name                    []string   `q:"name"`
+	LoadBalancerID          []string   `q:"loadbalancer_id"`
+	TLSCiphersPolicy        []string   `q:"tls_ciphers_policy"`
+	MemberAddress           []string   `q:"member_address"`
+	MemberDeviceID          []string   `q:"member_device_id"`
+	MemberTimeout           []int      `q:"member_timeout"`
+	ClientTimeout           []int      `q:"client_timeout"`
+	KeepAliveTimeout        []int      `q:"keep_alive_timeout"`
+}
+
+func (opts ListOpts) ToListenerListQuery() (string, error) {
+	q, err := golangsdk.BuildQueryString(opts)
+	if err != nil {
+		return "", err
+	}
+	return q.String(), nil
+}
+
+func List(client *golangsdk.ServiceClient, opts ListOptsBuilder) pagination.Pager {
+	url := rootURL(client)
+	if opts != nil {
+		q, err := opts.ToListenerListQuery()
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		url += q
+	}
+	return pagination.NewPager(client, url, func(r pagination.PageResult) pagination.Page {
+		return ListenerPage{PageWithInfo: pagination.NewPageWithInfo(r)}
+	})
 }
