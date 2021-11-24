5a735d403b470703a614a608e5143a03f226ee06
Anton Kachurin <anton.kachurin@t-systems.com>
2021-11-24 15:30:04 +0300

    [VPC] Add bandwidth v2 associate test

--- acceptance/openstack/networking/v2/bandwidths_test.go
+++ acceptance/openstack/networking/v2/bandwidths_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/networking/v2/bandwidths"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/networking/v2/extensions/layer3/floatingips"
 	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
 )
 
@@ -22,12 +23,12 @@ func TestBandwidthLifecycle(t *testing.T) {
 
 	bandwidth, err := bandwidths.Create(client, createOpts).Extract()
 	th.AssertNoErr(t, err)
-	defer func() {
+	t.Cleanup(func() {
 		t.Logf("Attempting to delete BandwidthV2: %s", bandwidth.ID)
 		err := bandwidths.Delete(client, bandwidth.ID).ExtractErr()
 		th.AssertNoErr(t, err)
 		t.Logf("Deleted BandwidthV2: %s", bandwidth.ID)
-	}()
+	})
 	th.AssertEquals(t, createOpts.Name, bandwidth.Name)
 	th.AssertEquals(t, createOpts.Size, bandwidth.Size)
 	t.Logf("Created BandwidthV2: %s", bandwidth.ID)
@@ -60,4 +61,67 @@ func TestBandwidthLifecycle(t *testing.T) {
 		}
 	}
 	th.AssertDeepEquals(t, newBandwidth, &bandwidthList[flag])
+}
+
+func TestBandwidthAssociate(t *testing.T) {
+	client, err := clients.NewNetworkV2Client()
+	th.AssertNoErr(t, err)
+
+	bandwidthName := tools.RandomString("band-create", 3)
+	createOpts := bandwidths.CreateOpts{
+		Name: bandwidthName,
+		Size: 20,
+	}
+
+	bandwidth, err := bandwidths.Create(client, createOpts).Extract()
+	th.AssertNoErr(t, err)
+	id := bandwidth.ID
+	t.Cleanup(func() {
+		t.Logf("Attempting to delete BandwidthV2: %s", id)
+		err := bandwidths.Delete(client, id).ExtractErr()
+		th.CheckNoErr(t, err)
+		t.Logf("Deleted BandwidthV2: %s", id)
+	})
+
+	eip, err := floatingips.Create(client, floatingips.CreateOpts{
+		FloatingNetworkID: "0a2228f2-7f8a-45f1-8e09-9039e1d09975", // yep, it's static
+	}).Extract()
+	th.AssertNoErr(t, err)
+	t.Cleanup(func() {
+		err := floatingips.Delete(client, eip.ID).ExtractErr()
+		th.CheckNoErr(t, err)
+	})
+
+	err = tools.WaitFor(func() (bool, error) {
+		fip, err := floatingips.Get(client, eip.ID).Extract()
+		if err != nil {
+			return false, err
+		}
+		return fip.Status == "ACTIVE" || fip.Status == "DOWN", nil
+	})
+	th.AssertNoErr(t, err)
+
+	assOpts := bandwidths.InsertOpts{PublicIpInfo: []bandwidths.PublicIpInfoInsertOpts{
+		{PublicIpID: eip.ID},
+	}}
+	_, err = bandwidths.Insert(client, id, assOpts).Extract()
+	th.AssertNoErr(t, err)
+
+	bnw, err := bandwidths.Get(client, id).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, 1, len(bnw.PublicIpInfo))
+
+	remOpts := bandwidths.RemoveOpts{
+		ChargeMode: "bandwidth",
+		Size:       1,
+		PublicIpInfo: []bandwidths.PublicIpInfoID{
+			{PublicIpID: eip.ID},
+		},
+	}
+	err = bandwidths.Remove(client, id, remOpts).ExtractErr()
+	th.AssertNoErr(t, err)
+
+	bnwAfter, err := bandwidths.Get(client, id).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, 0, len(bnwAfter.PublicIpInfo))
 }
