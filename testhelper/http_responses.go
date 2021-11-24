c4d565146262ab9b2df86f7ebe5e5c678746adf9
Anton Kachurin <anton.kachurin@t-systems.com>
2021-11-24 15:30:58 +0300

    Mark `testhelper` functions with `t.Helper`

--- testhelper/http_responses.go
+++ testhelper/http_responses.go
@@ -36,6 +36,7 @@ func Endpoint() string {
 
 // TestFormValues ensures that all the URL parameters given to the http.Request are the same as values.
 func TestFormValues(t *testing.T, r *http.Request, values map[string]string) {
+	t.Helper()
 	want := url.Values{}
 	for k, v := range values {
 		want.Add(k, v)
@@ -49,6 +50,7 @@ func TestFormValues(t *testing.T, r *http.Request, values map[string]string) {
 
 // TestMethod checks that the Request has the expected method (e.g. GET, POST).
 func TestMethod(t *testing.T, r *http.Request, expected string) {
+	t.Helper()
 	if expected != r.Method {
 		t.Errorf("Request method = %v, expected %v", r.Method, expected)
 	}
@@ -56,6 +58,7 @@ func TestMethod(t *testing.T, r *http.Request, expected string) {
 
 // TestHeader checks that the header on the http.Request matches the expected value.
 func TestHeader(t *testing.T, r *http.Request, header string, expected string) {
+	t.Helper()
 	if actual := r.Header.Get(header); expected != actual {
 		t.Errorf("Header %s = %s, expected %s", header, actual, expected)
 	}
@@ -63,6 +66,7 @@ func TestHeader(t *testing.T, r *http.Request, header string, expected string) {
 
 // TestBody verifies that the request body matches an expected body.
 func TestBody(t *testing.T, r *http.Request, expected string) {
+	t.Helper()
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		t.Errorf("Unable to read body: %v", err)
@@ -76,6 +80,7 @@ func TestBody(t *testing.T, r *http.Request, expected string) {
 // TestJSONRequest verifies that the JSON payload of a request matches an expected structure, without asserting things about
 // whitespace or ordering.
 func TestJSONRequest(t *testing.T, r *http.Request, expected string) {
+	t.Helper()
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		t.Errorf("Unable to read request body: %v", err)
