b98bdccf0030a763e53f8807556486194cace919
Anton Kachurin <anton.kachurin@t-systems.com>
2022-03-31 17:51:58 +0300

    [VPC] Fix EIP list filtering
    Fix incorrect EIP filtering by address

---
+++ acceptance/openstack/vpc/v1/eip_test.go
@@ -0,0 +1,52 @@
+package v1
+
+import (
+	"testing"
+
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/networking/v1/eips"
+	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
+)
+
+func TestEIPListing(t *testing.T) {
+	client, err := clients.NewNetworkV1Client()
+	th.AssertNoErr(t, err)
+
+	eipCreateOpts := eips.ApplyOpts{
+		IP: eips.PublicIpOpts{
+			Type: "5_bgp",
+		},
+		Bandwidth: eips.BandwidthOpts{
+			ShareType: "PER",
+			Name:      tools.RandomString("acc-band-", 3),
+			Size:      100,
+		},
+	}
+	eip, err := eips.Apply(client, eipCreateOpts).Extract()
+	th.AssertNoErr(t, err)
+	t.Cleanup(func() {
+		err = eips.Delete(client, eip.ID).ExtractErr()
+		th.AssertNoErr(t, err)
+	})
+
+	cases := map[string]eips.ListOpts{
+		"ID": {
+			ID: eip.ID,
+		},
+		"PublicIP": {
+			PublicAddress: eip.PublicAddress,
+		},
+	}
+	for name, opts := range cases {
+		t.Run(name, func(t *testing.T) {
+			opts := opts
+			t.Parallel()
+
+			list, err := eips.List(client, opts)
+			th.AssertNoErr(t, err)
+			th.AssertEquals(t, 1, len(list))
+			th.AssertEquals(t, eip.ID, list[0].ID)
+		})
+	}
+}
