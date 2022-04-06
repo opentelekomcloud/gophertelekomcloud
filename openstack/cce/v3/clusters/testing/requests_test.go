a057f8654f1d96b28958bb07e5520896b28863ad
Vladimir Vshivkov <32225815+enrrou@users.noreply.github.com>
2022-04-06 15:58:24 +0400

    add acceptance for cce turbocluster

--- openstack/cce/v3/clusters/testing/requests_test.go
+++ openstack/cce/v3/clusters/testing/requests_test.go
@@ -165,6 +165,77 @@ func TestCreateV3Cluster(t *testing.T) {
 
 }
 
+func TestCreateV3TurboCluster(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/api/v3/projects/c59fd21fd2a94963b822d8985b884673/clusters", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "POST")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		th.TestHeader(t, r, "Content-Type", "application/json")
+		th.TestHeader(t, r, "Accept", "application/json")
+
+		th.TestJSONRequest(t, r, `
+{
+    "kind": "Cluster",
+    "apiversion": "v3",
+    "metadata": {
+        "name": "turbo"
+           },
+    "spec": {
+        "type": "VirtualMachine",
+        "flavor": "cce.s2.small",
+        "version": "v1.19.10-r0",
+         "hostNetwork": {
+            "vpc": "3305eb40-2707-4940-921c-9f335f84a2ca",
+            "subnet": "00e41db7-e56b-4946-bf91-27bb9effd664"
+        },
+        "containerNetwork": {
+            "mode": "eni"
+        },
+		"eniNetwork": {
+			"eniSubnetId": "417dcc1f-95d7-43e7-8533-ab078d266303",
+			"eniSubnetCIDR": "192.168.0.0/24"
+		},
+        "authentication": {
+            "mode": "rbac",
+			"authenticatingProxy": {}
+        }
+    }
+
+}
+`)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		_, _ = fmt.Fprint(w, Output)
+	})
+	options := clusters.CreateOpts{Kind: "Cluster",
+		ApiVersion: "v3",
+		Metadata:   clusters.CreateMetaData{Name: "test-turbo-cluster"},
+		Spec: clusters.Spec{Type: "VirtualMachine",
+			Flavor:  "cce.s2.small",
+			Version: "v1.19.10-r0",
+			HostNetwork: clusters.HostNetworkSpec{
+				VpcId:    "3305eb40-2707-4940-921c-9f335f84a2ca",
+				SubnetId: "00e41db7-e56b-4946-bf91-27bb9effd664"},
+			ContainerNetwork: clusters.ContainerNetworkSpec{Mode: "eni"},
+			EniNetwork: clusters.EniNetworkSpec{
+				SubnetId: "417dcc1f-95d7-43e7-8533-ab078d266303",
+				Cidr:     "192.168.0.0/24",
+			},
+			Authentication: clusters.AuthenticationSpec{
+				Mode:                "rbac",
+				AuthenticatingProxy: make(map[string]string)},
+		},
+	}
+	actual, err := clusters.Create(fake.ServiceClient(), options).Extract()
+	th.AssertNoErr(t, err)
+	expected := Expected
+	th.AssertDeepEquals(t, expected, actual)
+
+}
+
 func TestUpdateV3Cluster(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
@@ -195,6 +266,21 @@ func TestUpdateV3Cluster(t *testing.T) {
 }
 
 func TestDeleteV3Cluster(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/api/v3/projects/c59fd21fd2a94963b822d8985b884673/clusters/daa97872-59d7-11e8-a787-0255ac101f54", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "DELETE")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		w.WriteHeader(http.StatusOK)
+	})
+
+	err := clusters.Delete(fake.ServiceClient(), "daa97872-59d7-11e8-a787-0255ac101f54").ExtractErr()
+	th.AssertNoErr(t, err)
+
+}
+
+func TestDeleteV3TurboCluster(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
 
