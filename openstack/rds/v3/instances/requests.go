b8381443d122f4da7c9b32e02c2b495afe915a9c
Vladimir Vshivkov <32225815+enrrou@users.noreply.github.com>
2022-05-17 16:48:51 +0400

    add listopts to rds instances

--- openstack/rds/v3/instances/requests.go
+++ openstack/rds/v3/instances/requests.go
@@ -141,6 +141,45 @@ func Delete(client *golangsdk.ServiceClient, instanceId string) (r DeleteInstanc
 	return
 }
 
+type ListOpts struct {
+	Id            string `q:"id"`
+	Name          string `q:"name"`
+	Type          string `q:"type"`
+	DataStoreType string `q:"datastore_type"`
+	VpcId         string `q:"vpc_id"`
+	SubnetId      string `q:"subnet_id"`
+	Offset        int    `q:"offset"`
+	Limit         int    `q:"limit"`
+}
+
+func (opts ListOpts) ToRdsListDetailQuery() (string, error) {
+	q, err := golangsdk.BuildQueryString(opts)
+	if err != nil {
+		return "", err
+	}
+	return q.String(), err
+}
+
+func List(client *golangsdk.ServiceClient, opts ListRdsBuilder) pagination.Pager {
+	url := listURL(client)
+	if opts != nil {
+		query, err := opts.ToRdsListDetailQuery()
+
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		url += query
+	}
+
+	pageRdsList := pagination.NewPager(client, url, func(r pagination.PageResult) pagination.Page {
+		return RdsPage{pagination.SinglePageBase(r)}
+	})
+
+	rdsheader := map[string]string{"Content-Type": "application/json"}
+	pageRdsList.Headers = rdsheader
+	return pageRdsList
+}
+
 type RestartRdsInstanceOpts struct {
 	Restart string `json:"restart" required:"true"`
 }
@@ -190,26 +229,6 @@ func (opts ListRdsInstanceOpts) ToRdsListDetailQuery() (string, error) {
 		return "", err
 	}
 	return q.String(), err
-}
-
-func List(client *golangsdk.ServiceClient, opts ListRdsBuilder) pagination.Pager {
-	url := listURL(client)
-	if opts != nil {
-		query, err := opts.ToRdsListDetailQuery()
-
-		if err != nil {
-			return pagination.Pager{Err: err}
-		}
-		url += query
-	}
-
-	pageRdsList := pagination.NewPager(client, url, func(r pagination.PageResult) pagination.Page {
-		return RdsPage{pagination.SinglePageBase(r)}
-	})
-
-	rdsheader := map[string]string{"Content-Type": "application/json"}
-	pageRdsList.Headers = rdsheader
-	return pageRdsList
 }
 
 type SingleToHaRdsOpts struct {
