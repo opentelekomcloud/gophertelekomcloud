f14c66aae34579ea1f84ae293938b56a6f1350a9
Aloento <11802769+Aloento@users.noreply.github.com>
2022-08-29 15:01:48 +0200

    groups_hcs

--- openstack/autoscaling/v1/groups_hcs/delete.go
+++
@@ -1,8 +0,0 @@
-package groups_hcs
-
-import "github.com/opentelekomcloud/gophertelekomcloud"
-
-func Delete(client *golangsdk.ServiceClient, id string) (err error) {
-	_, err = client.Delete(client.ServiceURL("scaling_group", id), nil)
-	return
-}
