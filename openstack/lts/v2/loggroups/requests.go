800c6a898dcdac76036edde3013ac4884e11236c
Vladimir Vshivkov <vshivkovvladimir@gmail.com>
2022-08-30 08:14:41 +0300

    lts create fix

--- openstack/lts/v2/loggroups/requests.go
+++ openstack/lts/v2/loggroups/requests.go
@@ -38,10 +38,12 @@ func Create(client *golangsdk.ServiceClient, ops CreateOptsBuilder) (r CreateRes
 
 // Delete a log group by id
 func Delete(client *golangsdk.ServiceClient, id string) (r DeleteResult) {
-	opts := golangsdk.RequestOpts{
-		MoreHeaders: map[string]string{"Content-Type": "application/json;charset=utf8"},
-	}
-	_, r.Err = client.Delete(deleteURL(client, id), &opts)
+	// opts := golangsdk.RequestOpts{
+	// 	MoreHeaders: map[string]string{"Content-Type": "application/json;charset=utf8"},
+	// }
+	_, r.Err = client.Delete(deleteURL(client, id), &golangsdk.RequestOpts{
+		OkCodes: []int{204},
+	})
 	return
 }
 
