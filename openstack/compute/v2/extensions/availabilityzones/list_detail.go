a62d594e4eeb8a873547ff345d4dc84ba1116a37
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-07 12:44:47 +0200

    availabilityzones

---
+++ openstack/compute/v2/extensions/availabilityzones/list_detail.go
@@ -0,0 +1,13 @@
+package availabilityzones
+
+import (
+	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/pagination"
+)
+
+// ListDetail will return the existing availability zones with detailed information.
+func ListDetail(client *golangsdk.ServiceClient) pagination.Pager {
+	return pagination.NewPager(client, listDetailURL(client), func(r pagination.PageResult) pagination.Page {
+		return AvailabilityZonePage{pagination.SinglePageBase(r)}
+	})
+}
