1df85957e278ac752af00ce652ceed2134ea969a
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-09 13:57:47 +0200

    attachinterfaces

--- openstack/compute/v2/extensions/attachinterfaces/delete.go
+++ openstack/compute/v2/extensions/attachinterfaces/delete.go
@@ -4,7 +4,7 @@ import "github.com/opentelekomcloud/gophertelekomcloud"
 
 // Delete makes a request against the nova API to detach a single interface from the server.
 // It needs server and port IDs to make a such request.
-func Delete(client *golangsdk.ServiceClient, serverID, portID string) (r DeleteResult) {
-	raw, err := client.Delete(client.ServiceURL("servers", serverID, "os-interface", portID), nil)
+func Delete(client *golangsdk.ServiceClient, serverID, portID string) (err error) {
+	_, err = client.Delete(client.ServiceURL("servers", serverID, "os-interface", portID), nil)
 	return
 }
--- openstack/compute/v2/extensions/attachinterfaces/list.go
+++ openstack/compute/v2/extensions/attachinterfaces/list.go
@@ -2,12 +2,25 @@ package attachinterfaces
 
 import (
 	"github.com/opentelekomcloud/gophertelekomcloud"
-	"github.com/opentelekomcloud/gophertelekomcloud/pagination"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
 )
 
 // List makes a request against the nova API to list the server's interfaces.
-func List(client *golangsdk.ServiceClient, serverID string) pagination.Pager {
-	return pagination.NewPager(client, client.ServiceURL("servers", serverID, "os-interface"), func(r pagination.PageResult) pagination.Page {
-		return InterfacePage{pagination.SinglePageBase(r)}
-	})
+func List(client *golangsdk.ServiceClient, serverID string) ([]Interface, error) {
+	raw, err := client.Get(client.ServiceURL("servers", serverID, "os-interface"), nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res []Interface
+	err = extract.IntoSlicePtr(raw.Body, &res, "interfaceAttachments")
+	return res, err
+}
+
+// FixedIP represents a Fixed IP Address.
+// This struct is also used when creating an attachment,
+// but it is not possible to specify a SubnetID.
+type FixedIP struct {
+	SubnetID  string `json:"subnet_id,omitempty"`
+	IPAddress string `json:"ip_address"`
 }
