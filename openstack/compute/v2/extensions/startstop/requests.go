0cc89040cdc3c2beaaa9cd0e52be8d7cbef9b4d4
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-12 13:01:12 +0200

    startstop

--- openstack/compute/v2/extensions/startstop/requests.go
+++ openstack/compute/v2/extensions/startstop/requests.go
@@ -7,13 +7,13 @@ func actionURL(client *golangsdk.ServiceClient, id string) string {
 }
 
 // Start is the operation responsible for starting a Compute server.
-func Start(client *golangsdk.ServiceClient, id string) (r StartResult) {
-	raw, err := client.Post(actionURL(client, id), map[string]interface{}{"os-start": nil}, nil, nil)
+func Start(client *golangsdk.ServiceClient, id string) (err error) {
+	_, err = client.Post(actionURL(client, id), map[string]interface{}{"os-start": nil}, nil, nil)
 	return
 }
 
 // Stop is the operation responsible for stopping a Compute server.
-func Stop(client *golangsdk.ServiceClient, id string) (r StopResult) {
-	raw, err := client.Post(actionURL(client, id), map[string]interface{}{"os-stop": nil}, nil, nil)
+func Stop(client *golangsdk.ServiceClient, id string) (err error) {
+	_, err = client.Post(actionURL(client, id), map[string]interface{}{"os-stop": nil}, nil, nil)
 	return
 }
