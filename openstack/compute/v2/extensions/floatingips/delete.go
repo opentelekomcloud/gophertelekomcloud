546efede5ee74125e46c2a318305e09cab62adab
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-13 19:49:58 +0200

    fix

--- openstack/compute/v2/extensions/floatingips/delete.go
+++ openstack/compute/v2/extensions/floatingips/delete.go
@@ -3,7 +3,7 @@ package floatingips
 import "github.com/opentelekomcloud/gophertelekomcloud"
 
 // Delete requests the deletion of a previous allocated Floating IP.
-func Delete(client *golangsdk.ServiceClient, id string) (r DeleteResult) {
-	raw, err := client.Delete(client.ServiceURL("os-floating-ips", id), nil)
+func Delete(client *golangsdk.ServiceClient, id string) (err error) {
+	_, err = client.Delete(client.ServiceURL("os-floating-ips", id), nil)
 	return
 }
