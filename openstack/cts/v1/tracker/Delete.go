e4cfd02eabbcc86aba36d542cbe81588214da645
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-16 09:05:15 +0200

    Delete

--- openstack/cts/v1/tracker/Delete.go
+++ openstack/cts/v1/tracker/Delete.go
@@ -3,9 +3,7 @@ package tracker
 import "github.com/opentelekomcloud/gophertelekomcloud"
 
 // Delete will permanently delete a particular tracker.
-func Delete(client *golangsdk.ServiceClient) (r DeleteResult) {
-	raw, err := client.Delete(client.ServiceURL("tracker"), &golangsdk.RequestOpts{
-		OkCodes: []int{204},
-	})
+func Delete(client *golangsdk.ServiceClient) (err error) {
+	_, err = client.Delete(client.ServiceURL("tracker"), nil)
 	return
 }
--- openstack/cts/v3/keyevent/Delete.go
+++ openstack/cts/v3/keyevent/Delete.go
@@ -1,1 +1,14 @@
 package keyevent
+
+import (
+	"strings"
+
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+)
+
+func Delete(client *golangsdk.ServiceClient, notificationId []string) (err error) {
+	// DELETE /v3/{project_id}/notifications
+	url := client.ServiceURL("notifications") + "?notification_id=\"" + strings.Join(notificationId, ",") + "\""
+	_, err = client.Delete(url, nil)
+	return
+}
