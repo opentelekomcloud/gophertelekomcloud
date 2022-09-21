b0a5ed32bd39b38827d80d5e522e1f7a65be4d58
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-21 17:43:54 +0200

    DeleteBackupFile

--- openstack/dcs/v1/backups/DeleteBackupFile.go
+++ openstack/dcs/v1/backups/DeleteBackupFile.go
@@ -4,10 +4,10 @@ import (
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 )
 
-func DeleteBackupFile(client *golangsdk.ServiceClient) (string, error) {
-	raw, err := client.Post(client.ServiceURL("availableZones"), nil, nil, &golangsdk.RequestOpts{
+func DeleteBackupFile(client *golangsdk.ServiceClient, instancesId string, backupId string) (err error) {
+	_, err = client.Delete(client.ServiceURL("instances", instancesId, "backups", backupId), &golangsdk.RequestOpts{
 		OkCodes: []int{200},
 	})
 
-	return res.BackupId, err
+	return
 }
