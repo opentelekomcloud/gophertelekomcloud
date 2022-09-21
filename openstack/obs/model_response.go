90202d01e1dd0e20b8a8e97cb84905f5db80348a
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-21 09:06:45 +0200

    clean

--- openstack/obs/model_response.go
+++ openstack/obs/model_response.go
@@ -19,38 +19,3 @@ type Error struct {
 	Code      string   `xml:"Code"`
 	Message   string   `xml:"Message"`
 }
-
-// FetchResponse defines the response fetch policy configuration
-type FetchResponse struct {
-	Status FetchPolicyStatusType `json:"status"`
-	Agency string                `json:"agency"`
-}
-
-// SetBucketFetchJobResponse defines the response SetBucketFetchJob configuration
-type SetBucketFetchJobResponse struct {
-	ID   string `json:"id"`
-	Wait int    `json:"Wait"`
-}
-
-// GetBucketFetchJobResponse defines the response fetch job configuration
-type GetBucketFetchJobResponse struct {
-	Err    string      `json:"err"`
-	Code   string      `json:"code"`
-	Status string      `json:"status"`
-	Job    JobResponse `json:"job"`
-}
-
-// JobResponse defines the response job configuration
-type JobResponse struct {
-	Bucket           string `json:"bucket"`
-	URL              string `json:"url"`
-	Host             string `json:"host"`
-	Key              string `json:"key"`
-	Md5              string `json:"md5"`
-	CallBackURL      string `json:"callbackurl"`
-	CallBackBody     string `json:"callbackbody"`
-	CallBackBodyType string `json:"callbackbodytype"`
-	CallBackHost     string `json:"callbackhost"`
-	FileType         string `json:"file_type"`
-	IgnoreSameKey    bool   `json:"ignore_same_key"`
-}
