6fdb5557299740ab3152e2014ec6fa69743d0e3c
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-22 17:19:14 +0200

    GetProducts

--- openstack/dcs/v1/others/results.go
+++ openstack/dcs/v1/others/results.go
@@ -10,16 +10,6 @@ type GetResponse struct {
 }
 
 // Product for dcs
-type Product struct {
-	Price        float64 `json:"price"`
-	Currency     string  `json:"currency"`
-	ProductID    string  `json:"product_id"`
-	SpecCode     string  `json:"spec_code"`
-	SpecDetails  string  `json:"spec_details"`
-	ChargingType string  `json:"charging_type"`
-	SpecDetails2 string  `json:"spec_details2"`
-	ProdType     string  `json:"prod_type"`
-}
 
 // GetResult contains the body of getting detailed
 type GetResult struct {
