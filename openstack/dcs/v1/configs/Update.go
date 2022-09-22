d5dc881cdc2d47460756452ff0631e7fa8e209ed
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-22 08:13:48 +0200

    configs

--- openstack/dcs/v1/configs/Update.go
+++ openstack/dcs/v1/configs/Update.go
@@ -2,8 +2,22 @@ package configs
 
 import "github.com/opentelekomcloud/gophertelekomcloud"
 
-func Update(client *golangsdk.ServiceClient, instanceID string, opts UpdateOptsBuilder) (err error) {
-	b, err := opts.ToConfigUpdateMap()
+type UpdateOpts struct {
+	// Array of configuration items of the DCS instance.
+	RedisConfigs []RedisConfig `json:"redis_config" required:"true"`
+}
+
+type RedisConfig struct {
+	// Configuration item ID.
+	ParamID string `json:"param_id" required:"true"`
+	// Configuration item name.
+	ParamName string `json:"param_name" required:"true"`
+	// Value of the configuration item.
+	ParamValue string `json:"param_value" required:"true"`
+}
+
+func Update(client *golangsdk.ServiceClient, instanceID string, opts UpdateOpts) (err error) {
+	b, err := golangsdk.BuildRequestBody(opts, "")
 	if err != nil {
 		return
 	}
