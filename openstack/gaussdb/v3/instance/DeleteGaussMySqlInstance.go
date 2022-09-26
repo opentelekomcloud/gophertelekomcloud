59e65f39bb3265f71b1cc45b42707624beb625c8
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-26 18:53:03 +0200

    DeleteGaussMySqlInstance

--- openstack/gaussdb/v3/instance/DeleteGaussMySqlInstance.go
+++ openstack/gaussdb/v3/instance/DeleteGaussMySqlInstance.go
@@ -1,1 +1,24 @@
 package instance
+
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+)
+
+// DeleteGaussMySqlInstance
+// return ID of DB instance deletion task
+func DeleteGaussMySqlInstance(client *golangsdk.ServiceClient, instanceId string) (string, error) {
+	// DELETE https://{Endpoint}/mysql/v3/{project_id}/instances/{instance_id}
+	raw, err := client.Delete(client.ServiceURL("instances", instanceId), &golangsdk.RequestOpts{
+		OkCodes: []int{200},
+	})
+	if err != nil {
+		return "", err
+	}
+
+	var res struct {
+		JobId string `json:"job_id"`
+	}
+	err = extract.Into(raw.Body, &res)
+	return res.JobId, err
+}
