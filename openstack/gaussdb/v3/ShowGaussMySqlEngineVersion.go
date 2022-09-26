e5efb3127963a493bf4293f09cc661ffcf6d8965
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-26 10:33:03 +0200

    ShowGaussMySqlEngineVersion

--- openstack/gaussdb/v3/ShowGaussMySqlEngineVersion.go
+++ openstack/gaussdb/v3/ShowGaussMySqlEngineVersion.go
@@ -1,1 +1,26 @@
 package v3
+
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+)
+
+// ShowGaussMySqlEngineVersion
+// databaseName: DB engine. The following DB engine is supported (case-insensitive): gaussdb-mysql
+func ShowGaussMySqlEngineVersion(client *golangsdk.ServiceClient, databaseName string) ([]MysqlEngineVersionInfo, error) {
+	raw, err := client.Get(client.ServiceURL("datastores", databaseName), nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res []MysqlEngineVersionInfo
+	err = extract.IntoSlicePtr(raw.Body, &res, "datastores")
+	return res, err
+}
+
+type MysqlEngineVersionInfo struct {
+	// DB version ID. Its value is unique.
+	Id string `json:"id"`
+	// DB version number. Only the major version number with two digits is returned.
+	Name string `json:"name"`
+}
