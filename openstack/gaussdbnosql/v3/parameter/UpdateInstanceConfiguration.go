a5e43c6d2ca37f1d569cd8dca8b0cc756d9b1845
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-28 16:48:43 +0200

    UpdateConfiguration

--- openstack/gaussdbnosql/v3/parameter/UpdateInstanceConfiguration.go
+++ openstack/gaussdbnosql/v3/parameter/UpdateInstanceConfiguration.go
@@ -1,1 +1,17 @@
 package parameter
+
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
+)
+
+func UpdateInstanceConfiguration(client *golangsdk.ServiceClient, opts UpdateInstanceNameOpts) (err error) {
+	b, err := build.RequestBody(opts, "")
+	if err != nil {
+		return
+	}
+
+	// PUT https://{Endpoint}/v3/{project_id}/instances/{instance_id}/name
+	_, err = client.Put(client.ServiceURL("instances", opts.InstanceId, "name"), b, nil, nil)
+	return
+}
