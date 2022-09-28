b1ee5dd212cd131b87383045b4730f3a25e0c8c0
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-28 17:07:43 +0200

    UpdateInstanceConfiguration

--- openstack/gaussdbnosql/v3/parameter/UpdateInstanceConfiguration.go
+++ openstack/gaussdbnosql/v3/parameter/UpdateInstanceConfiguration.go
@@ -3,15 +3,35 @@ package parameter
 import (
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
 )
 
-func UpdateInstanceConfiguration(client *golangsdk.ServiceClient, opts UpdateInstanceNameOpts) (err error) {
+type UpdateInstanceConfigurationOpts struct {
+	InstanceId string
+	// The parameter values defined by users based on the default parameter template
+	Values map[string]string `json:"values"`
+}
+
+func UpdateInstanceConfiguration(client *golangsdk.ServiceClient, opts UpdateInstanceConfigurationOpts) (*UpdateInstanceConfigurationResponse, error) {
 	b, err := build.RequestBody(opts, "")
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	// PUT https://{Endpoint}/v3/{project_id}/instances/{instance_id}/name
-	_, err = client.Put(client.ServiceURL("instances", opts.InstanceId, "name"), b, nil, nil)
-	return
+	// PUT https://{Endpoint}/v3/{project_id}/instances/{instance_id}/configurations
+	raw, err := client.Put(client.ServiceURL("instances", opts.InstanceId, "configurations"), b, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res UpdateInstanceConfigurationResponse
+	err = extract.Into(raw.Body, &res)
+	return &res, err
+}
+
+type UpdateInstanceConfigurationResponse struct {
+	// ID of the asynchronous task for modifying instance parameters
+	JobId string `json:"job_id,omitempty"`
+	// Whether the instance needs to be restarted.
+	RestartRequired bool `json:"restart_required,omitempty"`
 }
