a0a8f1dd5d792db39040b680f9f992b464b0410e
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-28 12:41:13 +0200

    Delete

--- openstack/gaussdbnosql/v3/instance/DeleteInstance.go
+++ openstack/gaussdbnosql/v3/instance/DeleteInstance.go
@@ -1,1 +1,20 @@
 package instance
+
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+)
+
+func Delete(client *golangsdk.ServiceClient, instanceId string) (string, error) {
+	// DELETE https://{Endpoint}/v3/{project_id}/instances/{instance_id}
+	raw, err := client.Delete(client.ServiceURL("instances", instanceId), nil)
+	if err != nil {
+		return "", err
+	}
+
+	var res struct {
+		JobId string `json:"job_id"`
+	}
+	err = extract.Into(raw.Body, &res)
+	return res.JobId, err
+}
