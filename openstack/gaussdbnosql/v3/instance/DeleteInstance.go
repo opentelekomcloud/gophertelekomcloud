51647d66dfaea960727f3d9f6676a9e1ebd7e8a9
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-28 12:59:52 +0200

    ResizeInstanceVolume

--- openstack/gaussdbnosql/v3/instance/DeleteInstance.go
+++ openstack/gaussdbnosql/v3/instance/DeleteInstance.go
@@ -1,6 +1,8 @@
 package instance
 
 import (
+	"net/http"
+
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
 )
@@ -8,6 +10,10 @@ import (
 func DeleteInstance(client *golangsdk.ServiceClient, instanceId string) (string, error) {
 	// DELETE https://{Endpoint}/v3/{project_id}/instances/{instance_id}
 	raw, err := client.Delete(client.ServiceURL("instances", instanceId), nil)
+	return extraJob(err, raw)
+}
+
+func extraJob(err error, raw *http.Response) (string, error) {
 	if err != nil {
 		return "", err
 	}
