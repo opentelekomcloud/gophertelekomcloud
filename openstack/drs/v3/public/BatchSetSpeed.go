9d9db08a154ce328f2d0a8e7342a6593e41a934f
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-30 13:33:14 +0200

    BatchSetSpeed

--- openstack/drs/v3/public/BatchSetSpeed.go
+++ openstack/drs/v3/public/BatchSetSpeed.go
@@ -1,1 +1,29 @@
 package public
+
+type BatchLimitSpeedReq struct {
+	SpeedLimits []LimitSpeedReq `json:"speed_limits"`
+}
+
+type LimitSpeedReq struct {
+	// Task ID.
+	JobId string `json:"job_id"`
+	// Request body of flow control information.
+	SpeedLimit []SpeedLimitInfo `json:"speed_limit"`
+}
+
+type SpeedLimitInfo struct {
+	// Start time (UTC) of flow control. The start time is an integer in hh:mm format and the minutes part is ignored.
+	// hh indicates the hour, for example, 01:00.
+	Begin string `json:"begin"`
+	// End time (UTC) in the format of hh:mm, for example, 15:59. The value must end with 59.
+	End string `json:"end"`
+	// Speed. The value ranges from 1 to 9,999, in MB/s.
+	Speed string `json:"speed"`
+}
+
+// PUT /v3/{project_id}/jobs/batch-limit-speed
+
+type BatchSetSpeedResponse struct {
+	Count   int32           `json:"count,omitempty"`
+	Results []ModifyJobResp `json:"results,omitempty"`
+}
