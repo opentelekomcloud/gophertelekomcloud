41d8f7b4e123baf9e8740b0fbbd34770db7e02bb
Aloento <11802769+Aloento@users.noreply.github.com>
2022-09-30 13:43:39 +0200

    BatchStartJobs

--- openstack/drs/v3/public/BatchStartJobs.go
+++ openstack/drs/v3/public/BatchStartJobs.go
@@ -1,1 +1,32 @@
 package public
+
+type BatchStartJobReq struct {
+	// Request list for starting tasks in batches.
+	Jobs []StartInfo `json:"jobs"`
+}
+
+type StartInfo struct {
+	// Task ID.
+	JobId string `json:"job_id"`
+	// Task start time. The timestamp is accurate to milliseconds, for example, 1608188903063.
+	// If the value is empty, the task is started immediately.
+	StartTime string `json:"start_time,omitempty"`
+}
+
+// POST /v3/{project_id}/jobs/batch-starting
+
+type BatchStartJobsResponse struct {
+	Results []StartJobResp `json:"results,omitempty"`
+	Count   int32          `json:"count,omitempty"`
+}
+
+type StartJobResp struct {
+	// Task ID.
+	Id string `json:"id"`
+	// Status Values: success failed
+	Status string `json:"status,omitempty"`
+	// Error code, which is optional and indicates the returned information about the failure status.
+	ErrorCode string `json:"error_code,omitempty"`
+	// Error message, which is optional and indicates the returned information about the failure status.
+	ErrorMsg string `json:"error_msg,omitempty"`
+}
