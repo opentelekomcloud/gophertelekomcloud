c33316defa8d9431f3fd51b1f59c7a050914f287
Aloento <11802769+Aloento@users.noreply.github.com>
2022-10-05 16:51:38 +0200

    SwitchVIP

--- openstack/drs/v3/sync/SwitchVIP.go
+++ openstack/drs/v3/sync/SwitchVIP.go
@@ -1,3 +1,12 @@
 package sync
 
+type SwitchVIPOpts struct {
+	// Switchover type. Values:
+	// source: Switch over the virtual IP address of the source database.
+	// target: Switch over the virtual IP address of the destination database.
+	Mode string `json:"mode"`
+}
+
 // POST  /v3/{project_id}/jobs/{job_id}/switch-vip
+
+// IdJobResp
