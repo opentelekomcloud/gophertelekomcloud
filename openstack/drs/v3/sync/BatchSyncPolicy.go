3f76a80105ea497f30ac8a60dfca30e6358f3283
Aloento <11802769+Aloento@users.noreply.github.com>
2022-10-06 08:42:08 +0200

    BatchSyncPolicy

--- openstack/drs/v3/sync/BatchSyncPolicy.go
+++ openstack/drs/v3/sync/BatchSyncPolicy.go
@@ -1,5 +1,12 @@
 package sync
 
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/drs/v3/public"
+)
+
 type BatchSyncPolicyOpts struct {
 	Jobs []SyncPolicyReq `json:"jobs"`
 }
@@ -23,6 +30,19 @@ type SyncPolicyReq struct {
 	JobId string `json:"job_id"`
 }
 
-// POST /v3/{project_id}/jobs/batch-sync-policy
+func BatchSyncPolicy(client *golangsdk.ServiceClient, opts BatchSyncPolicyOpts) (*public.IdJobResp, error) {
+	b, err := build.RequestBody(opts, "")
+	if err != nil {
+		return nil, err
+	}
 
-// IdJobResp
+	// POST /v3/{project_id}/jobs/batch-sync-policy
+	raw, err := client.Post(client.ServiceURL("jobs", "batch-sync-policy"), b, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res public.IdJobResp
+	err = extract.Into(raw.Body, &res)
+	return &res, err
+}
