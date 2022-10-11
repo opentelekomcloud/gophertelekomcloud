8584e4642f00ec97fd67e6a3c8b50e90a21b4504
Aloento <11802769+Aloento@users.noreply.github.com>
2022-10-11 14:50:47 +0200

    DeleteCluster

--- openstack/dws/v1/snapshot/DeleteSnapshot.go
+++ openstack/dws/v1/snapshot/DeleteSnapshot.go
@@ -1,11 +1,9 @@
 package snapshot
 
-type DeleteSnapshotRequest struct {
-	// Snapshot ID
-	SnapshotId string `json:"snapshot_id"`
-}
+import golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 
-// DELETE /v1.0/{project_id}/snapshots/{snapshot_id}
-
-type DeleteSnapshotResponse struct {
+func DeleteCluster(client *golangsdk.ServiceClient, snapshotId string) error {
+	// DELETE /v1.0/{project_id}/snapshots/{snapshot_id}
+	_, err := client.Delete(client.ServiceURL("snapshots", snapshotId), nil)
+	return err
 }
