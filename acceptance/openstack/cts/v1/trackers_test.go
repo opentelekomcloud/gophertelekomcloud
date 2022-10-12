5bb7e194991e28c90bfd52a961c2ffa96db6eced
Aloento <11802769+Aloento@users.noreply.github.com>
2022-10-12 17:32:19 +0200

    fix

--- acceptance/openstack/cts/v1/trackers_test.go
+++ acceptance/openstack/cts/v1/trackers_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/pointerto"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/cts/v1/tracker"
 	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
 )
@@ -23,41 +24,42 @@ func TestTrackersLifecycle(t *testing.T) {
 	})
 
 	t.Logf("Attempting to create CTSv1 Tracker")
-	f := false
-	createOpts := tracker.CreateOpts{
+	ctsTracker, err := tracker.Create(client, tracker.CreateOpts{
 		BucketName: bucketName,
 		Lts: tracker.CreateLts{
-			IsLtsEnabled: &f,
+			IsLtsEnabled: pointerto.Bool(true),
 		},
-	}
-	ctsTracker, err := tracker.Create(client, createOpts)
+	})
+
 	t.Cleanup(func() {
 		t.Logf("Attempting to delete CTSv1 Tracker: %s", ctsTracker.TrackerName)
 		err := tracker.Delete(client)
 		th.AssertNoErr(t, err)
 		t.Logf("Deleted CTSv1 Tracker: %s", ctsTracker.TrackerName)
 	})
+
 	th.AssertNoErr(t, err)
 	t.Logf("Created CTSv1 Tracker: %s", ctsTracker.TrackerName)
 	th.AssertEquals(t, true, ctsTracker.Lts.IsLtsEnabled)
 	th.AssertEquals(t, "enabled", ctsTracker.Status)
 
 	t.Logf("Attempting to update CTSv1 Tracker: %s", ctsTracker.TrackerName)
-	updateOpts := tracker.UpdateOpts{
+	_, err = tracker.Update(client, tracker.UpdateOpts{
 		BucketName: bucketName,
 		Status:     "disabled",
-	}
-	_, err = tracker.Update(client, updateOpts)
+	})
 	th.AssertNoErr(t, err)
 	t.Logf("Updated CTSv1 Tracker: %s", ctsTracker.TrackerName)
 
-	listOpts := tracker.ListOpts{
+	trackerList, err := tracker.List(client, tracker.ListOpts{
 		TrackerName: ctsTracker.TrackerName,
-	}
-	trackerList, err := tracker.List(client, listOpts)
+	})
 	th.AssertNoErr(t, err)
 	if len(trackerList) == 0 {
 		t.Fatalf("CTS tracker wasn't found: %s", ctsTracker.TrackerName)
 	}
-	th.AssertEquals(t, updateOpts.Status, trackerList[0].Status)
+	th.AssertEquals(t, tracker.UpdateOpts{
+		BucketName: bucketName,
+		Status:     "disabled",
+	}.Status, trackerList[0].Status)
 }
