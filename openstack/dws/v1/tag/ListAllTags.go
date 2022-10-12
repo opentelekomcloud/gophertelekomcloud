7c664d2a5b0da9db15f28ecd60e53d265664913d
Aloento <11802769+Aloento@users.noreply.github.com>
2022-10-12 15:50:24 +0200

    ListAllTags

--- openstack/dws/v1/tag/ListAllTags.go
+++ openstack/dws/v1/tag/ListAllTags.go
@@ -1,17 +1,23 @@
 package tag
 
-type ListAllTagsRequest struct {
-}
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+)
 
-// GET /v1.0/{project_id}/clusters/tags
+func ListAllTags(client *golangsdk.ServiceClient) ([]TagWithMultiValue, error) {
+	// GET /v1.0/{project_id}/clusters/tags
+	raw, err := client.Get(client.ServiceURL("clusters", "tags"), nil, nil)
+	if err != nil {
+		return nil, err
+	}
 
-type ListAllTagsResponse struct {
-	// Tag list.
-	Tags []TagWithMultiValue `json:"tags,omitempty"`
+	var res []TagWithMultiValue
+	err = extract.IntoSlicePtr(raw.Body, &res, "tags")
+	return res, err
 }
 
 type TagWithMultiValue struct {
-
 	// Tag key. A tag key can contain a maximum of 127 Unicode characters, which cannot be null. The first and last characters cannot be spaces.
 	// It can contain uppercase letters (A to Z), lowercase letters (a to z), digits (0-9), hyphens (-), and underscores (_).
 	Key string `json:"key"`
