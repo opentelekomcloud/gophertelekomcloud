a47f5b7859e21f1553be235e2fdcd686ff2c24a7
Aloento <11802769+Aloento@users.noreply.github.com>
2022-10-12 15:46:50 +0200

    BatchAddDeleteTags

--- openstack/dws/v1/tag/BatchAddDeleteTags.go
+++ openstack/dws/v1/tag/BatchAddDeleteTags.go
@@ -1,14 +1,14 @@
 package tag
 
-type BatchCreateClusterTagsRequest struct {
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
+)
+
+type BatchCreateClusterTagsOpts struct {
 	// Resource ID, for example, 7d85f602-a948-4a30-afd4-e84f47471c15.
-	ClusterId string `json:"resource_id"`
-
-	Body BatchCreateClusterTagsReq `json:"body,omitempty"`
-}
-
-type BatchCreateClusterTagsReq struct {
-	// Identifies the operation. The value can be create or delete.
+	ClusterId string
+	// Identifies the operation. The value can be created or delete.
 	// create: adds tags in batches.
 	// delete: deletes tags in batches.
 	Action string `json:"action"`
@@ -16,4 +16,15 @@ type BatchCreateClusterTagsReq struct {
 	Tags []Tag `json:"tags"`
 }
 
-// POST /v1.0/{project_id}/clusters/{resource_id}/tags/action
+func BatchAddDeleteTags(client *golangsdk.ServiceClient, opts BatchCreateClusterTagsOpts) (err error) {
+	b, err := build.RequestBody(opts, "")
+	if err != nil {
+		return
+	}
+
+	// POST /v1.0/{project_id}/clusters/{resource_id}/tags/action
+	_, err = client.Post(client.ServiceURL("clusters", opts.ClusterId, "tags", "action"), b, nil, &golangsdk.RequestOpts{
+		OkCodes: []int{200},
+	})
+	return
+}
