9f26633762fc5fa5b00b8b320cd6a571fff38a93
Aloento <11802769+Aloento@users.noreply.github.com>
2022-10-19 10:50:56 +0200

    List

--- openstack/csbs/v1/backup/List.go
+++ openstack/csbs/v1/backup/List.go
@@ -10,18 +10,44 @@ import (
 // the API. Filtering is achieved by passing in struct field values that map to
 // the attributes you want to see returned. Marker and Limit are used for pagination.
 type ListOpts struct {
-	Status       string `q:"status"`
-	Limit        string `q:"limit"`
-	Marker       string `q:"marker"`
-	Sort         string `q:"sort"`
-	AllTenants   string `q:"all_tenants"`
-	Name         string `q:"name"`
-	ResourceId   string `q:"resource_id"`
+	// Query based on field status is supported.
+	// Value range: waiting_protect, protecting, available, waiting_restore, restoring, error, waiting_delete, deleting, and deleted
+	Status string `q:"status"`
+	// Number of resources displayed per page. The value must be a positive integer. The value defaults to 1000.
+	Limit string `q:"limit"`
+	// ID of the last record displayed on the previous page
+	Marker string `q:"marker"`
+	// A group of properties separated by commas (,) and sorting directions. The value format is <key1>[:<direction>],
+	// <key2>[:<direction>], where the value of direction is asc (in ascending order) or desc (in descending order).
+	// If the parameter direction is not specified, the default sorting direction is desc.
+	// The value of sort contains a maximum of 255 characters. Enumeration values of the key are as follows:
+	// created_at, updated_at, name, status, protected_at, and id.
+	Sort string `q:"sort"`
+	// Whether to query the backup of all tenants. Only administrators can query the backup of all tenants.
+	AllTenants string `q:"all_tenants"`
+	// Fuzzy search based on field name is supported.
+	Name string `q:"name"`
+	// Filtering based on the backup AZ is supported.
+	Az string `q:"az"`
+	// Filtering based on the backup object ID is supported.
+	ResourceId string `q:"resource_id"`
+	// Fuzzy search based on the backup object name is supported.
 	ResourceName string `q:"resource_name"`
-	PolicyId     string `q:"policy_id"`
-	VmIp         string `q:"ip"`
+	// Filtering based on the backup start time is supported.
+	// For example: 2017-04-18T01:21:52.701973
+	StartTime string `q:"start_time"`
+	// Filtering based on the backup end time is supported.
+	// For example: 2017-04-18T01:21:52.701973
+	EndTime string `q:"end_time"`
+	// Supports filtering by image type, for example, backup.
+	ImageType string `q:"image_type"`
+	// Filtering based on policy_id is supported.
+	PolicyId string `q:"policy_id"`
+	// Offset value, which is a positive integer.
+	Offset string `q:"offset"`
+	// Filtering based on checkpoint_id is supported.
 	CheckpointId string `q:"checkpoint_id"`
-	ID           string
+	// Type of the backup object. For example, OS::Nova::Server
 	ResourceType string `q:"resource_type"`
 }
 
@@ -34,6 +60,7 @@ func List(c *golangsdk.ServiceClient, opts ListOpts) ([]Backup, error) {
 		return nil, err
 	}
 
+	// GET https://{endpoint}/v1/{project_id}/checkpoint_items
 	pages, err := pagination.NewPager(c, c.ServiceURL("checkpoint_items")+q.String(),
 		func(r pagination.PageResult) pagination.Page {
 			return СsbsBackupPage{pagination.LinkedPageBase{PageResult: r}}
@@ -43,15 +70,7 @@ func List(c *golangsdk.ServiceClient, opts ListOpts) ([]Backup, error) {
 	}
 
 	allBackups, err := ExtractBackups(pages)
-	if err != nil {
-		return nil, err
-	}
-
-	if opts.ID != "" {
-		return filterBackupsById(allBackups, opts.ID)
-	}
-
-	return allBackups, nil
+	return allBackups, err
 }
 
 // СsbsBackupPage is the page returned by a pager when traversing over a
@@ -88,17 +107,4 @@ func ExtractBackups(r pagination.Page) ([]Backup, error) {
 		return nil, err
 	}
 	return res, nil
-}
-
-func filterBackupsById(backups []Backup, filterId string) ([]Backup, error) {
-	var refinedBackups []Backup
-
-	for _, backup := range backups {
-
-		if filterId == backup.Id {
-			refinedBackups = append(refinedBackups, backup)
-		}
-	}
-
-	return refinedBackups, nil
 }
