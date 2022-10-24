1b735d479bd971c01f9fe07605d4b9f0489ea410
Aloento <11802769+Aloento@users.noreply.github.com>
2022-10-24 10:34:45 +0200

    CreateEvents

---
+++ openstack/ces/v1/events/ListEvents.go
@@ -0,0 +1,18 @@
+package events
+
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+)
+
+func ListEvents(client *golangsdk.ServiceClient, id string) ([]MetricAlarms, error) {
+	// GET /V1.0/{project_id}/alarms/{alarm_id}
+	raw, err := client.Get(client.ServiceURL("alarms", id), nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res []MetricAlarms
+	err = extract.IntoSlicePtr(raw.Body, &res, "metric_alarms")
+	return res, err
+}
