f04f739a9279fe5ef824dbfd4a1f2e62297b2481
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-03 15:42:22 +0100

    List

--- openstack/evs/v3/volumes/List.go
+++ openstack/evs/v3/volumes/List.go
@@ -8,25 +8,34 @@ import (
 
 // ListOpts holds options for listing Volumes. It is passed to the volumes.List function.
 type ListOpts struct {
-	// AllTenants will retrieve volumes of all tenants/projects.
-	AllTenants bool `q:"all_tenants"`
+	// Specifies the AZ.
+	AvailabilityZone bool `q:"availability_zone"`
 	// Metadata will filter results based on specified metadata.
 	Metadata map[string]string `q:"metadata"`
-	// Name will filter by the specified volume name.
+	// Specifies the disk name. The value can contain a maximum of 255 bytes.
 	Name string `q:"name"`
 	// Status will filter by the specified status.
 	Status string `q:"status"`
 	// TenantID will filter by a specific tenant/project ID.
 	// Setting AllTenants is required for this.
 	TenantID string `q:"project_id"`
-	// Comma-separated list of sort keys and optional sort directions in the
-	// form of <key>[:<direction>].
-	Sort string `q:"sort"`
-	// Requests a page size of items.
+	// Specifies the keyword based on which the returned results are sorted.
+	// The value can be id, status, size, or created_at, and the default value is created_at.
+	Sort string `q:"sort_key"`
+	// Specifies the result sorting order. The default value is desc.
+	// desc: specifies the descending order.
+	// asc: specifies the ascending order.
+	SortDir string `q:"sort_dir"`
+	// Specifies the maximum number of query results that can be returned.
+	// The value ranges from 1 to 1000, and the default value is 1000. The returned value cannot exceed this limit.
+	// If the tenant has more than 50 disks in total, you are advised to use this parameter and set its value to 50 to improve the query efficiency.
+	// Examples are provided as follows:
+	// GET /v3/xxx/volumes?limit=50: Queries the 1–50 disks. GET /v3/xxx/volumes?offset=50&limit=50: Queries the 51–100 disks.
 	Limit int `q:"limit"`
-	// Used in conjunction with limit to return a slice of items.
+	// Specifies the offset.
+	// All disks after this offset will be queried. The value must be an integer greater than 0 but less than the number of disks.
 	Offset int `q:"offset"`
-	// The ID of the last-seen item.
+	// Specifies the ID of the last record on the previous page. The returned value is the value of the item after this one.
 	Marker string `q:"marker"`
 }
 
@@ -37,7 +46,8 @@ func List(client *golangsdk.ServiceClient, opts ListOpts) pagination.Pager {
 		return pagination.Pager{Err: err}
 	}
 
-	return pagination.NewPager(client, client.ServiceURL("volumes", "detail")+q.String(),
+	// GET /v3/{project_id}/volumes
+	return pagination.NewPager(client, client.ServiceURL("volumes")+q.String(),
 		func(r pagination.PageResult) pagination.Page {
 			return VolumePage{pagination.LinkedPageBase{PageResult: r}}
 		})
