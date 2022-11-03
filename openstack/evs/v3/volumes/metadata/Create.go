83c83947c05fa32d19ef3a1459d89a0ff07deb27
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-03 16:51:28 +0100

    Create

--- openstack/evs/v3/volumes/metadata/Create.go
+++ openstack/evs/v3/volumes/metadata/Create.go
@@ -1,1 +1,26 @@
 package metadata
+
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+)
+
+func Create(client *golangsdk.ServiceClient, volumeId string, opts map[string]string) (map[string]string, error) {
+	b, err := build.RequestBody(opts, "metadata")
+	if err != nil {
+		return nil, err
+	}
+
+	// POST /v3/{project_id}/volumes/{volume_id}/metadata
+	raw, err := client.Post(client.ServiceURL("volumes", volumeId, "metadata"), b, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res struct {
+		Metadata map[string]string `json:"metadata"`
+	}
+	err = extract.Into(raw.Body, &res)
+	return res.Metadata, err
+}
