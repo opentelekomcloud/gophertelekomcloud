4b4036e3b4473a92cfd9ce498878dee2d44aec72
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-07 14:50:52 +0100

    Get

--- openstack/evs/v3/snapshots/metadata/Get.go
+++ openstack/evs/v3/snapshots/metadata/Get.go
@@ -2,8 +2,9 @@ package metadata
 
 import golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 
-func Get(client *golangsdk.ServiceClient, volumeId string) (map[string]string, error) {
-	// GET /v3/{project_id}/volumes/{volume_id}/metadata
-	raw, err := client.Get(client.ServiceURL("volumes", volumeId, "metadata"), nil, nil)
+// Get If metadata contains the __system__enableActive field, the snapshot is automatically created during the backup of a server.
+func Get(client *golangsdk.ServiceClient, snapshotId string) (map[string]string, error) {
+	// GET /v3/{project_id}/snapshots/{snapshot_id}/metadata
+	raw, err := client.Get(client.ServiceURL("snapshots", snapshotId, "metadata"), nil, nil)
 	return extraMetadata(err, raw)
 }
