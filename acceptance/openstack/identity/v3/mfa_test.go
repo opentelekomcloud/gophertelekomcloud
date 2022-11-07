ff1de563dc1d43b2949364d3b667f5bd6ea3e293
anton-sidelnikov <a.sidelnikov@t-systems.com>
2022-11-07 12:12:39 +0100

    changed test (left only queries)

--- acceptance/openstack/identity/v3/mfa_test.go
+++ acceptance/openstack/identity/v3/mfa_test.go
@@ -1,15 +1,11 @@
 package v3
 
 import (
-	"encoding/base32"
 	"os"
 	"testing"
-	"time"
 
-	"github.com/lucasbbb/otp"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
-	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/pointerto"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/identity/v3/users"
 	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
 )
@@ -21,55 +17,11 @@ func TestMFA(t *testing.T) {
 	client, err := clients.NewIdentityV3AdminClient()
 	th.AssertNoErr(t, err)
 
-	user, err := users.Create(client, users.CreateOpts{
-		Name:    tools.RandomString("user-name-", 4),
-		Enabled: pointerto.Bool(true),
-	}).Extract()
-	if err != nil {
-		t.Fatalf("Unable to create user: %v", err)
-	}
-
-	t.Cleanup(func() {
-		err = users.Delete(client, user.ID).ExtractErr()
-		th.AssertNoErr(t, err)
-	})
-
-	mfa, err := users.CreateMfaDevice(client, users.CreateMfaDeviceOpts{
-		Name:   "test-mfa",
-		UserId: user.ID,
-	})
-	t.Cleanup(func() {
-		err = users.DeleteMfaDevice(client, users.DeleteMfaDeviceOpts{
-			UserId:       user.ID,
-			SerialNumber: mfa.SerialNumber,
-		})
-		th.AssertNoErr(t, err)
-	})
-
-	th.AssertNoErr(t, err)
-	t.Logf("MFA device created: %v", mfa)
-
-	secret, _ := base32.StdEncoding.WithPadding(base32.NoPadding).DecodeString(mfa.Base32StringSeed)
-	totp := otp.NewTOTP(secret)
-	err = users.CreateBindingDevice(client, users.BindMfaDevice{
-		UserId:                   user.ID,
-		SerialNumber:             mfa.SerialNumber,
-		AuthenticationCodeFirst:  totp.Generate(time.Now().Add(-30 * time.Second)),
-		AuthenticationCodeSecond: totp.Generate(time.Now()),
-	})
-	th.AssertNoErr(t, err)
-
-	err = users.DeleteBindingDevice(client, users.UnbindMfaDevice{
-		UserId:             user.ID,
-		AuthenticationCode: totp.Generate(time.Now()),
-		SerialNumber:       mfa.SerialNumber,
-	})
-
-	get, err := users.ShowUserMfaDevice(client, user.ID)
-	th.AssertNoErr(t, err)
-	th.AssertEquals(t, get.SerialNumber, mfa.SerialNumber)
-
 	list, err := users.ListUserMfaDevices(client)
 	th.AssertNoErr(t, err)
 	tools.PrintResource(t, list)
+
+	get, err := users.ShowUserMfaDevice(client, client.UserID)
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, get.UserId, client.UserID)
 }
