2a0f3e9f23195406f1af015bc0a7a21f2591ceb8
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-08 14:29:09 +0100

    Create

--- openstack/evs/extensions/volumetransfers/requests.go
+++ openstack/evs/extensions/volumetransfers/requests.go
@@ -5,35 +5,6 @@ import (
 	"github.com/opentelekomcloud/gophertelekomcloud/pagination"
 )
 
-// CreateOpts contains options for a Volume transfer.
-type CreateOpts struct {
-	// The ID of the volume to transfer.
-	VolumeID string `json:"volume_id" required:"true"`
-
-	// The name of the volume transfer
-	Name string `json:"name,omitempty"`
-}
-
-// ToCreateMap assembles a request body based on the contents of a
-// TransferOpts.
-func (opts CreateOpts) ToCreateMap() (map[string]interface{}, error) {
-	return golangsdk.BuildRequestBody(opts, "transfer")
-}
-
-// Create will create a volume tranfer request based on the values in CreateOpts.
-func Create(client *golangsdk.ServiceClient, opts CreateOpts) (r CreateResult) {
-	b, err := opts.ToCreateMap()
-	if err != nil {
-		r.Err = err
-		return
-	}
-	resp, err := client.Post(transferURL(client), b, &r.Body, &golangsdk.RequestOpts{
-		OkCodes: []int{202},
-	})
-	_, r.Header, r.Err = golangsdk.ParseResponse(resp, err)
-	return
-}
-
 // AcceptOpts contains options for a Volume transfer accept reqeust.
 type AcceptOpts struct {
 	// The auth key of the volume transfer to accept.
@@ -53,7 +24,7 @@ func Accept(client *golangsdk.ServiceClient, id string, opts AcceptOpts) (r Crea
 		r.Err = err
 		return
 	}
-	resp, err := client.Post(acceptURL(client, id), b, &r.Body, &golangsdk.RequestOpts{
+	resp, err := client.Post(client.ServiceURL("os-volume-transfer", id, "accept"), b, &r.Body, &golangsdk.RequestOpts{
 		OkCodes: []int{202},
 	})
 	_, r.Header, r.Err = golangsdk.ParseResponse(resp, err)
@@ -62,7 +33,7 @@ func Accept(client *golangsdk.ServiceClient, id string, opts AcceptOpts) (r Crea
 
 // Delete deletes a volume transfer.
 func Delete(client *golangsdk.ServiceClient, id string) (r DeleteResult) {
-	resp, err := client.Delete(deleteURL(client, id), nil)
+	resp, err := client.Delete(client.ServiceURL("os-volume-transfer", id), nil)
 	_, r.Header, r.Err = golangsdk.ParseResponse(resp, err)
 	return
 }
@@ -101,7 +72,7 @@ func (opts ListOpts) ToTransferListQuery() (string, error) {
 
 // List returns Transfers optionally limited by the conditions provided in ListOpts.
 func List(client *golangsdk.ServiceClient, opts ListOptsBuilder) pagination.Pager {
-	url := listURL(client)
+	url := client.ServiceURL("os-volume-transfer", "detail")
 	if opts != nil {
 		query, err := opts.ToTransferListQuery()
 		if err != nil {
@@ -118,7 +89,7 @@ func List(client *golangsdk.ServiceClient, opts ListOptsBuilder) pagination.Page
 // Get retrieves the Transfer with the provided ID. To extract the Transfer object
 // from the response, call the Extract method on the GetResult.
 func Get(client *golangsdk.ServiceClient, id string) (r GetResult) {
-	resp, err := client.Get(getURL(client, id), &r.Body, nil)
+	resp, err := client.Get(client.ServiceURL("os-volume-transfer", id), &r.Body, nil)
 	_, r.Header, r.Err = golangsdk.ParseResponse(resp, err)
 	return
 }
--- openstack/evs/extensions/volumetransfers/results.go
+++ openstack/evs/extensions/volumetransfers/results.go
@@ -2,20 +2,29 @@ package volumetransfers
 
 import (
 	"encoding/json"
+	"net/http"
 	"time"
 
 	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
 	"github.com/opentelekomcloud/gophertelekomcloud/pagination"
 )
 
 // Transfer represents a Volume Transfer record
 type Transfer struct {
-	ID        string              `json:"id"`
-	AuthKey   string              `json:"auth_key"`
-	Name      string              `json:"name"`
-	VolumeID  string              `json:"volume_id"`
-	CreatedAt time.Time           `json:"-"`
-	Links     []map[string]string `json:"links"`
+	// Specifies the disk transfer ID.
+	ID string `json:"id"`
+	// Specifies the authentication key of the disk transfer.
+	AuthKey string `json:"auth_key"`
+	// Specifies the disk transfer name.
+	Name string `json:"name"`
+	// Specifies the disk ID.
+	VolumeID string `json:"volume_id"`
+	// Specifies the time when the disk transfer was created.
+	// Time format: UTC YYYY-MM-DDTHH:MM:SS.XXXXXX
+	CreatedAt time.Time `json:"-"`
+	// Specifies the links of the disk transfer.
+	Links []map[string]string `json:"links"`
 }
 
 // UnmarshalJSON is our unmarshalling helper
@@ -34,6 +43,16 @@ func (r *Transfer) UnmarshalJSON(b []byte) error {
 	r.CreatedAt = time.Time(s.CreatedAt)
 
 	return err
+}
+
+func extra(err error, raw *http.Response) (*Transfer, error) {
+	if err != nil {
+		return nil, err
+	}
+
+	var res Transfer
+	err = extract.IntoStructPtr(raw.Body, &res, "transfer")
+	return &res, err
 }
 
 type commonResult struct {
