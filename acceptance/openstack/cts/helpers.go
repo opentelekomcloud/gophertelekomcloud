93e95b5b2b09e348ca1603f82f556d6fbcf9b52c
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-11 19:07:31 +0100

    [CTS] New KeyEventNotification/Traces & Update Tracker API (#427)
    [CTS] New KeyEventNotification/Traces & Update Tracker API
    
    Move all API methods and related structures to a separate file.
    Inline all Extracts and use the extract package instead.
    Remove all Builders
    tracker/List -> Get
    New:
    KeyEventNotification
    traces/List
    fix #422
    
    Reviewed-by: Anton Sidelnikov
    Reviewed-by: Aloento
    Reviewed-by: Polina Gubina
    Reviewed-by: Artem Lifshits

--- acceptance/openstack/cts/helpers.go
+++ acceptance/openstack/cts/helpers.go
@@ -1,4 +1,4 @@
-package v1
+package cts
 
 import (
 	"strings"
@@ -7,11 +7,10 @@ import (
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/obs"
-	"github.com/opentelekomcloud/gophertelekomcloud/openstack/smn/v2/topics"
 	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
 )
 
-func createOBSBucket(t *testing.T) string {
+func CreateOBSBucket(t *testing.T) string {
 	t.Logf("Attempting to create OBS bucket")
 	client, err := clients.NewOBSClientWithoutHeader()
 	th.AssertNoErr(t, err)
@@ -30,7 +29,7 @@ func createOBSBucket(t *testing.T) string {
 	return bucketName
 }
 
-func deleteOBSBucket(t *testing.T, bucketName string) {
+func DeleteOBSBucket(t *testing.T, bucketName string) {
 	t.Logf("Attempting to delete OBS bucket: %s", bucketName)
 	client, err := clients.NewOBSClientWithoutHeader()
 	th.AssertNoErr(t, err)
@@ -56,31 +55,4 @@ func deleteOBSBucket(t *testing.T, bucketName string) {
 	_, err = client.DeleteBucket(bucketName)
 	th.AssertNoErr(t, err)
 	t.Logf("Deleted OBS Bucket: %s", bucketName)
-}
-
-func createSMNTopic(t *testing.T) *topics.Topic {
-	t.Logf("Attempting to create SMNv2 topic")
-	client, err := clients.NewSmnV2Client()
-	th.AssertNoErr(t, err)
-
-	smnTopicName := strings.ToLower(tools.RandomString("smn-cts-test", 5))
-
-	createOpts := topics.CreateOps{
-		Name: smnTopicName,
-	}
-
-	smnTopic, err := topics.Create(client, createOpts).Extract()
-	th.AssertNoErr(t, err)
-
-	t.Logf("Created SMNv2 Topic: %s", smnTopic.TopicUrn)
-	return smnTopic
-}
-
-func deleteSMNTopic(t *testing.T, topicUrn string) {
-	t.Logf("Attempting to delete SMNv2 topic: %s", topicUrn)
-	client, err := clients.NewSmnV2Client()
-	th.AssertNoErr(t, err)
-
-	th.AssertNoErr(t, topics.Delete(client, topicUrn).ExtractErr())
-	t.Logf("Deleted SMNv2 Topic: %s", topicUrn)
 }
