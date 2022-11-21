a67c8aa0d37e30fcaa1bf6dc8d50e792ae9d9deb
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-21 11:19:57 +0100

    List

--- openstack/rds/v3/datastores/List.go
+++ openstack/rds/v3/datastores/List.go
@@ -5,12 +5,26 @@ import (
 	"github.com/opentelekomcloud/gophertelekomcloud/pagination"
 )
 
+func List(client *golangsdk.ServiceClient, databasesname string) pagination.Pager {
+	url := client.ServiceURL("datastores", databasesname)
+
+	pageRdsList := pagination.NewPager(client, url, func(r pagination.PageResult) pagination.Page {
+		return DataStoresPage{pagination.SinglePageBase(r)}
+	})
+
+	rdsheader := map[string]string{"Content-Type": "application/json"}
+	pageRdsList.Headers = rdsheader
+	return pageRdsList
+}
+
 type DataStoresResult struct {
 	golangsdk.Result
 }
+
 type DataStores struct {
 	DataStores []dataStores `json:"dataStores" `
 }
+
 type dataStores struct {
 	Id   string `json:"id" `
 	Name string `json:"name"`
