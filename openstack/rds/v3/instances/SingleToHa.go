2e0b65db6684af2578a23fe6cc7b765aedfb55e0
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-23 13:08:59 +0100

    SingleToHa

--- openstack/rds/v3/instances/SingleToHa.go
+++ openstack/rds/v3/instances/SingleToHa.go
@@ -1,40 +1,31 @@
 package instances
 
-import "github.com/opentelekomcloud/gophertelekomcloud"
+import (
+	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
+)
 
 type SingleToHaRdsOpts struct {
-	//
-	SingleToHa *SingleToHaRds `json:"single_to_ha" required:"true"`
+	InstanceId string
+	SingleToHa SingleToHaRds `json:"single_to_ha" required:"true"`
 }
 
 type SingleToHaRds struct {
-	//
+	// Specifies the AZ code of the DB instance node.
 	AzCodeNewNode string `json:"az_code_new_node" required:"true"`
-	//
+	// This parameter is mandatory only when a Microsoft SQL Server DB instance type is changed from single to primary/standby.
 	Password string `json:"password,omitempty"`
 }
 
-type SingleToRdsHaBuilder interface {
-	ToSingleToRdsHaMap() (map[string]interface{}, error)
-}
-
-func (opts SingleToHaRdsOpts) ToSingleToRdsHaMap() (map[string]interface{}, error) {
-	b, err := golangsdk.BuildRequestBody(&opts, "")
+func SingleToHa(client *golangsdk.ServiceClient, opts SingleToHaRdsOpts) (*string, error) {
+	b, err := build.RequestBody(&opts, "")
 	if err != nil {
 		return nil, err
 	}
-	return b, nil
-}
 
-func SingleToHa(client *golangsdk.ServiceClient, opts SingleToRdsHaBuilder, instanceId string) (r SingleToHaRdsInstanceResult) {
-	b, err := opts.ToSingleToRdsHaMap()
-	if err != nil {
-		r.Err = err
-		return
-	}
-	raw, err := client.Post(client.ServiceURL("instances", instanceId, "action"), b, nil, &golangsdk.RequestOpts{
+	// POST https://{Endpoint}/v3/{project_id}/instances/{instance_id}/action
+	raw, err := client.Post(client.ServiceURL("instances", opts.InstanceId, "action"), b, nil, &golangsdk.RequestOpts{
 		OkCodes: []int{202},
 	})
-
-	return
+	return extraJob(err, raw)
 }
