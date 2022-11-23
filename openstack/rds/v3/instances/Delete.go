de363512caf0f6dad2f455d1f901307ee6f32409
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-23 13:18:10 +0100

    Delete

--- openstack/rds/v3/instances/Delete.go
+++ openstack/rds/v3/instances/Delete.go
@@ -2,26 +2,8 @@ package instances
 
 import "github.com/opentelekomcloud/gophertelekomcloud"
 
-type DeleteInstance struct {
-	//
-	InstanceId string `json:"instance_id" required:"true"`
-}
-
-type DeleteInstanceBuilder interface {
-	ToInstancesDeleteMap() (map[string]interface{}, error)
-}
-
-func (opts DeleteInstance) ToInstancesDeleteMap() (map[string]interface{}, error) {
-	b, err := golangsdk.BuildRequestBody(&opts, "")
-	if err != nil {
-		return nil, err
-	}
-	return b, nil
-}
-
-func Delete(client *golangsdk.ServiceClient, instanceId string) (r DeleteInstanceRdsResult) {
-	url := client.ServiceURL("instances", instanceId)
-
-	raw, err := client.Delete(url, &golangsdk.RequestOpts{JSONResponse: nil, MoreHeaders: map[string]string{"Content-Type": "application/json"}})
-	return
+func Delete(client *golangsdk.ServiceClient, instanceId string) (*string, error) {
+	// DELETE https://{Endpoint}/v3/{project_id}/instances/{instance_id}
+	raw, err := client.Delete(client.ServiceURL("instances", instanceId), nil)
+	return extraJob(err, raw)
 }
