952084c6550d506d55e09370d5a8355e343e3ac6
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-28 13:27:02 +0100

    Update

--- openstack/rds/v3/configurations/Update.go
+++ openstack/rds/v3/configurations/Update.go
@@ -2,39 +2,32 @@ package configurations
 
 import (
 	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack"
 )
 
-// UpdateOptsBuilder allows extensions to add additional parameters to the
-// Update request.
-type UpdateOptsBuilder interface {
-	ToConfigUpdateMap() (map[string]interface{}, error)
-}
-
 // UpdateOpts contains all the values needed to update a Configuration.
 type UpdateOpts struct {
-	// Configuration Name
+	// Specifies the parameter template ID.
+	ConfigId string
+	// Specifies the parameter template name. It contains a maximum of 64 characters and can contain only uppercase letters, lowercase letters, digits, hyphens (-), underscores (_), and periods (.).
 	Name string `json:"name,omitempty"`
-	// Configuration Description
+	// Specifies the parameter template description. It contains a maximum of 256 characters and does not support the following special characters: !<>='&" Its value is left blank by default.
 	Description string `json:"description,omitempty"`
-	// Configuration Values
+	// Specifies the parameter values defined by users based on the default parameter template. If this parameter is left blank, the parameter value cannot be changed.
 	Values map[string]string `json:"values,omitempty"`
 }
 
-// ToConfigUpdateMap builds a update request body from UpdateOpts.
-func (opts UpdateOpts) ToConfigUpdateMap() (map[string]interface{}, error) {
-	return golangsdk.BuildRequestBody(opts, "")
-}
-
 // Update accepts a UpdateOpts struct and uses the values to update a Configuration.The response code from api is 200
-func Update(c *golangsdk.ServiceClient, id string, opts UpdateOptsBuilder) (r UpdateResult) {
-	b, err := opts.ToConfigUpdateMap()
+func Update(c *golangsdk.ServiceClient, opts UpdateOpts) (err error) {
+	b, err := build.RequestBody(opts, "")
 	if err != nil {
-		r.Err = err
 		return
 	}
-	reqOpt := &golangsdk.RequestOpts{OkCodes: []int{200},
-		MoreHeaders: openstack.StdRequestOpts().MoreHeaders}
-	raw, err := c.Put(c.ServiceURL("configurations", id), b, nil, reqOpt)
+
+	// PUT https://{Endpoint}/v3/{project_id}/configurations/{config_id}
+	_, err = c.Put(c.ServiceURL("configurations", opts.ConfigId), b, nil,
+		&golangsdk.RequestOpts{OkCodes: []int{200}, MoreHeaders: openstack.StdRequestOpts().MoreHeaders})
+
 	return
 }
