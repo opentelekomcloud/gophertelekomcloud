bdb3a4ae040c62d1ac8ac6f941af9f6a7eb9ed75
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-28 13:10:36 +0100

    List

--- openstack/rds/v3/configurations/List.go
+++ openstack/rds/v3/configurations/List.go
@@ -1,10 +1,17 @@
 package configurations
 
-import "github.com/opentelekomcloud/gophertelekomcloud"
+import (
+	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+)
 
 // List is used to obtain the parameter template list, including default
 // parameter templates of all databases and those created by users.
-func List(client *golangsdk.ServiceClient) (r ListResult) {
+func List(client *golangsdk.ServiceClient) ([]Configuration, error) {
+	// GET https://{Endpoint}/v3/{project_id}/configurations
 	raw, err := client.Get(client.ServiceURL("configurations"), nil, nil)
-	return
+
+	var res []Configuration
+	err = extract.IntoSlicePtr(raw.Body, &res, "configurations")
+	return res, err
 }
