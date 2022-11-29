e61c3bc34cc6ccd916140d1e1f4b899562126b64
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-29 15:37:16 +0100

    Delete

--- openstack/rds/v3/configurations/Delete.go
+++ openstack/rds/v3/configurations/Delete.go
@@ -6,8 +6,10 @@ import (
 )
 
 // Delete will permanently delete a particular Configuration based on its unique ID.
-func Delete(c *golangsdk.ServiceClient, id string) (r DeleteResult) {
-	raw, err := c.Delete(c.ServiceURL("configurations", id),
-		&golangsdk.RequestOpts{OkCodes: []int{200}, MoreHeaders: openstack.StdRequestOpts().MoreHeaders})
+func Delete(c *golangsdk.ServiceClient, id string) (err error) {
+	// DELETE https://{Endpoint}/v3/{project_id}/configurations/{config_id}
+	_, err = c.Delete(c.ServiceURL("configurations", id),
+		&golangsdk.RequestOpts{OkCodes: []int{200},
+			MoreHeaders: openstack.StdRequestOpts().MoreHeaders})
 	return
 }
