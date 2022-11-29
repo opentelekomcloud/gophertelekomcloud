444ddc952f5140c42ea292c8fdbeb70485dc3608
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-29 15:35:07 +0100

    Get

--- openstack/rds/v3/configurations/Get.go
+++ openstack/rds/v3/configurations/Get.go
@@ -2,11 +2,19 @@ package configurations
 
 import (
 	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack"
 )
 
 // Get retrieves a particular Configuration based on its unique ID.
-func Get(c *golangsdk.ServiceClient, id string) (r GetResult) {
-	raw, err := c.Get(c.ServiceURL("configurations", id), nil, openstack.StdRequestOpts())
-	return
+func Get(client *golangsdk.ServiceClient, id string) (*Configuration, error) {
+	// GET https://{Endpoint}/v3/{project_id}/configurations/{config_id}
+	raw, err := client.Get(client.ServiceURL("configurations", id), nil, openstack.StdRequestOpts())
+	if err != nil {
+		return nil, err
+	}
+
+	var res Configuration
+	err = extract.Into(raw.Body, &res)
+	return &res, err
 }
