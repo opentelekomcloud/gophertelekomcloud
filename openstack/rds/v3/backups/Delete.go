7818cb1a68116548e373b428e8f9252fd59ff1e4
Aloento <11802769+Aloento@users.noreply.github.com>
2022-12-19 11:55:09 +0100

    Delete

--- openstack/rds/v3/backups/Delete.go
+++ openstack/rds/v3/backups/Delete.go
@@ -2,8 +2,9 @@ package backups
 
 import "github.com/opentelekomcloud/gophertelekomcloud"
 
-func Delete(c *golangsdk.ServiceClient, id string) (r DeleteResult) {
-	raw, err := c.Delete(c.ServiceURL("backups", id), &golangsdk.RequestOpts{
+func Delete(c *golangsdk.ServiceClient, backupId string) (err error) {
+	// DELETE https://{Endpoint}/v3/{project_id}/backups/{backup_id}
+	_, err = c.Delete(c.ServiceURL("backups", backupId), &golangsdk.RequestOpts{
 		OkCodes: []int{200, 201, 204},
 	})
 	return
