d8c04226d8b3e2fb93f72cbc69742224eb69aa21
Aloento <11802769+Aloento@users.noreply.github.com>
2022-12-20 12:26:47 +0100

    DeleteCluster

--- acceptance/openstack/dws/v1/dws_test.go
+++ acceptance/openstack/dws/v1/dws_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/openstack"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/dws/v1/cluster"
 	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
 )
@@ -12,9 +14,36 @@ func TestDWS(t *testing.T) {
 	client, err := clients.NewDWSV1Client()
 	th.AssertNoErr(t, err)
 
+	vpcID := clients.EnvOS.GetEnv("VPC_ID")
+	if vpcID == "" {
+		t.Skip("OS_VPC_ID is missing but DWS test requires using existing network")
+	}
+
+	subnetID := clients.EnvOS.GetEnv("SUBNET_ID")
+	if subnetID == "" {
+		t.Skip("OS_SUBNET_ID env var is missing but DWS test requires using existing network")
+	}
+
+	name := tools.RandomString("dws-test-", 3)
 	newCluster, err := cluster.CreateCluster(client, cluster.CreateClusterOpts{
-		NodeType:     "dws.m3.xlarge",
-		NumberOfNode: 3,
+		NodeType:        "dws.m3.xlarge",
+		NumberOfNode:    3,
+		SubnetId:        subnetID,
+		SecurityGroupId: openstack.DefaultSecurityGroup(t),
+		VpcId:           vpcID,
+		Name:            name,
+		UserName:        "test",
+		UserPwd:         "test",
 	})
+	th.AssertNoErr(t, err)
+	t.Cleanup(func() {
+		err := cluster.DeleteCluster(client, cluster.DeleteClusterOpts{
+			ClusterId:              newCluster,
+			KeepLastManualSnapshot: 0,
+		})
+		th.AssertNoErr(t, err)
+	})
+
+	err = cluster.WaitForCluster(client, newCluster, 600)
 	th.AssertNoErr(t, err)
 }
--- openstack/dws/v1/cluster/DeleteCluster.go
+++ openstack/dws/v1/cluster/DeleteCluster.go
@@ -3,17 +3,18 @@ package cluster
 import golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 
 type DeleteClusterOpts struct {
+	ClusterId string
 	// The number of the latest manual snapshots that need to be retained for a cluster.
-	KeepLastManualSnapshot int `json:"keep_last_manual_snapshot"`
+	KeepLastManualSnapshot int `json:"keep_last_manual_snapshot" required:"true"`
 }
 
-func DeleteCluster(client *golangsdk.ServiceClient, clusterId string, opts DeleteClusterOpts) error {
+func DeleteCluster(client *golangsdk.ServiceClient, opts DeleteClusterOpts) error {
 	q, err := golangsdk.BuildQueryString(opts)
 	if err != nil {
 		return err
 	}
 
 	// DELETE /v1.0/{project_id}/clusters/{cluster_id}
-	_, err = client.Delete(client.ServiceURL("clusters", clusterId)+q.String(), nil)
+	_, err = client.Delete(client.ServiceURL("clusters", opts.ClusterId)+q.String(), nil)
 	return err
 }
