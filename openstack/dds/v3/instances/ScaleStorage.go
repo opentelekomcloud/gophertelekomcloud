b4a40f7259c148394dbff14ab8e8e8c681600a79
Aloento <11802769+Aloento@users.noreply.github.com>
2022-12-30 14:21:10 +0100

    Tags

--- openstack/dds/v3/instances/ScaleStorage.go
+++ openstack/dds/v3/instances/ScaleStorage.go
@@ -6,8 +6,18 @@ import (
 )
 
 type ScaleStorageOpt struct {
-	GroupId    string `json:"group_id,omitempty"`
-	Size       string `json:"size" required:"true"`
+	// Specifies the role ID.
+	//
+	// For a cluster instance, this parameter is set to the ID of the shard group.
+	// This parameter is not transferred for replica set and single node instances.
+	GroupId string `json:"group_id,omitempty"`
+	// Specifies the requested disk capacity. The value must be an integer multiple of 10 and greater than the current storage space.
+	//
+	// In a cluster instance, this parameter indicates the storage space of shard nodes. The value range is from 10 GB to 2000 GB.
+	// In a replica set instance, this parameter indicates the disk capacity of the DB instance to be expanded. The value range is from 10 GB to 2000 GB.
+	// In a single node instance, this parameter indicates the disk capacity of the DB instance to be expanded. The value range is from 10 GB to 1000 GB.
+	Size string `json:"size" required:"true"`
+	// Specifies the instance
 	InstanceId string `json:"-"`
 }
 
@@ -17,6 +27,7 @@ func ScaleStorage(client *golangsdk.ServiceClient, opts ScaleStorageOpt) (*strin
 		return nil, err
 	}
 
+	// POST https://{Endpoint}/v3/{project_id}/instances/{instance_id}/enlarge-volume
 	raw, err := client.Post(client.ServiceURL("instances", opts.InstanceId, "enlarge-volume"), b, nil, &golangsdk.RequestOpts{
 		OkCodes: []int{200, 202},
 	})
