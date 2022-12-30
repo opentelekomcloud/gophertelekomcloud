929a562387afe9c893a904bf14ed1fdd0073269a
Aloento <11802769+Aloento@users.noreply.github.com>
2022-12-30 12:07:06 +0100

    KillSessions

--- openstack/dds/v3/connection/KillSessions.go
+++ openstack/dds/v3/connection/KillSessions.go
@@ -6,19 +6,25 @@ import (
 )
 
 type KillSessionsOpts struct {
+	// Specifies the node ID. The following nodes can be queried: mongos nodes in the cluster, and all nodes in the replica set and single node instances.
+	NodeId string `json:"-"`
+	// Specifies the IDs of sessions to be terminated.
 	Sessions []string `json:"sessions" required:"true"`
 }
 
-func KillSessions(client *golangsdk.ServiceClient, nodeId string, opts KillSessionsOpts) error {
+func KillSessions(client *golangsdk.ServiceClient, opts KillSessionsOpts) error {
 	b, err := build.RequestBody(opts, "")
 	if err != nil {
 		return err
 	}
-	_, err = client.Post(client.ServiceURL("nodes", nodeId, "session"), b, nil, &golangsdk.RequestOpts{
+
+	// POST https://{Endpoint}/v3/{project_id}/nodes/{node_id}/session
+	_, err = client.Post(client.ServiceURL("nodes", opts.NodeId, "session"), b, nil, &golangsdk.RequestOpts{
 		OkCodes: []int{200, 202},
 	})
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
