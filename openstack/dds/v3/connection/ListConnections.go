5be82a83516616fb9868457bb409fd23760e2999
Aloento <11802769+Aloento@users.noreply.github.com>
2022-12-30 12:13:39 +0100

    ListConnections

--- openstack/dds/v3/connection/ListConnections.go
+++ openstack/dds/v3/connection/ListConnections.go
@@ -6,16 +6,21 @@ import (
 )
 
 type ListConnectionsOpts struct {
+	// Specifies the DB instance ID.
+	InstanceId string `json:"-"`
+	// Specifies the node ID.
+	// If this parameter is left blank, the number of connections of all nodes that can be connected in the instance is queried.
 	NodeId string `q:"node_id"`
 }
 
-func ListConnections(client *golangsdk.ServiceClient, instanceId string, opts ListConnectionsOpts) (*ListConnectionsResponse, error) {
+func ListConnections(client *golangsdk.ServiceClient, opts ListConnectionsOpts) (*ListConnectionsResponse, error) {
 	q, err := golangsdk.BuildQueryString(opts)
 	if err != nil {
 		return nil, err
 	}
 
-	raw, err := client.Get(client.ServiceURL("instances", instanceId, "conn-statistics")+q.String(), nil, nil)
+	// GET https://{Endpoint}/v3/{project_id}/instances/{instance_id}/conn-statistics
+	raw, err := client.Get(client.ServiceURL("instances", opts.InstanceId, "conn-statistics")+q.String(), nil, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -26,14 +31,21 @@ func ListConnections(client *golangsdk.ServiceClient, instanceId string, opts Li
 }
 
 type ListConnectionsResponse struct {
-	TotalConnections      int           `json:"total_connections"`
-	TotalInnerConnections int           `json:"total_inner_connections"`
-	TotalOuterConnections int           `json:"total_outer_connections"`
-	InnerConnections      []Connections `json:"inner_connections"`
-	OuterConnections      []Connections `json:"outer_connections"`
+	// Indicates the total number of connections, including internal and external connections.
+	TotalConnections int `json:"total_connections"`
+	// Indicates the total number of internal connections.
+	TotalInnerConnections int `json:"total_inner_connections"`
+	// Indicates the total number of external connections.
+	TotalOuterConnections int `json:"total_outer_connections"`
+	// Indicates the internal connection statistics array. Up to 200 records are supported.
+	InnerConnections []Connections `json:"inner_connections"`
+	// Indicates the external connection statistics array. Up to 200 records are supported.
+	OuterConnections []Connections `json:"outer_connections"`
 }
 
 type Connections struct {
+	// Indicates the IP address of the client connected to the instance or node.
 	ClientIp string `json:"client_ip"`
-	Count    int    `json:"count"`
+	// Indicates the number of connections corresponding to the IP address.
+	Count int `json:"count"`
 }
