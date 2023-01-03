8eb4993d46d89c989173fe546ae6edddd5f7f296
Aloento <11802769+Aloento@users.noreply.github.com>
2023-01-03 15:00:37 +0100

    List

--- openstack/swr/v2/domains/List.go
+++ openstack/swr/v2/domains/List.go
@@ -1,14 +1,26 @@
 package domains
 
 import (
-	"fmt"
-
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
-	"github.com/opentelekomcloud/gophertelekomcloud/pagination"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
 )
 
-func List(client *golangsdk.ServiceClient, org, repo string) (p pagination.Pager) {
-	// GET /v2/manage/namespaces/{namespace}/repositories/{repository}/access-domains/{access_domain}
-	url := fmt.Sprintf("%s/%s", client.ServiceURL("manage", "namespaces", opts.Namespace, "repos", opts.Repository, "access-domains"), opts.AccessDomain)
+type ListOpts struct {
+	// Organization name
+	Namespace string `json:"-" required:"true"`
+	// Image repository name
+	Repository string `json:"-" required:"true"`
+}
+
+func List(client *golangsdk.ServiceClient, opts ListOpts) ([]AccessDomain, error) {
+	// GET /v2/manage/namespaces/{namespace}/repositories/{repository}/access-domains
+	url := client.ServiceURL("manage", "namespaces", opts.Namespace, "repos", opts.Repository, "access-domains")
 	raw, err := client.Get(url, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res []AccessDomain
+	err = extract.IntoSlicePtr(raw.Body, &res, "")
+	return res, err
 }
