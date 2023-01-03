0a422a681af9f8069b347a192a322cb099b7fbf3
Aloento <11802769+Aloento@users.noreply.github.com>
2023-01-03 12:33:21 +0100

    fin

--- acceptance/openstack/dws/v1/dws_test.go
+++ acceptance/openstack/dws/v1/dws_test.go
@@ -1,10 +1,12 @@
 package v1
 
 import (
+	"os"
 	"testing"
 
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/openstack"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/pointerto"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/dws/v1/cluster"
@@ -13,9 +15,9 @@ import (
 )
 
 func TestDWS(t *testing.T) {
-	// if os.Getenv("RUN_DWS_LIFECYCLE") == "" {
-	// 	t.Skip("too slow to run in zuul")
-	// }
+	if os.Getenv("RUN_DWS_LIFECYCLE") == "" {
+		t.Skip("too slow to run in zuul")
+	}
 
 	client, err := clients.NewDWSV1Client()
 	th.AssertNoErr(t, err)
@@ -30,36 +32,36 @@ func TestDWS(t *testing.T) {
 		t.Skip("OS_SUBNET_ID env var is missing but DWS test requires using existing network")
 	}
 
-	clusterId := "e3ececc2-e2ae-4f5c-8ab5-fc96f4f20f4f"
+	// clusterId := "e3ececc2-e2ae-4f5c-8ab5-fc96f4f20f4f"
 
 	t.Log("Creating cluster")
 
 	name := tools.RandomString("dws-test-", 3)
-	// clusterId, err := cluster.CreateCluster(client, cluster.CreateClusterOpts{
-	// 	NodeType:        "dws.m3.xlarge",
-	// 	NumberOfNode:    3,
-	// 	SubnetId:        subnetID,
-	// 	SecurityGroupId: openstack.DefaultSecurityGroup(t),
-	// 	VpcId:           vpcID,
-	// 	Name:            name,
-	// 	UserName:        "dbadmin",
-	// 	UserPwd:         "#dbadmin123",
-	// })
-	// th.AssertNoErr(t, err)
-	// t.Cleanup(func() {
-	// 	err = golangsdk.WaitFor(1000, func() (bool, error) {
-	// 		err = cluster.DeleteCluster(client, cluster.DeleteClusterOpts{
-	// 			ClusterId:              clusterId,
-	// 			KeepLastManualSnapshot: pointerto.Int(0),
-	// 		})
-	// 		if err != nil {
-	// 			t.Error(err)
-	// 			return false, nil
-	// 		}
-	// 		return true, nil
-	// 	})
-	// 	th.AssertNoErr(t, err)
-	// })
+	clusterId, err := cluster.CreateCluster(client, cluster.CreateClusterOpts{
+		NodeType:        "dws.m3.xlarge",
+		NumberOfNode:    3,
+		SubnetId:        subnetID,
+		SecurityGroupId: openstack.DefaultSecurityGroup(t),
+		VpcId:           vpcID,
+		Name:            name,
+		UserName:        "dbadmin",
+		UserPwd:         "#dbadmin123",
+	})
+	th.AssertNoErr(t, err)
+	t.Cleanup(func() {
+		err = golangsdk.WaitFor(1000, func() (bool, error) {
+			err = cluster.DeleteCluster(client, cluster.DeleteClusterOpts{
+				ClusterId:              clusterId,
+				KeepLastManualSnapshot: pointerto.Int(0),
+			})
+			if err != nil {
+				t.Error(err)
+				return false, nil
+			}
+			return true, nil
+		})
+		th.AssertNoErr(t, err)
+	})
 
 	err = cluster.WaitForCluster(client, clusterId, 1000)
 	th.AssertNoErr(t, err)
