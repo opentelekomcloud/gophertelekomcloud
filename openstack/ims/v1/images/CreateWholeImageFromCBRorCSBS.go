a781d58e91b9c98da4a75745b77dd87e9729d8b7
Aloento <11802769+Aloento@users.noreply.github.com>
2023-01-31 13:12:31 +0100

    CreateWholeImage

---
+++ openstack/ims/v1/images/CreateWholeImageFromCBRorCSBS.go
@@ -0,0 +1,52 @@
+package images
+
+import "github.com/opentelekomcloud/gophertelekomcloud/openstack/common/tags"
+
+type CreateWholeImageFromCBRorCSBSOpts struct {
+	// Specifies the image name. For detailed description, see Image Attributes.
+	Name string `json:"name" required:"true"`
+	// Provides supplementary information about the image. For detailed description, see Image Attributes.
+	Description string `json:"description,omitempty"`
+	// Lists the image tags. The value is left blank by default.
+	//
+	// Use either tags or image_tags.
+	Tags []string `json:"tags,omitempty"`
+	// Lists the image tags. The value is left blank by default.
+	//
+	// Use either tags or image_tags.
+	ImageTags []tags.ResourceTag `json:"image_tags,omitempty"`
+	// Specifies the CSBS backup ID or CBR backup ID.
+	//
+	// To obtain the CSBS backup ID, perform the following operations:
+	//
+	// Log in to the management console.
+	//
+	// Under Storage, click Cloud Server Backup Service.
+	//
+	// In the backup list, expand details of the backup to obtain its ID.
+	//
+	// To obtain the CBR backup ID, perform the following operations:
+	//
+	// Log in to the management console.
+	//
+	// Under Storage, click Cloud Backup and Recovery.
+	//
+	// On the displayed Cloud Server Backup page, click the Backups tab and obtain the backup ID from the backup list.
+	BackupId string `json:"backup_id" required:"true"`
+	// Specifies the maximum memory of the image in the unit of MB. This parameter is not configured by default.
+	MaxRam int `json:"max_ram,omitempty"`
+	// Specifies the minimum memory of the image in the unit of MB. The default value is 0, indicating that the memory is not restricted.
+	MinRam int `json:"min_ram,omitempty"`
+	// Specifies the method of creating a full-ECS image.
+	//
+	// If the value is CBR, a CBR backup is used to create a full-ECS image. In this case, backup_id is the CBR backup ID.
+	//
+	// If the value is CSBS, a CSBS backup is used to create a full-ECS image. In this case, backup_id is the CSBS backup ID.
+	//
+	// If you do not specify this parameter, value CSBS is used by default.
+	WholeImageType string `json:"whole_image_type,omitempty"`
+}
+
+// POST /v1/cloudimages/wholeimages/action
+
+// 200 JobResponse
--- openstack/ims/v1/images/CreateWholeImageFromECS.go
+++ openstack/ims/v1/images/CreateWholeImageFromECS.go
@@ -1,3 +1,41 @@
 package images
 
+import "github.com/opentelekomcloud/gophertelekomcloud/openstack/common/tags"
+
+// CreateWholeImageFromECSOpts Parameters for creating a full-ECS image using an ECS
+type CreateWholeImageFromECSOpts struct {
+	// Specifies the image name. For detailed description, see Image Attributes.
+	Name string `json:"name" required:"true"`
+	// Provides supplementary information about the image. For detailed description, see Image Attributes.
+	Description string `json:"description,omitempty"`
+	// Lists the image tags. The value is left blank by default.
+	// Use either tags or image_tags.
+	Tags []string `json:"tags,omitempty"`
+	// Lists the image tags. The value is left blank by default.
+	// Use either tags or image_tags.
+	ImageTags []tags.ResourceTag `json:"image_tags,omitempty"`
+	// Specifies the ECS ID. This parameter is required when an ECS is used to create a full-ECS image.
+	//
+	// To obtain the ECS ID, perform the following operations:
+	//
+	// Log in to management console.
+	//
+	// Under Computing, click Elastic Cloud Server.
+	//
+	// In the ECS list, click the name of the ECS and view its ID.
+	InstanceId string `json:"instance_id" required:"true"`
+	// Specifies the maximum memory of the image in the unit of MB. This parameter is not configured by default.
+	MaxRam int `json:"max_ram,omitempty"`
+	// Specifies the minimum memory of the image in the unit of MB. The default value is 0.
+	MinRam int `json:"min_ram,omitempty"`
+	// Specifies the ID of the vault to which an ECS is to be added or has been added.
+	//
+	// To create a full-ECS image from an ECS, create a backup from the ECS and then use the backup to create a full-ECS image. If a CBR backup is created, vault_id is mandatory. If a CSBS backup is created, vault_id is optional.
+	//
+	// You can obtain the vault ID from the CBR console or section "Querying the Vault List" in Cloud Backup and Recovery API Reference.
+	VaultId string `json:"vault_id,omitempty"`
+}
+
 // POST /v1/cloudimages/wholeimages/action
+
+// 200 JobResponse
