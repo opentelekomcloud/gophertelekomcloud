4cce40d212ff6004a1475bf2b2f7e91b8ada9b8e
Aloento <11802769+Aloento@users.noreply.github.com>
2023-01-31 15:30:55 +0100

    ShowImageQuota

---
+++ openstack/ims/v1/images/ShowImageQuota.go
@@ -0,0 +1,24 @@
+package images
+
+// GET /v1/cloudimages/quota
+
+type ShowImageQuotaResponse struct {
+	Quotas Quota `json:"quotas,omitempty"`
+}
+
+type Quota struct {
+	Resources []QuotaInfo `json:"resources"`
+}
+
+type QuotaInfo struct {
+	// Specifies the type of the resource to be queried.
+	Type string `json:"type"`
+	// Specifies the used quota.
+	Used int32 `json:"used"`
+	// Specifies the total quota.
+	Quota int32 `json:"quota"`
+	// Specifies the minimum quota.
+	Min int32 `json:"min"`
+	// Specifies the maximum quota.
+	Max int32 `json:"max"`
+}
