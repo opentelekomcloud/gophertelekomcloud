016826647bbd2403ad82fa7db918b7430946dd24
Aloento <11802769+Aloento@users.noreply.github.com>
2023-02-01 15:21:52 +0100

    DeleteImageTag

--- openstack/ims/v2/tags/DeleteImageTag.go
+++ openstack/ims/v2/tags/DeleteImageTag.go
@@ -1,12 +1,20 @@
 package tags
 
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+)
+
 type DeleteImageTagOpts struct {
 	// Specifies the image ID.
-	ImageId string `json:"-"`
+	ImageId string `json:"-" required:"true"`
 	// Specifies the key of the tag to be deleted.
-	Key string `json:"-"`
+	Key string `json:"-" required:"true"`
 }
 
-// DELETE /v2/{project_id}/images/{image_id}/tags/{key}
-
-// 204
+func DeleteImageTag(client *golangsdk.ServiceClient, opts DeleteImageTagOpts) (err error) {
+	// DELETE /v2/{project_id}/images/{image_id}/tags/{key}
+	_, err = client.Delete(client.ServiceURL("images", opts.ImageId, "tags", opts.Key), &golangsdk.RequestOpts{
+		OkCodes: []int{204},
+	})
+	return
+}
