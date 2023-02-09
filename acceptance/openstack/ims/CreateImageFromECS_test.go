1f610d434cb238447b5caa62d23e0c551aa04051
Aloento <11802769+Aloento@users.noreply.github.com>
2023-02-09 17:19:11 +0100

    CreateImageFromDisk

--- acceptance/openstack/ims/CreateImageFromECS_test.go
+++ acceptance/openstack/ims/CreateImageFromECS_test.go
@@ -21,6 +21,14 @@ func TestCreateImageFromECS(t *testing.T) {
 	ecs := openstack.CreateCloudServer(t, computeClient, openstack.GetCloudServerCreateOpts(t))
 	t.Cleanup(func() { openstack.DeleteCloudServer(t, computeClient, ecs.ID) })
 
+	fromDisk, err := images.CreateImageFromDisk(client2, images.CreateImageFromDiskOpts{
+		Name:      tools.RandomString("ims-test-", 3),
+		VolumeId:  ecs.VolumeAttached[0].ID,
+		OsVersion: "Debian GNU/Linux 10.0.0 64bit",
+	})
+	th.AssertNoErr(t, err)
+	jobEntities(t, client1, client2, fromDisk)
+
 	fromECS, err := images.CreateImageFromECS(client2, images.CreateImageFromECSOpts{
 		Name:       tools.RandomString("ims-test-", 3),
 		InstanceId: ecs.ID,
