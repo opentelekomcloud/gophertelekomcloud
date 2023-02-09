bc40f7a150ada0f021378c84b52e7f04ced95254
Aloento <11802769+Aloento@users.noreply.github.com>
2023-02-09 13:00:48 +0100

    fix

--- openstack/ims/v1/images/CreateWholeImageFromECS.go
+++ openstack/ims/v1/images/CreateWholeImageFromECS.go
@@ -47,10 +47,10 @@ func CreateWholeImageFromECS(client *golangsdk.ServiceClient, opts CreateWholeIm
 		return nil, err
 	}
 
-	return wholeImages(client, err, b)
+	return wholeImages(client, b)
 }
 
-func wholeImages(client *golangsdk.ServiceClient, err error, b *build.Body) (*string, error) {
+func wholeImages(client *golangsdk.ServiceClient, b *build.Body) (*string, error) {
 	// POST /v1/cloudimages/wholeimages/action
 	raw, err := client.Post(client.ServiceURL("cloudimages", "wholeimages", "action"), b, nil, &golangsdk.RequestOpts{
 		OkCodes: []int{200},
