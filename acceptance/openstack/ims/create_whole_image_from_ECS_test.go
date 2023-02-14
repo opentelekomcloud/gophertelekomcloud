61181db29f24865102b375f32ddc6475e51ad1e2
Aloento <11802769+Aloento@users.noreply.github.com>
2023-02-14 16:06:48 +0100

    CreateWholeImageFromCBRorCSBS

--- acceptance/openstack/ims/create_whole_image_from_ECS_test.go
+++ acceptance/openstack/ims/create_whole_image_from_ECS_test.go
@@ -7,12 +7,16 @@ import (
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/openstack"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
 	tag "github.com/opentelekomcloud/gophertelekomcloud/openstack/common/tags"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/csbs/v1/backup"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/ims/v1/images"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/ims/v2/tags"
 	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
 )
 
 func TestCreateWholeImageFromECS(t *testing.T) {
+	clientCSBS, err := clients.NewCsbsV1Client()
+	th.AssertNoErr(t, err)
+
 	client1, client2 := getClient(t)
 
 	computeClient, err := clients.NewComputeV1Client()
@@ -21,6 +25,17 @@ func TestCreateWholeImageFromECS(t *testing.T) {
 	ecs := openstack.CreateCloudServer(t, computeClient, openstack.GetCloudServerCreateOpts(t))
 	t.Cleanup(func() { openstack.DeleteCloudServer(t, computeClient, ecs.ID) })
 
+	checkpoint, err := backup.Create(clientCSBS, ecs.ID, backup.CreateOpts{
+		BackupName:   tools.RandomString("backup-", 3),
+		Description:  "bla-bla",
+		ResourceType: "OS::Nova::Server",
+	})
+	t.Cleanup(func() {
+		err = backup.Delete(clientCSBS, checkpoint.Id)
+		th.AssertNoErr(t, err)
+	})
+	th.AssertNoErr(t, err)
+
 	fromECS, err := images.CreateWholeImageFromECS(client1, images.CreateWholeImageFromECSOpts{
 		Name:       tools.RandomString("ims-test-", 3),
 		InstanceId: ecs.ID,
@@ -28,6 +43,13 @@ func TestCreateWholeImageFromECS(t *testing.T) {
 	th.AssertNoErr(t, err)
 
 	image := jobEntities(t, client1, client2, fromECS)
+
+	fromCSBS, err := images.CreateWholeImageFromCBRorCSBS(client1, images.CreateWholeImageFromCBRorCSBSOpts{
+		Name:     tools.RandomString("ims-test-", 3),
+		BackupId: checkpoint.Id,
+	})
+
+	jobEntities(t, client1, client2, fromCSBS)
 
 	err = tags.AddImageTag(client2, tags.AddImageTagOpts{
 		ImageId: image.ImageId,
