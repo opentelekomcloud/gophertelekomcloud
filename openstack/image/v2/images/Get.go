f79320ab310ce07c59001ff6cc2fbf455fc83d99
Aloento <11802769+Aloento@users.noreply.github.com>
2023-02-27 09:10:30 +0100

    Get

--- openstack/image/v2/images/Get.go
+++ openstack/image/v2/images/Get.go
@@ -1,9 +1,20 @@
 package images
 
-import "github.com/opentelekomcloud/gophertelekomcloud"
+import (
+	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/ims/v2/images"
+)
 
 // Get implements image get request.
-func Get(client *golangsdk.ServiceClient, id string) (r GetResult) {
-	_, r.Err = client.Get(client.ServiceURL("images", id), &r.Body, nil)
-	return
+func Get(client *golangsdk.ServiceClient, id string) (*images.ImageInfo, error) {
+	// GET /v2/images/{image_id}
+	raw, err := client.Get(client.ServiceURL("images", id), nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res images.ImageInfo
+	err = extract.Into(raw.Body, &res)
+	return &res, err
 }
