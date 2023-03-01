17454936e91ce8a2d7866f22908fc3d25f29dc89
Aloento <11802769+Aloento@users.noreply.github.com>
2023-03-01 13:10:04 +0100

    Delete

--- openstack/image/v2/members/Delete.go
+++ openstack/image/v2/members/Delete.go
@@ -2,9 +2,14 @@ package members
 
 import "github.com/opentelekomcloud/gophertelekomcloud"
 
+type DeleteOpts struct {
+	ImageId  string `json:"-" required:"true"`
+	MemberId string `json:"-" required:"true"`
+}
+
 // Delete membership for given image. Callee should be image owner.
-func Delete(client *golangsdk.ServiceClient, imageID string, memberID string) (r DeleteResult) {
-	_, r.Err = client.Delete(client.ServiceURL("images", imageID, "members", memberID), &golangsdk.RequestOpts{
+func Delete(client *golangsdk.ServiceClient, opts DeleteOpts) (err error) {
+	_, err = client.Delete(client.ServiceURL("images", opts.ImageId, "members", opts.MemberId), &golangsdk.RequestOpts{
 		OkCodes: []int{204},
 	})
 	return
