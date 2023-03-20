075bc23947fc59f49ba9efcd1dcc3b39f5468c1a
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2023-03-20 17:49:33 +0100

    [ELB] ip address group (#508)
    [ELB] ip address group
    
    What this PR does / why we need it
    Need to implement new feature for elbv3
    Closes #506
    === RUN   TestIpGroupsLifecycle
    helpers.go:19: Attempting to create ELBv3 LoadBalancer
    helpers.go:57: Created ELBv3 LoadBalancer: 9d5cd98e-8ac8-4bb2-96ce-0d33a1d42811
    ipgroups_test.go:44: Attempting to create ELBv3 IpGroup
    ipgroups_test.go:54: Attempting to update ELBv3 IpGroup: f724d111-3cec-42a9-9943-b2d6642e7584
    ipgroups_test.go:71: Updated ELBv3 ipGroup: f724d111-3cec-42a9-9943-b2d6642e7584
    ipgroups_test.go:83: Attempting to create ELBv3 Listener with ipGroup association
    helpers.go:63: Attempting to delete ELBv3 LoadBalancer: 9d5cd98e-8ac8-4bb2-96ce-0d33a1d42811
    helpers.go:66: Deleted ELBv3 LoadBalancer: 9d5cd98e-8ac8-4bb2-96ce-0d33a1d42811
    ipgroups_test.go:49: Attempting to delete ELBv3 IpGroup: f724d111-3cec-42a9-9943-b2d6642e7584
    ipgroups_test.go:51: Deleted ELBv3 IpGroup: f724d111-3cec-42a9-9943-b2d6642e7584
    --- PASS: TestIpGroupsLifecycle (7.35s)
    PASS
    Process finished with the exit code 0
    
    Reviewed-by: Aloento
    Reviewed-by: Artem Lifshits

---
+++ openstack/elb/v3/ipgroups/List.go
@@ -0,0 +1,66 @@
+package ipgroups
+
+import (
+	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/structs"
+)
+
+type ListOpts struct {
+	Limit       int    `q:"limit"`
+	Marker      string `q:"marker"`
+	PageReverse bool   `q:"page_reverse"`
+
+	ID          []string `q:"id"`
+	Name        []string `q:"name"`
+	Description []string `q:"description"`
+	IpList      []string `q:"ip_list"`
+}
+
+// List is used to obtain the parameter ipGroup list
+func List(client *golangsdk.ServiceClient, opts ListOpts) ([]IpGroup, error) {
+	q, err := golangsdk.BuildQueryString(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	// GET https://{Endpoint}/v3/{project_id}/backups
+	raw, err := client.Get(client.ServiceURL("ipgroups")+q.String(), nil, openstack.StdRequestOpts())
+	if err != nil {
+		return nil, err
+	}
+
+	var res []IpGroup
+	err = extract.IntoSlicePtr(raw.Body, &res, "ipgroups")
+	return res, err
+}
+
+// IpGroup The IP address can contain IP addresses or CIDR blocks.
+// 0.0.0.0 will be considered the same as 0.0.0.0/32. If you enter both 0.0.0.0 and 0.0.0.0/32,
+// only one will be kept. 0:0:0:0:0:0:0:1 will be considered the same as ::1 and ::1/128.
+// If you enter 0:0:0:0:0:0:0:1, ::1 and ::1/128, only one will be kept.
+type IpGroup struct {
+	// The unique ID for the IpGroup.
+	ID string `json:"id"`
+	// Specifies the IP address group name.
+	Name string `json:"name"`
+	// Provides remarks about the IP address group.
+	Description string `json:"description"`
+	// Specifies the project ID of the IP address group.
+	ProjectId string `json:"project_id"`
+	// Specifies the IP addresses or CIDR blocks in the IP address group. [] indicates any IP address.
+	IpList []IpInfo `json:"ip_list"`
+	// Lists the IDs of listeners with which the IP address group is associated.
+	Listeners []structs.ResourceRef `json:"listeners"`
+	// Specifies the time when the IP address group was created.
+	CreatedAt string `json:"created_at"`
+	// Specifies the time when the IP address group was updated.
+	UpdatedAt string `json:"updated_at"`
+}
+type IpInfo struct {
+	// Specifies the IP addresses in the IP address group.
+	Ip string `json:"ip"`
+	// Provides remarks about the IP address group.
+	Description string `json:"description"`
+}
