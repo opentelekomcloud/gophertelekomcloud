a321d88084b617b620550fa4318b44a0a37f7ffc
Aloento <11802769+Aloento@users.noreply.github.com>
2023-04-14 14:51:54 +0200

    Update

--- openstack/elb/v3/certificates/Update.go
+++ openstack/elb/v3/certificates/Update.go
@@ -1,39 +1,62 @@
 package certificates
 
-import "github.com/opentelekomcloud/gophertelekomcloud"
-
-// UpdateOptsBuilder is the interface options structs have to satisfy in order
-// to be used in the main Update operation in this package. Since many
-// extensions decorate or modify the common logic, it is useful for them to
-// satisfy a basic interface in order for them to be used.
-type UpdateOptsBuilder interface {
-	ToCertificateUpdateMap() (map[string]interface{}, error)
-}
+import (
+	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
+)
 
 // UpdateOpts is the common options' struct used in this package's Update
 // operation.
 type UpdateOpts struct {
-	Name        string  `json:"name,omitempty"`
-	Description *string `json:"description,omitempty"`
-	Domain      string  `json:"domain,omitempty"`
-	PrivateKey  string  `json:"private_key,omitempty"`
-	Certificate string  `json:"certificate,omitempty"`
-}
-
-// ToCertificateUpdateMap casts a UpdateOpts struct to a map.
-func (opts UpdateOpts) ToCertificateUpdateMap() (map[string]interface{}, error) {
-	return golangsdk.BuildRequestBody(opts, "certificate")
+	// Specifies the certificate name.
+	//
+	// Minimum: 0
+	//
+	// Maximum: 255
+	Name string `json:"name,omitempty"`
+	// Provides supplementary information about the certificate.
+	//
+	// Minimum: 0
+	//
+	// Maximum: 255
+	Description string `json:"description,omitempty"`
+	// Specifies the domain names used by the server certificate.
+	//
+	// This parameter will take effect only when type is set to server, and its default value is "".
+	//
+	// This parameter will not take effect even if it is passed and type is set to client. However, domain names will still be verified.
+	//
+	// Note:
+	//
+	// The value can contain 0 to 1024 characters and consists of multiple common domain names or wildcard domain names separated by commas. A maximum of 30 domain names are allowed.
+	//
+	// A common domain name consists of several labels separated by periods (.). Each label can contain a maximum of 63 characters, including letters, digits, and hyphens (-), and must start and end with a letter or digit. Example: www.test.com
+	//
+	// A wildcard domain name is a domain name starts with an asterisk (*). Example: *.test.com
+	//
+	// Minimum: 0
+	//
+	// Maximum: 1024
+	Domain string `json:"domain,omitempty"`
+	// Specifies the private key of the server certificate. The value must be PEM encoded.
+	//
+	// This parameter will be ignored if type is set to client. A CA server can still be created and used normally. This parameter will be left blank even if you enter a private key that is not PEM encoded.
+	//
+	// This parameter is valid and mandatory only when type is set to server. If you enter an invalid private key, an error is returned.
+	PrivateKey string `json:"private_key,omitempty"`
+	// Specifies the private key of the certificate. The value must be PEM encoded.
+	Certificate string `json:"certificate,omitempty"`
 }
 
 // Update is an operation which modifies the attributes of the specified Certificate.
-func Update(client *golangsdk.ServiceClient, id string, opts UpdateOpts) (r UpdateResult) {
-	b, err := opts.ToCertificateUpdateMap()
+func Update(client *golangsdk.ServiceClient, id string, opts UpdateOpts) (*Certificate, error) {
+	b, err := build.RequestBody(opts, "certificate")
 	if err != nil {
-		r.Err = err
-		return
+		return nil, err
 	}
-	_, r.Err = client.Put(client.ServiceURL("certificates", id), b, &r.Body, &golangsdk.RequestOpts{
+
+	raw, err := client.Put(client.ServiceURL("certificates", id), b, nil, &golangsdk.RequestOpts{
 		OkCodes: []int{200},
 	})
-	return
+	return extra(err, raw)
 }
