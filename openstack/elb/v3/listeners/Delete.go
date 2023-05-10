45851b80c88ee08bb77a2a78555d72d4e6183f77
Aloento <11802769+Aloento@users.noreply.github.com>
2023-05-10 11:01:06 +0200

    Delete

--- openstack/elb/v3/listeners/Delete.go
+++ openstack/elb/v3/listeners/Delete.go
@@ -3,7 +3,7 @@ package listeners
 import "github.com/opentelekomcloud/gophertelekomcloud"
 
 // Delete will permanently delete a particular Listeners based on its unique ID.
-func Delete(client *golangsdk.ServiceClient, id string) (r DeleteResult) {
-	_, r.Err = client.Delete(client.ServiceURL("listeners", id), nil)
+func Delete(client *golangsdk.ServiceClient, id string) (err error) {
+	_, err = client.Delete(client.ServiceURL("listeners", id), nil)
 	return
 }
