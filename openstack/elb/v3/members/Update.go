496a804ccc49597ffac9e60fb2f5386418ed214a
Aloento <11802769+Aloento@users.noreply.github.com>
2023-05-16 14:52:48 +0200

    Update

--- openstack/elb/v3/members/Update.go
+++ openstack/elb/v3/members/Update.go
@@ -1,44 +1,46 @@
 package members
 
-import "github.com/opentelekomcloud/gophertelekomcloud"
+import (
+	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
+)
 
-// UpdateOptsBuilder allows extensions to add additional parameters to the
-// List request.
-type UpdateOptsBuilder interface {
-	ToMemberUpdateMap() (map[string]interface{}, error)
-}
-
-// UpdateOpts is the common options' struct used in this package's Update
-// operation.
+// UpdateOpts is the common options' struct used in this package's Update operation.
 type UpdateOpts struct {
-	// Name of the Member.
-	Name *string `json:"name,omitempty"`
-
-	// A positive integer value that indicates the relative portion of traffic
-	// that this member should receive from the pool. For example, a member with
-	// a weight of 10 receives five times as much traffic as a member with a
-	// weight of 2.
-	Weight *int `json:"weight,omitempty"`
-
-	// The administrative state of the Pool. A valid value is true (UP)
-	// or false (DOWN).
+	// Specifies the administrative status of the backend server.
+	//
+	// Although this parameter can be used in the APIs for creating and updating backend servers, its actual value depends on whether cloud servers exist. If cloud servers exist, the value is true. Otherwise, the value is false.
+	//
+	// This parameter is unsupported. Please do not use it.
 	AdminStateUp *bool `json:"admin_state_up,omitempty"`
-}
-
-// ToMemberUpdateMap builds a request body from UpdateOptsBuilder.
-func (opts UpdateOpts) ToMemberUpdateMap() (map[string]interface{}, error) {
-	return golangsdk.BuildRequestBody(opts, "member")
+	// Specifies the backend server name.
+	//
+	// Minimum: 0
+	//
+	// Maximum: 255
+	Name string `json:"name,omitempty"`
+	// Specifies the weight of the backend server. Requests are routed to backend servers in the same backend server group based on their weights.
+	//
+	// The value ranges from 0 to 100, and the default value is 1. The larger the weight is, the higher proportion of requests the backend server receives. If the weight is set to 0, the backend server will not accept new requests.
+	//
+	// If lb_algorithm is set to SOURCE_IP, this parameter will not take effect.
+	//
+	// Minimum: 0
+	//
+	// Maximum: 100
+	Weight *int `json:"weight,omitempty"`
 }
 
 // Update allows Member to be updated.
-func Update(client *golangsdk.ServiceClient, poolID string, memberID string, opts UpdateOptsBuilder) (r UpdateResult) {
-	b, err := opts.ToMemberUpdateMap()
+func Update(client *golangsdk.ServiceClient, poolID string, memberID string, opts UpdateOpts) (*Member, error) {
+	b, err := build.RequestBody(opts, "member")
 	if err != nil {
-		r.Err = err
-		return
+		return nil, err
 	}
-	_, r.Err = client.Put(client.ServiceURL("pools", poolID, "members", memberID), b, &r.Body, &golangsdk.RequestOpts{
-		OkCodes: []int{200, 201, 202},
+
+	// PUT /v3/{project_id}/elb/pools/{pool_id}/members/{member_id}
+	raw, err := client.Put(client.ServiceURL("pools", poolID, "members", memberID), b, nil, &golangsdk.RequestOpts{
+		OkCodes: []int{200},
 	})
-	return
+	return extra(err, raw)
 }
