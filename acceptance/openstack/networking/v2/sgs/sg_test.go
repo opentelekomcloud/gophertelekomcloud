17f5591575e6d3a069f1e5be1e3e436516c4e99b
anton-sidelnikov <a.sidelnikov@t-systems.com>
2023-06-21 15:01:18 +0200

    port range nil value in response

--- acceptance/openstack/networking/v2/sgs/sg_test.go
+++ acceptance/openstack/networking/v2/sgs/sg_test.go
@@ -6,10 +6,103 @@ import (
 
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/pointerto"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/compute/v2/extensions/secgroups"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/networking/v2/extensions/security/rules"
 	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
 )
+
+func TestICMPSecurityGroupRules(t *testing.T) {
+	clientNetworking, err := clients.NewNetworkV2Client()
+	if err != nil {
+		t.Fatalf("Unable to create a networking client: %v", err)
+	}
+	clientCompute, err := clients.NewComputeV2Client()
+	if err != nil {
+		t.Fatalf("Unable to create a networking client: %v", err)
+	}
+
+	createSGOpts := secgroups.CreateOpts{
+		Name:        "sg-test-01",
+		Description: "desc",
+	}
+	t.Logf("Attempting to create sg: %s", createSGOpts.Name)
+
+	sg, err := secgroups.Create(clientCompute, createSGOpts).Extract()
+	th.AssertNoErr(t, err)
+
+	optsEchoReply := rules.CreateOpts{
+		Description:  "ICMP echo reply",
+		SecGroupID:   sg.ID,
+		PortRangeMin: pointerto.Int(0),
+		PortRangeMax: pointerto.Int(0),
+		Direction:    "ingress",
+		EtherType:    "IPv4",
+		Protocol:     "ICMP",
+	}
+	log.Print("[DEBUG] Create OpenTelekomCloud Neutron ICMP echo reply Security Group Rule")
+	echoReply, err := rules.Create(clientNetworking, optsEchoReply).Extract()
+
+	getEchoReply, err := rules.Get(clientNetworking, echoReply.ID).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, *getEchoReply.PortRangeMin, 0)
+	th.AssertEquals(t, *getEchoReply.PortRangeMax, 0)
+
+	optsAll := rules.CreateOpts{
+		Description:  "ICMP all",
+		SecGroupID:   sg.ID,
+		PortRangeMin: nil,
+		PortRangeMax: nil,
+		Direction:    "ingress",
+		EtherType:    "IPv4",
+		Protocol:     "ICMP",
+	}
+	log.Print("[DEBUG] Create OpenTelekomCloud Neutron ICMP All Security Group Rule")
+	all, err := rules.Create(clientNetworking, optsAll).Extract()
+
+	getAll, err := rules.Get(clientNetworking, all.ID).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, getAll.PortRangeMin, (*int)(nil))
+	th.AssertEquals(t, getAll.PortRangeMax, (*int)(nil))
+
+	optsEcho := rules.CreateOpts{
+		Description:  "ICMP echo",
+		SecGroupID:   sg.ID,
+		PortRangeMin: pointerto.Int(8),
+		PortRangeMax: pointerto.Int(0),
+		Direction:    "ingress",
+		EtherType:    "IPv4",
+		Protocol:     "ICMP",
+	}
+	log.Print("[DEBUG] Create OpenTelekomCloud Neutron ICMP Echo Security Group Rule")
+	echo, err := rules.Create(clientNetworking, optsEcho).Extract()
+
+	getEcho, err := rules.Get(clientNetworking, echo.ID).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, *getEcho.PortRangeMin, 8)
+	th.AssertEquals(t, *getEcho.PortRangeMax, 0)
+
+	optsFragment := rules.CreateOpts{
+		Description:  "ICMP Fragment need DF set",
+		SecGroupID:   sg.ID,
+		PortRangeMin: pointerto.Int(3),
+		PortRangeMax: pointerto.Int(4),
+		Direction:    "ingress",
+		EtherType:    "IPv4",
+		Protocol:     "ICMP",
+	}
+	log.Print("[DEBUG] Create OpenTelekomCloud Neutron ICMP Fragment need DF set Security Group Rule")
+	fragment, err := rules.Create(clientNetworking, optsFragment).Extract()
+
+	getFragment, err := rules.Get(clientNetworking, fragment.ID).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, *getFragment.PortRangeMin, 3)
+	th.AssertEquals(t, *getFragment.PortRangeMax, 4)
+
+	t.Cleanup(func() {
+		secgroups.Delete(clientCompute, sg.ID)
+	})
+}
 
 func TestThrottlingSgs(t *testing.T) {
 	t.Skip("please run only manually, long test")
