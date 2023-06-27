82076c1d49f5f9637c4acdae3f04b57449f13991
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2023-06-27 18:28:45 +0200

    missed type fixes (#539)
    [DIS] missed type fixes
    
    It helps to avoid unnecessary type conversions on provider side
    
    Reviewed-by: Artem Lifshits
    Reviewed-by: Aloento

--- openstack/dis/v2/dump/GetTransferTask.go
+++ openstack/dis/v2/dump/GetTransferTask.go
@@ -62,9 +62,9 @@ type GetTransferTaskResponse struct {
 	//  DWS
 	DestinationType string `json:"destination_type,omitempty"`
 	// Time when the dump task is created.
-	CreateTime int64 `json:"create_time,omitempty"`
+	CreatedAt *int64 `json:"create_time,omitempty"`
 	// Latest dump time of the dump task.
-	LastTransferTimestamp int64 `json:"last_transfer_timestamp,omitempty"`
+	LastTransferTimestamp *int64 `json:"last_transfer_timestamp,omitempty"`
 	// List of partition dump details.
 	Partitions []PartitionResult `json:"partitions,omitempty"`
 	// Parameter list of OBS to which data in the DIS stream will be dumped.
