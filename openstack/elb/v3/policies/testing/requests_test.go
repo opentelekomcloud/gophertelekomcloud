9fd6f83064957b9b5695114dff24afe3e7508b50
Aloento <11802769+Aloento@users.noreply.github.com>
2023-06-29 22:48:58 +0200

    Create

--- openstack/elb/v3/policies/testing/requests_test.go
+++ openstack/elb/v3/policies/testing/requests_test.go
@@ -5,22 +5,22 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/structs"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/pointerto"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/elb/v3/policies"
 	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
 	"github.com/opentelekomcloud/gophertelekomcloud/testhelper/client"
 )
 
-func expectedResult() *policies.Policy {
-	return &policies.Policy{
-		ID:                 "cf4360fd-8631-41ff-a6f5-b72c35da74be",
+func expectedResult() *policies.L7Policy {
+	return &policies.L7Policy{
+		Id:                 "cf4360fd-8631-41ff-a6f5-b72c35da74be",
 		Action:             "REDIRECT_TO_LISTENER",
-		ListenerID:         "e2220d2a-3faf-44f3-8cd6-0c42952bd0ab",
-		Position:           100,
-		ProjectID:          "99a3fff0d03c428eac3678da6a7d0f24",
-		Status:             "ACTIVE",
-		RedirectListenerID: "48a97732-449e-4aab-b561-828d29e45050",
-		Rules:              []structs.ResourceRef{},
+		ListenerId:         "e2220d2a-3faf-44f3-8cd6-0c42952bd0ab",
+		Position:           pointerto.Int(100),
+		ProjectId:          "99a3fff0d03c428eac3678da6a7d0f24",
+		ProvisioningStatus: "ACTIVE",
+		RedirectListenerId: "48a97732-449e-4aab-b561-828d29e45050",
+		Rules:              []policies.RuleCondition{},
 	}
 }
 
@@ -40,10 +40,10 @@ func TestCreateRequest(t *testing.T) {
 
 	opts := policies.CreateOpts{
 		Action:             "REDIRECT_TO_LISTENER",
-		ListenerID:         "e2220d2a-3faf-44f3-8cd6-0c42952bd0ab",
-		RedirectListenerID: "48a97732-449e-4aab-b561-828d29e45050",
+		ListenerId:         "e2220d2a-3faf-44f3-8cd6-0c42952bd0ab",
+		RedirectListenerId: "48a97732-449e-4aab-b561-828d29e45050",
 	}
-	created, err := policies.Create(client.ServiceClient(), opts).Extract()
+	created, err := policies.Create(client.ServiceClient(), opts)
 	th.AssertNoErr(t, err)
 
 	th.AssertDeepEquals(t, expectedResult(), created)
@@ -54,7 +54,7 @@ func TestGetRequest(t *testing.T) {
 	defer th.TeardownHTTP()
 
 	expected := expectedResult()
-	th.Mux.HandleFunc(fmt.Sprintf("/l7policies/%s", expected.ID), func(w http.ResponseWriter, r *http.Request) {
+	th.Mux.HandleFunc(fmt.Sprintf("/l7policies/%s", expected.Id), func(w http.ResponseWriter, r *http.Request) {
 		th.TestMethod(t, r, "GET")
 		th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
 
@@ -63,7 +63,7 @@ func TestGetRequest(t *testing.T) {
 		_, _ = fmt.Fprint(w, createResponseBody)
 	})
 
-	policy, err := policies.Get(client.ServiceClient(), expected.ID).Extract()
+	policy, err := policies.Get(client.ServiceClient(), expected.Id)
 	th.AssertNoErr(t, err)
 
 	th.AssertDeepEquals(t, expected, policy)
@@ -82,7 +82,7 @@ func TestListRequest(t *testing.T) {
 		_, _ = fmt.Fprint(w, listResponseBody)
 	})
 
-	pages, err := policies.List(client.ServiceClient(), nil).AllPages()
+	pages, err := policies.List(client.ServiceClient(), policies.ListOpts{}).AllPages()
 	th.AssertNoErr(t, err)
 
 	policySlice, err := policies.ExtractPolicies(pages)
@@ -94,7 +94,7 @@ func TestDeleteRequest(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
 
-	id := expectedResult().ID
+	id := expectedResult().Id
 	th.Mux.HandleFunc(fmt.Sprintf("/l7policies/%s", id), func(w http.ResponseWriter, r *http.Request) {
 		th.TestMethod(t, r, "DELETE")
 		th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
@@ -103,5 +103,5 @@ func TestDeleteRequest(t *testing.T) {
 		w.WriteHeader(http.StatusNoContent)
 	})
 
-	th.AssertNoErr(t, policies.Delete(client.ServiceClient(), id).ExtractErr())
+	th.AssertNoErr(t, policies.Delete(client.ServiceClient(), id))
 }
--- openstack/elb/v3/pools/results.go
+++ openstack/elb/v3/pools/results.go
@@ -2,66 +2,8 @@ package pools
 
 import (
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
-	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/structs"
 	"github.com/opentelekomcloud/gophertelekomcloud/pagination"
 )
-
-// Pool represents a logical set of devices, such as web servers, that you
-// group together to receive and process traffic. The load balancing function
-// chooses a Member of the Pool according to the configured load balancing
-// method to handle the new requests or connections received on the VIP address.
-type Pool struct {
-	// The load-balancer algorithm, which is round-robin, least-connections, and
-	// so on. This value, which must be supported, is dependent on the provider.
-	// Round-robin must be supported.
-	LBMethod string `json:"lb_algorithm"`
-
-	// The protocol of the Pool, which is TCP, HTTP, or HTTPS.
-	Protocol string `json:"protocol"`
-
-	// Description for the Pool.
-	Description string `json:"description"`
-
-	// A list of listeners objects IDs.
-	Listeners []structs.ResourceRef `json:"listeners"`
-
-	// A list of member objects IDs.
-	Members []structs.ResourceRef `json:"members"`
-
-	// The ID of associated health monitor.
-	MonitorID string `json:"healthmonitor_id"`
-
-	// The administrative state of the Pool, which is up (true) or down (false).
-	AdminStateUp bool `json:"admin_state_up"`
-
-	// Pool name. Does not have to be unique.
-	Name string `json:"name"`
-
-	ProjectID string `json:"project_id"`
-
-	// The unique ID for the Pool.
-	ID string `json:"id"`
-
-	// A list of load balancer objects IDs.
-	Loadbalancers []structs.ResourceRef `json:"loadbalancers"`
-
-	// Indicates whether connections in the same session will be processed by the
-	// same Pool member or not.
-	Persistence *SessionPersistence `json:"session_persistence"`
-
-	IpVersion string `json:"ip_version"`
-
-	SlowStart *SlowStart `json:"slow_start"`
-
-	// Deletion protection for the pool.
-	DeletionProtectionEnable bool `json:"member_deletion_protection_enable"`
-
-	// ID of the VPC where the backend server group works.
-	VpcId string `json:"vpc_id"`
-
-	// Type of the backend server group.
-	Type string `json:"type"`
-}
 
 // PoolPage is the page returned by a pager when traversing over a
 // collection of pools.
