055f3dabd53e7351c0a36ba2f0bfafd184e4b100
Aloento <11802769+Aloento@users.noreply.github.com>
2023-06-29 22:54:34 +0200

    Update

--- openstack/elb/v3/pools/Update.go
+++ openstack/elb/v3/pools/Update.go
@@ -1,6 +1,9 @@
 package pools
 
-import "github.com/opentelekomcloud/gophertelekomcloud"
+import (
+	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
+)
 
 // UpdateOptsBuilder allows extensions to add additional parameters to the
 // Update request.
@@ -11,56 +14,91 @@ type UpdateOptsBuilder interface {
 // UpdateOpts is the common options' struct used in this package's Update
 // operation.
 type UpdateOpts struct {
-	// Name of the pool.
-	Name *string `json:"name,omitempty"`
-
-	// Human-readable description for the pool.
-	Description *string `json:"description,omitempty"`
-
-	// The algorithm used to distribute load between the members of the pool. The
-	// current specification supports LBMethodRoundRobin, LBMethodLeastConnections
-	// and LBMethodSourceIp as valid values for this attribute.
-	LBMethod string `json:"lb_algorithm,omitempty"`
-
-	// Specifies whether to enable sticky sessions.
-	Persistence *SessionPersistence `json:"session_persistence,omitempty"`
-
-	// The administrative state of the Pool. The value can only be updated to true.
+	// Specifies the administrative status of the backend server group. The value can only be updated to true.
+	//
 	// This parameter is unsupported. Please do not use it.
 	AdminStateUp *bool `json:"admin_state_up,omitempty"`
-
-	// Specifies whether to enable slow start.
-	// This parameter is unsupported. Please do not use it.
-	SlowStart *SlowStart `json:"slow_start,omitempty"`
-
-	// Specifies whether to enable deletion protection for the load balancer.
-	DeletionProtectionEnable *bool `json:"member_deletion_protection_enable,omitempty"`
-
+	// Provides supplementary information about the backend server group.
+	//
+	// Minimum: 0
+	//
+	// Maximum: 255
+	Description string `json:"description,omitempty"`
+	// Specifies the load balancing algorithm used by the load balancer to route requests to backend servers in the associated backend server group.
+	//
+	// The value can be one of the following:
+	//
+	// ROUND_ROBIN: weighted round robin
+	//
+	// LEAST_CONNECTIONS: weighted least connections
+	//
+	// SOURCE_IP: source IP hash
+	//
+	// QUIC_CID: connection ID
+	//
+	// Note:
+	//
+	// If the value is SOURCE_IP, the weight parameter will not take effect for backend servers.
+	//
+	// QUIC_CID is supported only when the protocol of the backend server group is QUIC.
+	//
+	// QUIC_CID is not supported in eu-nl region.
+	LbAlgorithm string `json:"lb_algorithm,omitempty"`
+	// Specifies the backend server group name.
+	//
+	// Minimum: 0
+	//
+	// Maximum: 255
+	Name string `json:"name,omitempty"`
+	// Specifies the sticky session.
+	SessionPersistence SessionPersistence `json:"session_persistence,omitempty"`
+	// Specifies slow start details. After you enable slow start, new backend servers added to the backend server group are warmed up, and the number of requests they can receive increases linearly during the configured slow start duration.
+	//
+	// This parameter can be used when the protocol of the backend server group is HTTP or HTTPS. An error will be returned if the protocol is not HTTP or HTTPS.
+	//
+	// This parameter is not available in eu-nl region. Please do not use it.
+	SlowStart SlowStart `json:"slow_start,omitempty"`
+	// Specifies whether to enable removal protection for the load balancer.
+	//
+	// true: Enable removal protection.
+	//
+	// false: Disable removal protection.
+	//
+	// Disable removal protection for all your resources before deleting your account.
+	//
+	// This parameter is not available in eu-nl region. Please do not use it.
+	MemberDeletionProtectionEnable *bool `json:"member_deletion_protection_enable,omitempty"`
 	// Specifies the ID of the VPC where the backend server group works.
+	//
+	// This parameter can be updated only when vpc_id is left blank.
+	//
+	// Minimum: 0
+	//
+	// Maximum: 36
 	VpcId string `json:"vpc_id,omitempty"`
-
 	// Specifies the type of the backend server group.
+	//
 	// Values:
+	//
 	// instance: Any type of backend servers can be added. vpc_id is mandatory.
-	// ip: Only cross-VPC backend servers can be added. vpc_id cannot be specified.
+	//
+	// ip: Only IP as Backend servers can be added. vpc_id cannot be specified.
+	//
 	// "": Any type of backend servers can be added.
+	//
+	// Note: This parameter can be updated only when type is left blank.
 	Type string `json:"type,omitempty"`
 }
 
-// ToPoolUpdateMap builds a request body from UpdateOpts.
-func (opts UpdateOpts) ToPoolUpdateMap() (map[string]interface{}, error) {
-	return golangsdk.BuildRequestBody(opts, "pool")
-}
-
 // Update allows pools to be updated.
-func Update(client *golangsdk.ServiceClient, id string, opts UpdateOptsBuilder) (r UpdateResult) {
-	b, err := opts.ToPoolUpdateMap()
+func Update(client *golangsdk.ServiceClient, id string, opts UpdateOptsBuilder) (*Pool, error) {
+	b, err := build.RequestBody(opts, "pool")
 	if err != nil {
-		r.Err = err
-		return
+		return nil, err
 	}
-	_, r.Err = client.Put(client.ServiceURL("pools", id), b, &r.Body, &golangsdk.RequestOpts{
+
+	raw, err := client.Put(client.ServiceURL("pools", id), b, nil, &golangsdk.RequestOpts{
 		OkCodes: []int{200},
 	})
-	return
+	return extra(err, raw)
 }
