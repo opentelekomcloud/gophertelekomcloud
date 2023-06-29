997986b22a4837aeb2e46b1570805ff02ab896b9
Aloento <11802769+Aloento@users.noreply.github.com>
2023-06-29 23:49:01 +0200

    clean

--- openstack/elb/v3/pools/Delete.go
+++ openstack/elb/v3/pools/Delete.go
@@ -3,7 +3,7 @@ package pools
 import "github.com/opentelekomcloud/gophertelekomcloud"
 
 // Delete will permanently delete a particular pool based on its unique ID.
-func Delete(client *golangsdk.ServiceClient, id string) (r DeleteResult) {
-	_, r.Err = client.Delete(client.ServiceURL("pools", id), nil)
+func Delete(client *golangsdk.ServiceClient, id string) (err error) {
+	_, err = client.Delete(client.ServiceURL("pools", id), nil)
 	return
 }
