9e4512c5b7bbde01d5767eefb2e890124c271354
Aloento <11802769+Aloento@users.noreply.github.com>
2023-07-12 07:18:30 +0200

    ListAlarmRule

--- openstack/aom/v2/monitor/ListAlarmRule.go
+++ openstack/aom/v2/monitor/ListAlarmRule.go
@@ -1,6 +1,9 @@
 package monitor
 
-import "github.com/opentelekomcloud/gophertelekomcloud"
+import (
+	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+)
 
 type ListAlarmRuleOpts struct {
 	// Value range: 1-1000. Default value: 1000.
@@ -10,17 +13,30 @@ type ListAlarmRuleOpts struct {
 	// Value Range
 	//
 	// (0,1000]
-	Limit *int `json:"limit,omitempty"`
+	Limit *int `q:"limit"`
 	// Pagination information.
 	//
 	// Value Range
 	//
 	// [0, (Maximum value of the int type - 1000)]
-	Start *int `json:"start,omitempty"`
+	Start *int `q:"start"`
 }
 
 func ListAlarmRule(client *golangsdk.ServiceClient, opts ListAlarmRuleOpts) (*ListAlarmRuleResponse, error) {
+	q, err := golangsdk.BuildQueryString(opts)
+	if err != nil {
+		return nil, err
+	}
+
 	// GET /v2/{project_id}/ams/alarms
+	raw, err := client.Get(client.ServiceURL("ams", "alarms")+q.String(), nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res ListAlarmRuleResponse
+	err = extract.Into(raw.Body, &res)
+	return &res, err
 }
 
 type ListAlarmRuleResponse struct {
