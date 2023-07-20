113d8eb31176a4f4602f5e625a5e6807c71ff85a
artem_lifshits <artem.lifshits@t-systems.com>
2023-07-20 20:28:17 +0200

    update fix

--- openstack/identity/v3/groups/UpdateGroupAllProjects.go
+++ openstack/identity/v3/groups/UpdateGroupAllProjects.go
@@ -7,7 +7,9 @@ import (
 func UpdateGroupAllProjects(client *golangsdk.ServiceClient, domainId, groupId, roleId string) error {
 	// PUT https://{Endpoint}/v3/OS-INHERIT/domains/{domain_id}/groups/{group_id}/roles/{role_id}/inherited_to_projects
 	_, err := client.Put(client.ServiceURL("OS-INHERIT", "domains", domainId, "groups", groupId, "roles", roleId, "inherited_to_projects"),
-		nil, nil, nil)
+		nil, nil, &golangsdk.RequestOpts{
+			OkCodes: []int{204},
+		})
 	if err != nil {
 		return err
 	}
