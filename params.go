4d9552140faaab18ba7a7e90c11f58b039657359
Aloento <11802769+Aloento@users.noreply.github.com>
2023-07-27 18:55:33 +0200

    no isZero

--- params.go
+++ params.go
@@ -3,8 +3,6 @@ package golangsdk
 import (
 	"encoding/json"
 	"net/url"
-	"reflect"
-	"time"
 
 	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
 )
@@ -26,40 +24,6 @@ func BuildRequestBody(opts interface{}, parent string) (map[string]interface{}, 
 
 	err = json.Unmarshal(marshal, &res)
 	return res, err
-}
-
-// isZero checks if given argument has default type value.
-func isZero(v reflect.Value) bool {
-	// fmt.Printf("\n\nchecking isZero for value: %+v\n", v)
-	switch v.Kind() {
-	case reflect.Ptr:
-		if v.IsNil() {
-			return true
-		}
-		return false
-	case reflect.Func, reflect.Map, reflect.Slice:
-		return v.IsNil()
-	case reflect.Array:
-		z := true
-		for i := 0; i < v.Len(); i++ {
-			z = z && isZero(v.Index(i))
-		}
-		return z
-	case reflect.Struct:
-		var warp time.Time
-		if v.Type() == reflect.TypeOf(warp) {
-			return v.Interface().(time.Time).IsZero()
-		}
-		z := true
-		for i := 0; i < v.NumField(); i++ {
-			z = z && isZero(v.Field(i))
-		}
-		return z
-	}
-	// Compare other types directly:
-	z := reflect.Zero(v.Type())
-	// fmt.Printf("zero type for value: %+v\n\n\n", z)
-	return v.Interface() == z.Interface()
 }
 
 /*
