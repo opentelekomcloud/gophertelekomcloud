8e8065905220241f5636634895ab3fdeab3bd7a5
Artem Lifshits <55093318+artem-lifshits@users.noreply.github.com>
2023-08-10 10:03:31 +0200

    IAM: UpdateOpts fix for users  (#564)
    IAM: UpdateOpts fix for users 
    
    What this PR does / why we need it
    Fix updateopts
    
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>
    Reviewed-by: Anton Sidelnikov
    Reviewed-by: Aloento

--- openstack/identity/v3.0/users/ModifyUser.go
+++ openstack/identity/v3.0/users/ModifyUser.go
@@ -6,7 +6,38 @@ import (
 	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
 )
 
-func ModifyUser(client *golangsdk.ServiceClient, id string, opts CreateOpts) (*User, error) {
+// UpdateOpts provides options used to create a user.
+type UpdateOpts struct {
+	// Name is the name of the new user.
+	Name string `json:"name,omitempty"`
+
+	// Password is the password of the new user.
+	Password string `json:"password,omitempty"`
+
+	// Email address with a maximum of 255 characters
+	Email string `json:"email,omitempty"`
+
+	// AreaCode is a country code, must be used together with Phone.
+	AreaCode string `json:"areacode,omitempty"`
+
+	// Phone is a mobile number with a maximum of 32 digits, must be used together with AreaCode.
+	Phone string `json:"phone,omitempty"`
+
+	// Description is a description of the user.
+	Description string `json:"description,omitempty"`
+
+	// AccessMode is the access type for IAM user
+	AccessMode string `json:"access_mode,omitempty"`
+
+	// Enabled sets the user status to enabled or disabled.
+	Enabled *bool `json:"enabled,omitempty"`
+
+	// PasswordReset Indicates whether password reset is required at the first login.
+	// By default, password reset is true.
+	PasswordReset *bool `json:"pwd_status,omitempty"`
+}
+
+func ModifyUser(client *golangsdk.ServiceClient, id string, opts UpdateOpts) (*User, error) {
 	b, err := build.RequestBody(opts, "user")
 	if err != nil {
 		return nil, err
