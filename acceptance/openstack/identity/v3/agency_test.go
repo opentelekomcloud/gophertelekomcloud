517f880bc8f8340bc1041ee38fedf81a9ed24a9a
Artem Lifshits <55093318+artem-lifshits@users.noreply.github.com>
2023-09-04 13:46:42 +0200

    [IAM]: Temporary AK/SK duration fix (#574)
    [IAM]: Temporary AK/SK duration fix
    
    What this PR does / why we need it
    Fix duration_time and structure when generating temporary ak/sk.
    Refers to: #568.
    
    Reviewed-by: Anton Sidelnikov
    Reviewed-by: Polina Gubina
    Reviewed-by: Aloento

--- acceptance/openstack/identity/v3/agency_test.go
+++ acceptance/openstack/identity/v3/agency_test.go
@@ -1,14 +1,16 @@
 package v3
 
 import (
+	"fmt"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"time"
 
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/identity/v3/credentials"
+	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestTemporaryCredential(t *testing.T) {
@@ -28,9 +30,64 @@ func TestTemporaryCredential(t *testing.T) {
 	cred, err := credentials.CreateTemporary(client, credentials.CreateTemporaryOpts{
 		Methods:  []string{"token"},
 		Token:    client.Token(),
-		Duration: 60,
+		Duration: 1800,
 	}).Extract()
 	require.NoError(t, err, "error creating temporary AK/SK")
+
+	currentTime := time.Now()
+	returnedTime, err := time.Parse(time.RFC3339Nano, cred.ExpiresAt)
+	th.AssertNoErr(t, err)
+
+	expiration := returnedTime.Sub(currentTime).Minutes()
+	assert.Condition(t, func() bool {
+		if expiration <= 30.0 {
+			fmt.Println(expiration)
+			return false
+		}
+		return true
+	}, "returned time should be 30 min or more")
+
+	assert.NotEmpty(t, cred.AccessKey)
+	assert.NotEmpty(t, cred.SecretKey)
+	assert.NotEmpty(t, cred.SecurityToken)
+	assert.NotEmpty(t, cred.ExpiresAt)
+}
+
+func TestTemporaryCredentialAgency(t *testing.T) {
+	cloud, err := openstack.NewEnv("OS_").Cloud()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if cloud.AuthType != "agency" || cloud.AuthInfo.AgencyName == "" || cloud.AuthInfo.DelegatedProject == "" {
+		t.Skip("Agency authorization is not set up")
+	}
+
+	client, err := clients.NewIdentityV3Client()
+	if err != nil {
+		t.Fatalf("Unable to obtain an identity client: %v", err)
+	}
+
+	cred, err := credentials.CreateTemporary(client, credentials.CreateTemporaryOpts{
+		Methods:    []string{"assume_role"},
+		Token:      client.Token(),
+		Duration:   1800,
+		AgencyName: cloud.AuthInfo.AgencyName,
+		DomainID:   client.DomainID,
+	}).Extract()
+	require.NoError(t, err, "error creating temporary AK/SK")
+
+	currentTime := time.Now()
+	returnedTime, err := time.Parse(time.RFC3339Nano, cred.ExpiresAt)
+	th.AssertNoErr(t, err)
+
+	expiration := returnedTime.Sub(currentTime).Minutes()
+	assert.Condition(t, func() bool {
+		if expiration <= 30.0 {
+			fmt.Println(expiration)
+			return false
+		}
+		return true
+	}, "returned time should be 30 min or more")
 
 	assert.NotEmpty(t, cred.AccessKey)
 	assert.NotEmpty(t, cred.SecretKey)
