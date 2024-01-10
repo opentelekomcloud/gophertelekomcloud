96303ef9466f059fc72546fbaa2193ee59cf6db2
Artem Lifshits <55093318+artem-lifshits@users.noreply.github.com>
2024-01-10 13:21:27 +0100

    [APIGW]: Env implementation (#603)
    [APIGW]: Env implementation
    
    What this PR does / why we need it
    Environment management implementation
    Acceptance tests
    === RUN   TestEnvLifecycle
    tools.go:72: {
    "id": "aa2b02a70cfd490d9d81378820f2c23d",
    "create_time": "2024-01-10T05:20:59Z",
    "name": "test_env_7HFZq_updated",
    "remark": "test env updated"
    }
    --- PASS: TestEnvLifecycle (1.06s)
    PASS
    Process finished with the exit code 0
    === RUN   TestEnvList
    tools.go:72: [
    {
    "id": "DEFAULT_ENVIRONMENT_RELEASE_ID",
    "create_time": "2022-10-18T10:15:45.133669Z",
    "name": "RELEASE",
    "remark": "生产环境"
    }
    ]
    --- PASS: TestEnvList (0.50s)
    PASS
    Process finished with the exit code 0
    
    Reviewed-by: Anton Sidelnikov

---
+++ acceptance/openstack/apigw/v2/env_test.go
@@ -0,0 +1,67 @@
+package v2
+
+import (
+	"os"
+	"testing"
+
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/apigw/v2/env"
+	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
+)
+
+func TestEnvLifecycle(t *testing.T) {
+	gatewayID := os.Getenv("GATEWAY_ID")
+
+	if gatewayID == "" {
+		t.Skip("`GATEWAY_ID` needs to be defined")
+	}
+
+	client, err := clients.NewAPIGWClient()
+	th.AssertNoErr(t, err)
+
+	name := tools.RandomString("test_env_", 5)
+
+	createOpts := env.CreateOpts{
+		GatewayID:   gatewayID,
+		Description: "test env",
+		Name:        name,
+	}
+
+	createResp, err := env.Create(client, createOpts)
+	th.AssertNoErr(t, err)
+
+	t.Cleanup(func() {
+		th.AssertNoErr(t, env.Delete(client, gatewayID, createResp.ID))
+	})
+
+	updateOpts := env.UpdateOpts{
+		EnvID:       createResp.ID,
+		GatewayID:   gatewayID,
+		Description: "test env updated",
+		Name:        name + "_updated",
+	}
+
+	updateResp, err := env.Update(client, updateOpts)
+	th.AssertNoErr(t, err)
+
+	tools.PrintResource(t, updateResp)
+}
+
+func TestEnvList(t *testing.T) {
+	gatewayID := os.Getenv("GATEWAY_ID")
+
+	if gatewayID == "" {
+		t.Skip("`GATEWAY_ID` needs to be defined")
+	}
+
+	client, err := clients.NewAPIGWClient()
+	th.AssertNoErr(t, err)
+
+	listResp, err := env.List(client, env.ListOpts{
+		GatewayID: gatewayID,
+	})
+	th.AssertNoErr(t, err)
+
+	tools.PrintResource(t, listResp)
+}
