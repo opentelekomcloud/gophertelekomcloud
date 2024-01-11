943a30fbf01e4804cd8fb76483d6d7bf72974279
Artem Lifshits <55093318+artem-lifshits@users.noreply.github.com>
2024-01-11 02:12:25 +0100

    [APIGW]: Group implementation (#604)
    [APIGW]: Group implementation
    
    What this PR does / why we need it
    APIGW group management implementation
    Acceptance tests
    === RUN   TestGroupLifecycle
    tools.go:72: {
    "id": "33675baafeca42a49d242242d5469858",
    "name": "apigw_group-lqY-updated",
    "status": 1,
    "sl_domain": "33675baafeca42a49d242242d5469858.apic.eu-de.otc.t-systems.com",
    "register_time": "2024-01-10T05:23:13Z",
    "update_time": "2024-01-10T05:23:14.009794Z",
    "on_sell_status": 2,
    "url_domains": [],
    "sl_domains": [
    "33675baafeca42a49d242242d5469858.apic.eu-de.otc.t-systems.com"
    ],
    "remark": "test updated"
    }
    --- PASS: TestGroupLifecycle (1.49s)
    === RUN   TestGroupList
    tools.go:72: [
    {
    "id": "31f160699d66429aa9a292036c8479a7",
    "name": "DEFAULT",
    "status": 1,
    "sl_domain": "31f160699d66429aa9a292036c8479a7.apic.eu-de.otc.t-systems.com",
    "register_time": "2024-01-10T04:21:45Z",
    "update_time": "2024-01-10T04:21:45Z",
    "on_sell_status": 2,
    "url_domains": [],
    "sl_domains": [
    "31f160699d66429aa9a292036c8479a7.apic.eu-de.otc.t-systems.com"
    ],
    "remark": "This is an automatically generated group. APIs in this group can be accessed with an EIP or a private IP address."
    }
    ]
    --- PASS: TestGroupList (0.51s)
    PASS
    Process finished with the exit code 0
    
    Reviewed-by: Anton Sidelnikov

---
+++ acceptance/openstack/apigw/v2/group_test.go
@@ -0,0 +1,68 @@
+package v2
+
+import (
+	"os"
+	"testing"
+
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/tools"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/apigw/v2/group"
+	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
+)
+
+func TestGroupLifecycle(t *testing.T) {
+	gatewayId := os.Getenv("GATEWAY_ID")
+
+	if gatewayId == "" {
+		t.Skip("`GATEWAY_ID` need to be defined")
+	}
+	client, err := clients.NewAPIGWClient()
+	th.AssertNoErr(t, err)
+
+	name := tools.RandomString("apigw_group-", 3)
+
+	createOpts := group.CreateOpts{
+		Name:        name,
+		Description: "test",
+		GatewayID:   gatewayId,
+	}
+
+	createResp, err := group.Create(client, createOpts)
+	th.AssertNoErr(t, err)
+
+	t.Cleanup(func() {
+		th.AssertNoErr(t, group.Delete(client, gatewayId, createResp.ID))
+	})
+
+	updateOpts := group.UpdateOpts{
+		GroupID:     createResp.ID,
+		Name:        name + "-updated",
+		Description: "test updated",
+		GatewayID:   gatewayId,
+	}
+
+	_, err = group.Update(client, updateOpts)
+	th.AssertNoErr(t, err)
+
+	getResp, err := group.Get(client, gatewayId, createResp.ID)
+	th.AssertNoErr(t, err)
+
+	tools.PrintResource(t, getResp)
+}
+
+func TestGroupList(t *testing.T) {
+	gatewayId := os.Getenv("GATEWAY_ID")
+
+	if gatewayId == "" {
+		t.Skip("`GATEWAY_ID` need to be defined")
+	}
+
+	client, err := clients.NewAPIGWClient()
+	th.AssertNoErr(t, err)
+
+	listResp, err := group.List(client, group.ListOpts{
+		GatewayID: gatewayId,
+	})
+	th.AssertNoErr(t, err)
+	tools.PrintResource(t, listResp)
+}
---
+++ openstack/apigw/v2/group/List.go
@@ -0,0 +1,47 @@
+package group
+
+import (
+	"bytes"
+
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+	"github.com/opentelekomcloud/gophertelekomcloud/pagination"
+)
+
+type ListOpts struct {
+	Limit        int    `q:"limit"`
+	GatewayID    string `json:"-"`
+	InstanceName string `q:"instance_name"`
+	Status       string `q:"status"`
+}
+
+func List(client *golangsdk.ServiceClient, opts ListOpts) ([]GroupResp, error) {
+	q, err := golangsdk.BuildQueryString(&opts)
+	if err != nil {
+		return nil, err
+	}
+	pages, err := pagination.Pager{
+		Client:     client,
+		InitialURL: client.ServiceURL("apigw", "instances", opts.GatewayID, "api-groups") + q.String(),
+		CreatePage: func(r pagination.NewPageResult) pagination.NewPage {
+			return GroupPage{NewSinglePageBase: pagination.NewSinglePageBase{NewPageResult: r}}
+		},
+	}.NewAllPages()
+
+	if err != nil {
+		return nil, err
+	}
+	return ExtractGateways(pages)
+}
+
+type GroupPage struct {
+	pagination.NewSinglePageBase
+}
+
+func ExtractGateways(r pagination.NewPage) ([]GroupResp, error) {
+	var s struct {
+		Gateways []GroupResp `json:"groups"`
+	}
+	err := extract.Into(bytes.NewReader((r.(GroupPage)).Body), &s)
+	return s.Gateways, err
+}
