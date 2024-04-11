651ed1f59dfb21766c2941b790a5dc9e0f61c090
Sergei Martynov <sergei.martynov@t-systems.com>
2024-04-11 13:40:46 +0200

    fix test

--- openstack/dms/v2/instances/requests.go
+++ openstack/dms/v2/instances/requests.go
@@ -205,12 +205,12 @@ type ListOpts struct {
 }
 
 func List(client *golangsdk.ServiceClient, opts ListOpts) (*ListResponse, error) {
-	url, err := golangsdk.NewURLBuilder().WithEndpoints(listURL(client)).WithQueryParams(&opts).Build()
+	url, err := golangsdk.NewURLBuilder().WithEndpoints(resourcePath).WithQueryParams(&opts).Build()
 	if err != nil {
 		return nil, err
 	}
 
-	raw, err := client.Get(url.String(), nil, nil)
+	raw, err := client.Get(client.ServiceURL(url.String()), nil, nil)
 	if err != nil {
 		return nil, err
 	}
