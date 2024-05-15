9bb4436d889e516af236364b9a7ce191098cccfc
Artem Lifshits <55093318+artem-lifshits@users.noreply.github.com>
2024-05-15 14:01:00 +0200

    fgs events (#658)
    [FGS]: Events test
    
    What this PR does / why we need it
    Management of test events.
    Acceptance tests
    === RUN   TestFunctionGraphEventsLifecycle
        events_test.go:28: Attempting to CREATE TEST EVENT
        events_test.go:49: Attempting to LIST TEST EVENTS
        events_test.go:57: Attempting to UPDATE TEST EVENT
        events_test.go:67: Attempting to GET TEST EVENT
        tools.go:72: {
              "id": "fd9942a4-71d2-471b-9bd5-c9c6eb351ee7",
              "content": "ewogICAgImJvZHkiOiAiIiwKICAgICJy",
              "last_modified": 1715772656,
              "name": "funcgraph-eventuI9D"
            }
        events_test.go:42: Attempting to DELETE TEST EVENT
    --- PASS: TestFunctionGraphEventsLifecycle (2.32s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: Aloento
    Reviewed-by: Anton Sidelnikov

---
+++ openstack/fgs/v2/events/Update.go
@@ -0,0 +1,30 @@
+package events
+
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+)
+
+type UpdateOpts struct {
+	FuncUrn string `json:"-"`
+	EventId string `json:"-"`
+	Content string `json:"content" required:"true"`
+}
+
+func Update(client *golangsdk.ServiceClient, opts UpdateOpts) (*Event, error) {
+	b, err := build.RequestBody(opts, "")
+	if err != nil {
+		return nil, err
+	}
+
+	raw, err := client.Put(client.ServiceURL("fgs", "functions", opts.FuncUrn, "events", opts.EventId), b, nil, &golangsdk.RequestOpts{
+		OkCodes: []int{200},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var res Event
+	return &res, extract.Into(raw.Body, &res)
+}
