47dd9b92476b76814f4ae9a45486a5aa65d1b64c
anton-sidelnikov <a.sidelnikov@t-systems.com>
2024-05-17 15:20:11 +0200

    css updates

--- openstack/css/v1/snapshots/UpdateConfiguration.go
+++ openstack/css/v1/snapshots/UpdateConfiguration.go
@@ -1,6 +1,9 @@
 package snapshots
 
-import "github.com/opentelekomcloud/gophertelekomcloud"
+import (
+	"github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
+)
 
 type UpdateConfigurationOpts struct {
 	// OBS bucket used for index data backup.
@@ -8,12 +11,14 @@ type UpdateConfigurationOpts struct {
 	Bucket string `json:"bucket" required:"true"`
 	// IAM agency used to access OBS.
 	Agency string `json:"agency" required:"true"`
+	// IAM agency used to access OBS.
+	BasePath string `json:"basePath" required:"true"`
 	// Key ID used for snapshot encryption.
 	SnapshotCmkID string `json:"snapshotCmkId,omitempty"`
 }
 
 func UpdateConfiguration(client *golangsdk.ServiceClient, clusterID string, opts UpdateConfigurationOpts) (err error) {
-	b, err := golangsdk.BuildRequestBody(opts, "")
+	b, err := build.RequestBody(opts, "")
 	if err != nil {
 		return
 	}
