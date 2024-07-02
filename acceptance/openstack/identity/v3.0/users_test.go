1b5cf7672e0d5ca5b4b2e85675f0c8a862aded07
Artem Lifshits <ar.lifshits@gmail.com>
2024-07-02 10:59:09 +0200

    method for swiss

--- acceptance/openstack/identity/v3.0/users_test.go
+++ acceptance/openstack/identity/v3.0/users_test.go
@@ -2,6 +2,7 @@ package v3
 
 import (
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
@@ -128,4 +129,73 @@ func TestSecurityLifecycle(t *testing.T) {
 	list, err := security.ListUserMfaDevices(client)
 	th.AssertNoErr(t, err)
 	tools.PrintResource(t, list)
+}
+
+func TestUserLifecycleSwiss(t *testing.T) {
+	if os.Getenv("OS_TENANT_ADMIN") == "" {
+		t.Skip("Policy doesn't allow NewIdentityV3AdminClient() to be initialized.")
+	}
+
+	client, err := clients.NewIdentityV30AdminClient()
+	th.AssertNoErr(t, err)
+
+	if !strings.Contains(client.Endpoint, "eu-ch2") {
+		t.Skip("Test is working only for SWISSCLOUD")
+	}
+
+	oldClient, err := clients.NewIdentityV3AdminClient()
+	th.AssertNoErr(t, err)
+
+	createOpts := users.CreateOpts{
+		Name:     tools.RandomString("user-name-", 4),
+		Enabled:  pointerto.Bool(true),
+		DomainID: client.DomainID,
+	}
+
+	user, err := users.CreateUser(client, createOpts)
+	if err != nil {
+		t.Fatalf("Unable to create user: %v", err)
+	}
+
+	t.Cleanup(func() {
+		// SWISS requires xuser_type and xuser_id modification before deletion
+		_, err = users.ModifyUserAdmin(client, users.UpdateAdminOpts{
+			XuserId:   "",
+			XuserType: "",
+			Id:        user.ID,
+		})
+		th.AssertNoErr(t, err)
+
+		err = oldusers.Delete(oldClient, user.ID).ExtractErr()
+		th.AssertNoErr(t, err)
+	})
+
+	th.AssertEquals(t, createOpts.Name, user.Name)
+	th.AssertEquals(t, *createOpts.Enabled, user.Enabled)
+
+	userGet, err := users.GetUser(client, user.ID)
+	if err != nil {
+		t.Fatalf("Unable to retrieve user: %v", err)
+	}
+
+	th.AssertEquals(t, userGet.Name, user.Name)
+	th.AssertEquals(t, userGet.Enabled, user.Enabled)
+	th.AssertEquals(t, userGet.Email, user.Email)
+	th.AssertEquals(t, userGet.DomainID, user.DomainID)
+
+	updateOpts := users.UpdateOpts{
+		Enabled:  pointerto.Bool(false),
+		Name:     tools.RandomString("new-user-name-", 4),
+		Password: tools.RandomString("Hello-world-", 5),
+	}
+
+	userUpdate, err := users.ModifyUser(client, user.ID, updateOpts)
+	if err != nil {
+		t.Fatalf("Unable to update user info: %v", err)
+	}
+
+	th.AssertEquals(t, userUpdate.Name, updateOpts.Name)
+	th.AssertEquals(t, userUpdate.Enabled, *updateOpts.Enabled)
+	th.AssertEquals(t, userUpdate.Email, updateOpts.Email)
+	th.AssertEquals(t, userUpdate.DomainID, userGet.DomainID)
 }
