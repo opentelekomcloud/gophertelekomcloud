6599147d9e823bdf7bc44337a9636fe18894ba36
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2024-09-02 10:58:38 +0200

    [Feat.] Add delete instance function

---
+++ openstack/ddm/instances/Delete.go
@@ -0,0 +1,46 @@
+package instances
+
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+)
+
+type deleteQueryParams struct {
+	// Query param: ?delete_rds_data
+	// This specifies whether data stored on the associated DB instances is deleted.
+	// Default value: delete_rds_data=false.
+	deleteRdsData string `q:"delete_rds_data"`
+}
+
+func Delete(client *golangsdk.ServiceClient, instanceId string, deleteRdsData bool) (*string, error) {
+
+	deleteData := "false"
+	if deleteRdsData {
+		deleteData = "true"
+	}
+	// DELETE https://{Endpoint}/v1/{project_id}/instances/{instance_id}?delete_rds_data=false(OR true)
+	url, err := golangsdk.NewURLBuilder().WithEndpoints("instances", instanceId).WithQueryParams(&deleteQueryParams{deleteRdsData: deleteData}).Build()
+	if err != nil {
+		return nil, err
+	}
+
+	raw, err := client.Delete(client.ServiceURL(url.String()), nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var res Job
+	err = extract.Into(raw.Body, &res)
+	return &res.JobId, err
+
+}
+
+type Job struct {
+
+	// DDM instance ID
+	Id string `json:"id"`
+	// ID of the job for deleting an instance.
+	JobId string `json:"job_id"`
+}
