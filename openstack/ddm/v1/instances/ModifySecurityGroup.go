6fdae6a494f8d3e4325bc879f8558a07c113498b
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2024-09-05 13:28:47 +0200

    [Feat. ] Add Modify Security Group

---
+++ openstack/ddm/v1/instances/ModifySecurityGroup.go
@@ -0,0 +1,35 @@
+package instances
+
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+)
+
+type ModifySecurityGroupOpts struct {
+	// Security group ID. The default value is the original security group ID. You can change the value as required.
+	SecurityGroupId string `json:"security_group_id" required:"true"`
+}
+
+func ModifySecurityGroup(client *golangsdk.ServiceClient, instanceId string, opts ModifySecurityGroupOpts) (*ModifySecurityGroupResponse, error) {
+	b, err := build.RequestBody(opts, "")
+	if err != nil {
+		return nil, err
+	}
+
+	// PUT /v1/{project_id}/instances/{instance_id}/modify-security-group
+	raw, err := client.Put(client.ServiceURL("instances", instanceId, "modify-security-group"), b, nil, &golangsdk.RequestOpts{
+		OkCodes: []int{200},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var res ModifySecurityGroupResponse
+	return &res, extract.Into(raw.Body, &res)
+}
+
+type ModifySecurityGroupResponse struct {
+	// Security group ID. The default value is the original security group ID. You can change the value as required.
+	SecurityGroupId string `json:"security_group_id"`
+}
