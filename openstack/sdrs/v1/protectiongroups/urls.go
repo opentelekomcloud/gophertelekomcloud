c872c5c6d06fb98456533e947ea83ccc819bd4b5
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2024-09-20 12:58:56 +0200

    [Style] Remove unused function

--- openstack/sdrs/v1/protectiongroups/urls.go
+++
@@ -1,7 +0,0 @@
-package protectiongroups
-
-import golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
-
-func resourceURL(c *golangsdk.ServiceClient, id string) string {
-	return c.ServiceURL("server-groups", id)
-}
