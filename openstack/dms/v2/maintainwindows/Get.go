cc1a75df1f6f290cffb8949aa7f4bd26c71a8032
artem-lifshits <ar.lifshits@gmail.com>
2024-09-25 16:15:22 +0200

    lint

--- openstack/dms/v2/maintainwindows/Get.go
+++ openstack/dms/v2/maintainwindows/Get.go
@@ -12,7 +12,7 @@ func Get(client *golangsdk.ServiceClient) (*GetResponse, error) {
 	url := strings.Replace(client.ServiceURL("instances", "maintain-windows"), "/"+client.ProjectID, "", -1)
 	raw, err := client.Get(url, nil, nil)
 	if err != nil {
-
+		return nil, err
 	}
 	var s GetResponse
 	err = extract.Into(raw.Body, &s)
