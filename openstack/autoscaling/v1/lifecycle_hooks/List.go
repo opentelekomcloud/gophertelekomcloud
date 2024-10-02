c8f975612fb442c2317e9907bc650c1d0175e9cb
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2024-10-02 12:04:02 +0200

    [Style] Fix comment

--- openstack/autoscaling/v1/lifecycle_hooks/List.go
+++ openstack/autoscaling/v1/lifecycle_hooks/List.go
@@ -7,7 +7,7 @@ import (
 
 // This function is used to query lifecycle hooks by AS group ID.
 func List(client *golangsdk.ServiceClient, asGroupId string) ([]LifecycleHook, error) {
-	// GET https://{Endpoint}/autoscaling-api/v1/{project_id}/scaling_lifecycle_hook/e5d27f5c-dd76-4a61-b4bc-a67c5686719a/list
+	// GET https://{Endpoint}/autoscaling-api/v1/{project_id}/scaling_lifecycle_hook/{scaling_group_id}/list
 	raw, err := client.Get(client.ServiceURL("scaling_lifecycle_hook", asGroupId, "list"), nil, &golangsdk.RequestOpts{
 		OkCodes: []int{200},
 	})
