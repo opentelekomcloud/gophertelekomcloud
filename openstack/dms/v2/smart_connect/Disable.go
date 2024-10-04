97ff0238bf4224126ec45a4db5d8ef7f34d15529
artem-lifshits <ar.lifshits@gmail.com>
2024-10-04 18:22:45 +0200

    fix disable

--- openstack/dms/v2/smart_connect/Disable.go
+++ openstack/dms/v2/smart_connect/Disable.go
@@ -2,6 +2,7 @@ package smart_connect
 
 import (
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
 	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
 )
 
@@ -9,7 +10,14 @@ import (
 // Send POST /v2/{project_id}/kafka/instances/{instance_id}/delete-connector
 func Disable(client *golangsdk.ServiceClient, id string) (*DisableResp, error) {
 	// Providing empty body results to an error therefore filler is used
-	raw, err := client.Post(client.ServiceURL("kafka", "instances", id, "delete-connector"), "{}", nil, &golangsdk.RequestOpts{
+	emptyBody := struct{}{}
+
+	b, err := build.RequestBody(emptyBody, "")
+	if err != nil {
+		return nil, err
+	}
+
+	raw, err := client.Post(client.ServiceURL("kafka", "instances", id, "delete-connector"), b, nil, &golangsdk.RequestOpts{
 		OkCodes: []int{200},
 	})
 	if err != nil {
