6732372daed53165886c864dc4649ea9737c121d
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2024-12-04 15:47:16 +0100

    [Refactor] Refactor CCE list nodes

--- openstack/cce/v3/nodes/urls.go
+++
@@ -1,7 +0,0 @@
-package nodes
-
-import golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
-
-func rootURL(c *golangsdk.ServiceClient, clusterid string) string {
-	return c.ServiceURL("clusters", clusterid, "nodes")
-}
