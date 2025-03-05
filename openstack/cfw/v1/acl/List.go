c8e694687ddf3737abaec3c05c1ede4583f2db7e
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2025-03-05 11:53:57 +0100

    [Feat.] Add function to list acl rules

--- openstack/cfw/v1/acl/List.go
+++ openstack/cfw/v1/acl/List.go
@@ -1,13 +1,11 @@
 package acl
 
 import (
-	"fmt"
-
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
 )
 
-type GetQueryParameters struct {
+type ListQueryParameters struct {
 	// Protected object ID, which is used to distinguish between Internet border protection and VPC border protection
 	// after a cloud firewall is created. You can obtain the ID by calling the Get function in management package.
 	// In the return value, find the ID in ProtectObjects[n].ObjectID.
@@ -18,23 +16,20 @@ type GetQueryParameters struct {
 	Offset string `q:"offset" required:"true"`
 	// Number of records displayed on each page. The value ranges from 1 to 1024.
 	Limit int `q:"limit" required:"true"`
-	// Rule name.
-	Name string `q:"name,omitempty"`
 }
 
-// This function is used to query a protection rule.
+// This function is used to query all protection rules.
 // Protected object ID, which is used to distinguish between Internet border protection and VPC border protection
 // after a cloud firewall is created. You can obtain the ID by calling the Get function in management package.
 // In the return value, find the ID in ProtectObjects[n].ObjectID.
 // If the value of type is 0, the protected object ID belongs to the Internet border.
 // If the value of type is 1, the protected object ID belongs to the VPC border.
-func GetACLRule(client *golangsdk.ServiceClient, objectId string, ruleName string) (*ACLRule, error) {
+func ListACLRules(client *golangsdk.ServiceClient, objectId string) ([]ACLRule, error) {
 	// GET /v1/{project_id}/acl-rules
-	url, err := golangsdk.NewURLBuilder().WithEndpoints("acl-rules").WithQueryParams(&GetQueryParameters{
+	url, err := golangsdk.NewURLBuilder().WithEndpoints("acl-rules").WithQueryParams(&ListQueryParameters{
 		ObjectID: objectId,
 		Limit:    1024,
 		Offset:   "0",
-		Name:     ruleName,
 	}).Build()
 	if err != nil {
 		return nil, err
@@ -45,18 +40,12 @@ func GetACLRule(client *golangsdk.ServiceClient, objectId string, ruleName strin
 		return nil, err
 	}
 
-	var res GetResponse
+	var res ListResponse
 	err = extract.Into(raw.Body, &res)
-	if err != nil {
-		return nil, err
-	}
-	if len(res.Data.Records) != 0 {
-		return &res.Data.Records[0], nil
-	}
-	return nil, fmt.Errorf("no acl rule found")
+	return res.Data.Records, err
 }
 
-type GetResponse struct {
+type ListResponse struct {
 	// Return value for querying the rule list.
 	Data ACLRuleQueryResponseData `json:"data"`
 }
