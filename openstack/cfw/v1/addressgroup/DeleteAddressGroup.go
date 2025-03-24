83211e26b28088230371097bd70c8ccb225121fc
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2025-03-24 07:12:35 +0100

    [Style] Change description

--- openstack/cfw/v1/addressgroup/DeleteAddressGroup.go
+++ openstack/cfw/v1/addressgroup/DeleteAddressGroup.go
@@ -3,8 +3,7 @@ package addressgroup
 import golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 
 // This function is used to delete an address group.
-// groupId: Address group ID. It is the same as ID retuned while creating a rule.
-// You can also obtain it using GetAddressGroup.
+// groupId: Address group ID. It is the same as ID retuned while creating an address group.
 func DeleteAddressGroup(client *golangsdk.ServiceClient, groupId string) error {
 	// DELETE /v1/{project_id}/address-sets/{set_id}
 	_, err := client.Delete(client.ServiceURL("address-sets", groupId), &golangsdk.RequestOpts{
