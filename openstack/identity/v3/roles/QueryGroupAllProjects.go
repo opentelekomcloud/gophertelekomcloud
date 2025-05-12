b568cf805b8b6aca68e1cbdb9fe3986214d06246
Artem Lifshits <55093318+artem-lifshits@users.noreply.github.com>
2025-05-12 14:38:21 +0200

    fix QueryGroupAllProjects.go (#833)

--- openstack/identity/v3/roles/QueryGroupAllProjects.go
+++ openstack/identity/v3/roles/QueryGroupAllProjects.go
@@ -3,19 +3,37 @@ package roles
 import (
 	"github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
-	"github.com/opentelekomcloud/gophertelekomcloud/openstack/identity/v3/policies"
 )
 
-// QueryGroupAllProjects API not registered
-func QueryGroupAllProjects(client *golangsdk.ServiceClient, domainId, groupId, roleId string) (*policies.ListPolicy, error) {
-	// GET https://{Endpoint}/v3/OS-INHERIT/domains/{domain_id}/groups/{group_id}/roles/{role_id}/inherited_to_projects
-	raw, err := client.Get(client.ServiceURL("OS-INHERIT", "domains", domainId, "groups", groupId, "roles", roleId, "inherited_to_projects"),
+func QueryGroupAllProjects(client *golangsdk.ServiceClient, domainId, groupId string) ([]RoleList, error) {
+	// GET https://{Endpoint}/v3/OS-INHERIT/domains/{domain_id}/groups/{group_id}/roles/inherited_to_projects
+	raw, err := client.Get(client.ServiceURL("OS-INHERIT", "domains", domainId, "groups", groupId, "roles", "inherited_to_projects"),
 		nil, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	var res policies.ListPolicy
+	var res []RoleList
 	err = extract.IntoSlicePtr(raw.Body, &res, "roles")
-	return &res, err
+	return res, err
+}
+
+type RoleList struct {
+	Flag        string `json:"flag"`
+	Catalog     string `json:"catalog"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Links       Links  `json:"links"`
+	ID          string `json:"id"`
+	DisplayName string `json:"display_name"`
+	Type        string `json:"type"`
+	Policy      Policy `json:"policy"`
+	UpdatedTime string `json:"updated_time"`
+	CreatedTime string `json:"created_time"`
+}
+
+type Links struct {
+	Self     string `json:"self"`
+	Previous string `json:"previous"`
+	Next     string `json:"next"`
 }
