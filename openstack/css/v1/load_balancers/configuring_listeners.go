5cf355bf23b50af81246665c801ddbc4d5ee01f9
sattila1999 <somogyiattila417@gmail.com>
2025-07-14 16:02:53 +0200

    Adding CSS load balancing functions (configuring_listeners, updating_listeners)

---
+++ openstack/css/v1/load_balancers/configuring_listeners.go
@@ -0,0 +1,50 @@
+package load_balancers
+
+import (
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
+	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
+)
+
+type LoadBalancingListenerOpts struct {
+	// These parameters passed to the load_balancers.ConfigureLoadBalancingListeners function.
+	// Protocol type. HTTP and HTTPS are supported.
+	Protocol string `json:"protocol" required:"true"`
+	// Port.
+	ProtocolPort int `json:"protocol_port" required:"true"`
+	// Server certificate ID. This parameter is mandatory when protocol is set to HTTPS.
+	ServerCertId string `json:"server_cert_id"`
+	// CA certificate ID. This parameter is mandatory when protocol is set to HTTPS and bidirectional authentication is used.
+	CaCertId string `json:"ca_cert_id"`
+	// Type: searchTool indicates that the listener is configured for Elasticsearch/OpenSearch.
+	// viewTool indicates that the listener is configured for Kibana/OpenSearch Dashboards.
+	// The default value is searchTool.
+	Type string `json:"type"`
+}
+
+// ConfigureLoadBalancingListeners will configure load balancing listeners for a CSS cluster based on LoadBalancingListenerOpts.
+func ConfigureLoadBalancingListeners(client *golangsdk.ServiceClient, clusterID string, opts LoadBalancingListenerOpts) (string, error) {
+	b, err := build.RequestBody(opts, "")
+	if err != nil {
+		return "", err
+	}
+
+	url := client.ServiceURL("clusters", clusterID, "es-listeners")
+
+	raw, err := client.Post(url, b, nil, &golangsdk.RequestOpts{
+		OkCodes: []int{200},
+		MoreHeaders: map[string]string{
+			"Content-Type": "application/json",
+		},
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	var res struct {
+		ElbId string `json:"elb_id"`
+	}
+	err = extract.IntoStructPtr(raw.Body, &res, "")
+	return res.ElbId, err
+}
