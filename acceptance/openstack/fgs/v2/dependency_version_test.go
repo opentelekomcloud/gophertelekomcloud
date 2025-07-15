bdf5c79af47e1b79cc14526b7c05091e03ee785e
artem-lifshits <ar.lifshits@gmail.com>
2025-07-15 14:30:40 +0200

    fix test & structs

--- acceptance/openstack/fgs/v2/dependency_version_test.go
+++ acceptance/openstack/fgs/v2/dependency_version_test.go
@@ -26,13 +26,40 @@ func TestDependencyVersionLifecycle(t *testing.T) {
 	}(client, funcUrn)
 
 	createOpts := dependency_version.CreateOpts{
-		Name:       tools.RandomString("dep-version-", 4),
-		Runtime:    "Python2.7",
-		DependType: "zip",
-		DependFile: "",
+		Name:        tools.RandomString("dep-version-", 4),
+		Runtime:     "Python3.9",
+		DependType:  "obs",
+		DependLink:  "https://regr-func-graph.obs.eu-de.otc.t-systems.com/requirements.zip",
+		Description: "test dependency",
 	}
 
 	createDepResp, err := dependency_version.Create(client, createOpts)
 	th.AssertNoErr(t, err)
-	tools.PrintResource(t, createDepResp)
+
+	defer func(client *golangsdk.ServiceClient, id string) {
+		err = dependency_version.Delete(client, createDepResp.DepId, "1")
+		th.AssertNoErr(t, err)
+	}(client, funcUrn)
+
+	getDepResp, err := dependency_version.Get(client, createDepResp.DepId, "1")
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, createDepResp.Name, getDepResp.Name)
+	th.AssertEquals(t, createDepResp.Runtime, getDepResp.Runtime)
+	th.AssertEquals(t, createDepResp.Etag, getDepResp.Etag)
+
+	listDepResp, err := dependency_version.ListDependencies(client, dependency_version.ListDependenciesOpts{
+		DependencyType: "obs",
+		Runtime:        "Python3.9",
+	})
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, len(listDepResp.Dependencies) > 1, true)
+
+	listDepVerResp, err := dependency_version.ListDependencyVersions(client, dependency_version.ListDependencyVersionOpts{
+		DependId: createDepResp.DepId,
+	})
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, listDepVerResp.Dependencies[0].Name, getDepResp.Name)
+	th.AssertEquals(t, listDepVerResp.Dependencies[0].Runtime, getDepResp.Runtime)
+	th.AssertEquals(t, listDepVerResp.Dependencies[0].Etag, getDepResp.Etag)
+
 }
--- openstack/fgs/v2/dependency_version/List.go
+++ openstack/fgs/v2/dependency_version/List.go
@@ -5,13 +5,13 @@ import (
 	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
 )
 
-type ListOpts struct {
+type ListDependencyVersionOpts struct {
 	DependId string `json:"-"`
 	Marker   string `q:"marker"`
 	MaxItems string `q:"max_items"`
 }
 
-func List(client *golangsdk.ServiceClient, opts ListOpts) (*ListDepVersionResp, error) {
+func ListDependencyVersions(client *golangsdk.ServiceClient, opts ListDependencyVersionOpts) (*ListDepVersionResp, error) {
 	url, err := golangsdk.NewURLBuilder().WithEndpoints("fgs", "dependencies", opts.DependId, "version").WithQueryParams(&opts).Build()
 	if err != nil {
 		return nil, err
@@ -28,7 +28,24 @@ func List(client *golangsdk.ServiceClient, opts ListOpts) (*ListDepVersionResp, 
 }
 
 type ListDepVersionResp struct {
-	Dependencies []DepVersionResp `json:"dependencies"`
-	NextMarker   int              `json:"next_marker"`
-	Count        int              `json:"count"`
+	Dependencies []DepVersionListResp `json:"dependencies"`
+	NextMarker   int                  `json:"next_marker"`
+	Count        int                  `json:"count"`
+}
+
+type DepVersionListResp struct {
+	Id           string `json:"id"`
+	Owner        string `json:"owner"`
+	Link         string `json:"link"`
+	Runtime      string `json:"runtime"`
+	Etag         string `json:"etag"`
+	Size         int    `json:"size"`
+	Name         string `json:"name"`
+	Description  string `json:"description"`
+	FileName     string `json:"file_name"`
+	Version      int    `json:"version"`
+	DepId        string `json:"dep_id"`
+	LastModified string `json:"last_modified"`
+	DownloadLink string `json:"download_link"`
+	IsShared     bool   `json:"is_shared"`
 }
