6ac977598950c68bd72338ab7daae7ff50d3df99
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2025-09-01 16:02:57 +0200

    [Feat.] TMS providers (#864)

---
+++ acceptance/openstack/tms/provider_test.go
@@ -0,0 +1,22 @@
+package tms
+
+import (
+	"os"
+	"testing"
+
+	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/tms/v1/provider"
+	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
+)
+
+func TestTMSProviderList(t *testing.T) {
+	if os.Getenv("RUN_TMS_TAGS") == "" {
+		t.Skip("unstable test")
+	}
+	client, err := clients.NewTmsV1Client()
+	th.AssertNoErr(t, err)
+
+	p, err := provider.List(client, provider.ListOpts{})
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, true, len(p) > 1)
+}
