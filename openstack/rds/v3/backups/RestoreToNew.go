320c76f413ee2ec81903d390f8c1b0b06c12fc73
lazysnail <lazysnail1024@gmail.com>
2025-09-17 22:39:17 +0800

    [Fix] revert

--- openstack/rds/v3/backups/RestoreToNew.go
+++ openstack/rds/v3/backups/RestoreToNew.go
@@ -67,9 +67,7 @@ type RestoreToNewOpts struct {
 	// Specifies the restoration information.
 	RestorePoint RestorePoint `json:"restore_point" required:"true"`
 	// This parameter applies only to Microsoft SQL Server DB instances.
-	Collation string `json:"collation,omitempty"`
-	// Specifies the UTC time zone.
-	TimeZone          string           `json:"time_zone,omitempty"`
+	Collation         string           `json:"collation,omitempty"`
 	UnchangeableParam *instances.Param `json:"unchangeable_param,omitempty"`
 }
 
