6c26163597538766c35cffae87e9922bff3a68f9
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2025-10-10 18:24:39 +0200

    [Feat.] New attributes for SMN subscriptions (#880)

--- openstack/smn/v2/subscriptions/Create.go
+++ openstack/smn/v2/subscriptions/Create.go
@@ -10,9 +10,40 @@ type CreateOpts struct {
 	// Message endpoint
 	Endpoint string `json:"endpoint" required:"true"`
 	// Protocol of the message endpoint
+	// The following protocols are supported:
+	// email: The endpoints are email address.
+	// sms: The endpoints are phone numbers.
+	// http and https: The endpoints are URLs.
 	Protocol string `json:"protocol" required:"true"`
 	// Description of the subscription
 	Remark string `json:"remark,omitempty"`
+	// Extended information
+	Extension *SubscriptionExtension `json:"extension,omitempty"`
+	// This parameter is mandatory when subscriptions need to be created in batches.
+	// SMN allows you to create a maximum of 50 subscriptions at a time.
+	Subscriptions []AddSubscriptions `json:"subscriptions,omitempty"`
+}
+
+type AddSubscriptions struct {
+	// Message endpoint
+	Endpoint string `json:"endpoint" required:"true"`
+	// Protocol of the message endpoint
+	// The following protocols are supported:
+	// email: The endpoints are email address.
+	// sms: The endpoints are phone numbers.
+	// http and https: The endpoints are URLs.
+	Protocol string `json:"protocol" required:"true"`
+	// Description of the subscription
+	Remark string `json:"remark,omitempty"`
+	// Extended information
+	Extension *SubscriptionExtension `json:"extension,omitempty"`
+}
+
+type SubscriptionExtension struct {
+	// This is an HTTP header field, which can be customized within the field range.
+	// The field content exists in the form of key/value pairs.
+	// When a topic is used to send messages, confirmed subscription messages carry the user-defined HTTP header.
+	Header map[string]string `json:"header,omitempty"`
 }
 
 func Create(client *golangsdk.ServiceClient, opts CreateOpts, topicUrn string) (*CreateResponse, error) {
