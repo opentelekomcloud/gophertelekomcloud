c6960974612c8d1a686c0ee9b30e0af88b8f5dc6
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2025-10-30 05:53:34 +0100

    [Fix] Fix and update test

--- acceptance/openstack/cts/v1/trackers_test.go
+++ acceptance/openstack/cts/v1/trackers_test.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/clients"
 	"github.com/opentelekomcloud/gophertelekomcloud/acceptance/openstack/cts"
-	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/pointerto"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/cts/v1/tracker"
 	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
 )
@@ -26,6 +25,11 @@ func TestTrackersLifecycle(t *testing.T) {
 	t.Logf("Attempting to create CTSv1 Tracker")
 	ctsTracker, err := tracker.Create(client, tracker.CreateOpts{
 		BucketName: bucketName,
+		Lts: &tracker.CreateLts{
+			IsLtsEnabled: true,
+			LogGroupName: "test-acc-cts-group",
+			LogTopicName: "test-acc-cts-topic",
+		},
 	})
 
 	t.Cleanup(func() {
