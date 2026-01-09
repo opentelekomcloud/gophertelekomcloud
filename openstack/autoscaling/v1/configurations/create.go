85dbf203e930c4fab36bc9485b2e4060a3609f4a
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2026-01-09 12:28:23 +0100

    [Fix] Autoscaling: Fix Public IP name in v1 configuration opts

--- openstack/autoscaling/v1/configurations/create.go
+++ openstack/autoscaling/v1/configurations/create.go
@@ -3,7 +3,7 @@ package configurations
 import (
 	"encoding/base64"
 
-	"github.com/opentelekomcloud/gophertelekomcloud"
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/internal/extract"
 )
 
@@ -36,7 +36,7 @@ type InstanceConfigOpts struct {
 	// Specifies the EIP of the ECS. The EIP can be configured in two ways.
 	// Do not use an EIP. In this case, this parameter is unavailable.
 	// Automatically assign an EIP. You need to specify the information about the new EIP.
-	PubicIp *PublicIp `json:"public_ip,omitempty"`
+	PublicIp *PublicIp `json:"public_ip,omitempty"`
 	// Specifies the user data to be injected during the ECS creation process. Text, text files, and gzip files can be injected.
 	// Constraints:
 	// The content to be injected must be encoded with base64. The maximum size of the content to be injected (before encoding) is 32 KB.
