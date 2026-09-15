39dab71adae0a68acdcbac2af3516138670ab09a
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2026-09-15 11:35:41 +0200

    [Fix] VPC: Fix remarks parameter in address group response (#996)

--- acceptance/openstack/vpc/v3/address_group_test.go
+++ acceptance/openstack/vpc/v3/address_group_test.go
@@ -49,3 +49,57 @@ func TestVPCAddressGroupV3Lifecycle(t *testing.T) {
 	th.AssertNoErr(t, err)
 	th.AssertNotEquals(t, 0, len(addressGroupList.AddressGroups))
 }
+
+func TestVPCAddressGroupV3Lifecycle_IpExtraSet(t *testing.T) {
+	client, err := clients.NewVPCV3Client()
+	th.AssertNoErr(t, err)
+
+	name := tools.RandomString("address-grp-acc-", 3)
+
+	addressGroup, err := addressgroup.Create(client, addressgroup.CreateOpts{
+		AddressGroup: addressgroup.AddressGroupOptions{
+			Name:        name,
+			Description: "create-address-group",
+			IpVersion:   4,
+			IpExtraSet: []addressgroup.IpExtraSetOption{
+				{
+					Ip:      "192.168.10.10",
+					Remarks: "test",
+				},
+			},
+		},
+	})
+	th.AssertNoErr(t, err)
+	t.Cleanup(func() {
+		th.AssertNoErr(t, addressgroup.Delete(client, addressGroup.ID))
+	})
+	th.AssertEquals(t, "192.168.10.10", addressGroup.IPExtraSet[0].IP)
+
+	updatedDescription := "updated-description"
+	updatedName := name + "-updated"
+
+	addressGroup, err = addressgroup.Update(client, addressGroup.ID, addressgroup.UpdateOpts{
+		AddressGroup: addressgroup.UpdateAddressGroupOptions{
+			Name:        updatedName,
+			Description: updatedDescription,
+			IpExtraSet: []addressgroup.IpExtraSetOption{
+				{
+					Ip:      "192.168.10.11",
+					Remarks: "test-updated",
+				},
+			},
+		},
+	})
+	th.AssertNoErr(t, err)
+
+	updatedGroup, err := addressgroup.Get(client, addressGroup.ID)
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, updatedName, updatedGroup.Name)
+	th.AssertEquals(t, updatedDescription, updatedGroup.Description)
+	th.AssertEquals(t, "192.168.10.11", updatedGroup.IPExtraSet[0].IP)
+	th.AssertEquals(t, "test-updated", updatedGroup.IPExtraSet[0].Remarks)
+
+	addressGroupList, err := addressgroup.List(client, addressgroup.ListQueryParams{})
+	th.AssertNoErr(t, err)
+	th.AssertNotEquals(t, 0, len(addressGroupList.AddressGroups))
+}
